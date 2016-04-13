723a098729989f884705ff871868687985a695ca
jason gilbertson <jagilber@microsoft.com>
2016-04-12 21:01:09 -0400

    update to rds-lic-svr-chk for win32_tsdeploymentlicensing. added some rds cal per device scripts

---
+++ PowerShellProject/PowerShellProject/rds-lic-per-device-cal-enumerate.ps1
@@ -0,0 +1,28 @@
+
+# script to enumerate ts perdevice cals
+ 
+$activeLicenses = @()
+$licenses = get-wmiobject Win32_TSIssuedLicense
+ 
+#licenseStatus = 4 = revoked, 1 = temp, 2 = permanent
+$activelicenses = @($licenses | where {$_.licenseStatus -ne 4})
+
+if($activeLicenses.Count -ge 1)
+{
+    #$activeLicenses | out-gridview
+    $activeLicenses | select sIssuedToComputer | fl
+   
+    foreach ($lic in $activeLicenses)
+    {
+        write-host "enumerating license:$($lic)"
+        write-host $lic | fl *
+        #write-host "removing clientName:$($clientName)"
+        #$lic.Revoke() | select ReturnValue, RevokableCals, NextRevokeAllowedOn | fl
+    }
+}
+else
+{
+    write-host "no licenses to enumerate"
+}
+ 
+write-host "finished"
--- PowerShellProject/PowerShellProject/rds-lic-svr-chk.ps1
+++ PowerShellProject/PowerShellProject/rds-lic-svr-chk.ps1
@@ -5,20 +5,6 @@
     This script will enumerate license server configuration off of an RDS server. it will check the known registry locations
     as well as run WMI tests against the configuration. a log file named rds-lic-svr-chk.txt will be generated that should
     be uploaded to support.
-    
-    ** Copyright (c) Microsoft Corporation. All rights reserved - 2016.
-    **
-    ** This script is not supported under any Microsoft standard support program or service.
-    ** The script is provided AS IS without warranty of any kind. Microsoft further disclaims all
-    ** implied warranties including, without limitation, any implied warranties of merchantability
-    ** or of fitness for a particular purpose. The entire risk arising out of the use or performance
-    ** of the scripts and documentation remains with you. In no event shall Microsoft, its authors,
-    ** or anyone else involved in the creation, production, or delivery of the script be liable for
-    ** any damages whatsoever (including, without limitation, damages for loss of business profits,
-    ** business interruption, loss of business information, or other pecuniary loss) arising out of
-    ** the use of or inability to use the script or documentation, even if Microsoft has been advised
-    ** of the possibility of such damages.
-    **
  
 .NOTES  
    File Name  : rds-lic-svr-chk.ps1  
@@ -56,19 +42,41 @@ $licServers = @()
 function main()
 { 
     log-info "-----------------------------------------"
+    log-info "REGISTRY"
+    log-info "-----------------------------------------"
+
+    log-info (read-reg 'SYSTEM\CurrentControlSet\Control\Terminal Server\RCM')
+ 
+    log-info (read-reg 'SOFTWARE\Microsoft\Windows NT\CurrentVersion\Terminal Server\TSAppAllowList\LicenseServers')
+    log-info (read-reg 'SOFTWARE\Microsoft\Windows NT\CurrentVersion\Terminal Server\TSAppAllowList\LicensingMode')
+ 
+    log-info (read-reg 'SYSTEM\CurrentControlSet\Services\TermService\Parameters\LicenseServers\SpecifiedLicenseServers')
+    log-info (read-reg 'SYSTEM\CurrentControlSet\Services\TermService\Parameters\LicenseServers\LicensingMode')
+ 
+    log-info (read-reg 'SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services\LicenseServers')
+    log-info (read-reg 'SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services\LicensingMode')
+
+    log-info "-----------------------------------------"
     log-info "Win32_TerminalServiceSetting"
     log-info "-----------------------------------------"
  
     $rWmi = Get-WmiObject -Namespace root/cimv2/TerminalServices -Class Win32_TerminalServiceSetting
     log-info $rWmi
- 
+    
+    
+    log-info "-----------------------------------------"
+    log-info "Win32_TSDeploymentLicensing"
+    log-info "-----------------------------------------"
+    
+    $rWmiL = Get-WmiObject -Namespace root/cimv2/TerminalServices -Class Win32_TSDeploymentLicensing
+    log-info $rWmiL
+    
     if([string]::IsNullOrEmpty($licServer))
     {
         log-info "checking gpo for lic server"
-        #--> HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services\LicenseServers
-        #--> HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows NT\Terminal Services\LicenseServers
-        $licServers = @((read-reg 'SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services\LicenseServers').ToString().Split(","))
-   
+        $licServers = @(([string](read-reg 'SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services\LicenseServers')).Split(",",[StringSplitOptions]::RemoveEmptyEntries))
+        $licMode = (read-reg 'SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services\LicensingMode')
+        log-info "gpo lic servers: $($licServers) license mode: $($licMode)"
     }
     else
     {
@@ -80,24 +88,17 @@ function main()
     {
         log-info "checking wmi for lic server"
         $licServers = @($rWmi.GetSpecifiedLicenseServerList().SpecifiedLSList)
+        $licMode = "$($rWmi.LicensingName) ($($rWmi.LicensingType))"
+        log-info "wmi lic servers: $($licServers) license mode: $($licMode)"
     }
  
     if($licServers.Length -lt 1)
     {
         log-info "checking ps for lic server"
-        $licServers = @((Get-RDLicenseConfiguration).LicenseServer.ToString().Split(","))
+        $licServers = @(([string]((Get-RDLicenseConfiguration).LicenseServer)).Split(",", [StringSplitOptions]::RemoveEmptyEntries))
+        $licMode = (Get-RDLicenseConfiguration).Mode
+        log-info "ps lic servers: $($licServers) license mode: $($licMode)"
     }
- 
-    log-info (read-reg 'SYSTEM\CurrentControlSet\Control\Terminal Server\RCM')
- 
-    log-info (read-reg 'SOFTWARE\Microsoft\Windows NT\CurrentVersion\Terminal Server\TSAppAllowList\LicenseServers')
-    log-info (read-reg 'SOFTWARE\Microsoft\Windows NT\CurrentVersion\Terminal Server\TSAppAllowList\LicensingMode')
- 
-    log-info (read-reg 'SYSTEM\CurrentControlSet\Services\TermService\Parameters\LicenseServers\SpecifiedLicenseServers')
-    log-info (read-reg 'SYSTEM\CurrentControlSet\Services\TermService\Parameters\LicenseServers\LicensingMode')
- 
-    log-info (read-reg 'SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services\LicenseServers')
-    log-info (read-reg 'SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services\LicensingMode')
  
     if($licServers.Length -lt 1)
     {
@@ -209,6 +210,8 @@ function log-info($data)
 # ----------------------------------------------------------------------------------------------------------------
 function read-reg($key)
 {
+    $retVal
+
     log-info "-----------------------------------------" 
     log-info "enumerating $($key)"
     #log-info "-----------------------------------------" 
@@ -234,8 +237,9 @@ function read-reg($key)
                 $baseKey = [Microsoft.Win32.RegistryKey]::OpenBaseKey( `
                                 [Microsoft.Win32.RegistryHive]::LocalMachine, `
                                 [Microsoft.Win32.RegistryView]::Default).OpenSubKey([IO.Path]::GetDirectoryName($key));
-                log-info ([string]::Format("{0,-20}: {1}", [IO.Path]::GetFileName($key) , ($baseKey.GetValue([IO.Path]::GetFileName($key)) -join ",")))
-                return
+                $retVal = ($baseKey.GetValue([IO.Path]::GetFileName($key)) -join ",")
+                log-info ([string]::Format("{0,-20}: {1}", [IO.Path]::GetFileName($key) , $retVal))
+                return $retVal
             }
             catch
             {
@@ -248,8 +252,11 @@ function read-reg($key)
     foreach($valueName in $baseKey.GetValueNames())
     {
         $value = $baseKey.GetValue($valueName)
-        log-info ([string]::Format("{0,-20}: {1}", $valueName, ($value -join ",")))
+        $tempVal = ([string]::Format("{0,-20}: {1}", $valueName, ($value -join ",")))
+        log-info $tempVal
         log-info ""
+
+        $retVal += $tempVal
     }
 
     foreach($subkey in $baseKey.GetSubKeyNames())
@@ -258,6 +265,7 @@ function read-reg($key)
     }
 
     $baseKey.Close();
+    return $retVal
 }
 
 # ----------------------------------------------------------------------------------------------------------------
