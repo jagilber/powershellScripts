19758f8202740ab48284d9dbf902dff296cf71ab
Jason Gilbertson <jagilber@microsoft.com>
2017-07-07 23:56:53 -0400

    get-update

--- azure-rm-rdp-post-deployment-multi.ps1
+++ azure-rm-rdp-post-deployment-multi.ps1
@@ -779,27 +779,31 @@ function get-update($updateUrl, $destinationFile)
     try 
     {
         $git = Invoke-RestMethod -Method Get -Uri $updateUrl 
-        $gitClean = [regex]::Replace($git, '\W+', "")
+
+        # git  may not have carriage return
+        if ([regex]::Matches($git, "`r").Count -eq 0)
+        {
+            $git = [regex]::Replace($git, "`n", "`r`n")
+        }
 
         if (![IO.File]::Exists($destinationFile))
         {
-            $fileClean = ""    
+            $file = ""    
         }
         else
         {
-            $fileClean = [regex]::Replace(([IO.File]::ReadAllText($destinationFile)), '\W+', "")
+            $file = [IO.File]::ReadAllText($destinationFile)
         }
 
-        if (([string]::Compare($gitClean, $fileClean) -ne 0))
+        if (([string]::Compare($git, $file) -ne 0))
         {
-            write-host "copying script $($destinationFile)"
+            log-info "copying script $($destinationFile)"
             [IO.File]::WriteAllText($destinationFile, $git)
-            write-host "script updated. restart script" -ForegroundColor Yellow
             return $true
         }
         else
         {
-            write-host "script is up to date"
+            log-info "script is up to date"
         }
         
         return $false
