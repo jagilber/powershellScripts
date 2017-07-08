3131e8118f7d6c2e78eeaafdbc6ee7b4a09902a0
Jason Gilbertson <jagilber@microsoft.com>
2017-07-08 18:45:14 -0400

    new for azure rm

--- azure-rm-create-rdg-file.ps1
+++ azure-rm-create-rdg-file.ps1
@@ -23,85 +23,155 @@
     .\azure-rm-create-rdg-file.ps1 
     Example command to query azure rm for internal and public IaaS ip addresses
 
+.PARAMETER rdgFile
+    path and file name for output .rdg file. default is azure-ms.rdg in script directory
+
 .LINK
     https://www.microsoft.com/en-us/download/confirmation.aspx?id=44989
 #>
 
-$rdgFile = "c:\temp\azure-ms.rdg"
+Param(
+    [string]$rdgFile = ".\azure-ms.rdg"
+)
+
+$ErrorActionPreference = "Stop"
+$profileContext = "$($env:TEMP)\ProfileContext.ctx"
 
 # ----------------------------------------------------------------------------------------------------------------
 function main()
 {
 
-    get-workingDirectory
-
-    # check if we need to sign on
-    authenticate-azureRm
-
-    # Create a new XML File with config root node
-    [XML.XMLDocument]$doc=New-Object XML.XMLDocument
-    $xmlDec = $doc.CreateXmlDeclaration("1.0","utf-8","")
-
-    # set delaration
-    [XML.XMLElement]$root=$doc.DocumentElement
-    $doc.InsertBefore($xmlDec, $root)
-
-    # Append as child to an existing node
-    [XML.XMLElement]$element=$doc.CreateElement("RDCMan")
-    $doc.appendChild($element)
-
-    # Add Attributes
-    $element.SetAttribute("programVersion","2.7")
-    $element.SetAttribute("schemaVersion","3")
-
-
-    [XML.XMLElement]$fileElement=$element.appendChild($doc.CreateElement("file"))
-    [XML.XMLElement]$credElement=$fileElement.appendChild($doc.CreateElement("credentialsProfiles"))
-    [XML.XMLElement]$propertiesElement=$fileElement.appendChild($doc.CreateElement("properties"))
-    [XML.XMLElement]$expandedElement =$propertiesElement.appendChild($doc.CreateElement("expanded"))
-    $expandedElement.InnerText = "True"
-    [XML.XMLElement]$nameElement =$propertiesElement.appendChild($doc.CreateElement("name"))
-    $nameElement.InnerText = "Azure"
-
-    foreach($service in Get-AzureService)
+    try
     {
-        [XML.XMLElement]$groupElement=$fileElement.appendChild($doc.CreateElement("group"))
-        [XML.XMLElement]$propertiesElement=$groupElement.appendChild($doc.CreateElement("properties"))
-        [XML.XMLElement]$expandedElement =$propertiesElement.appendChild($doc.CreateElement("expanded"))
+        $error.Clear()
+        get-workingDirectory
+        $rdgFile = $rdgFile.Replace(".\",(get-location))
+
+        # check if we need to sign on
+        authenticate-azureRm
+
+        # Create a new XML File with config root node
+        [XML.XMLDocument]$doc=New-Object XML.XMLDocument
+        $xmlDec = $doc.CreateXmlDeclaration("1.0","utf-8","")
+
+        # set delaration
+        [XML.XMLElement]$root=$doc.DocumentElement
+        $doc.InsertBefore($xmlDec, $root)
+
+        # Append as child to an existing node
+        [XML.XMLElement]$element=$doc.CreateElement("RDCMan")
+        $doc.appendChild($element)
+
+        # Add Attributes
+        $element.SetAttribute("programVersion","2.7")
+        $element.SetAttribute("schemaVersion","3")
+
+
+        [XML.XMLElement]$fileElement = $element.appendChild($doc.CreateElement("file"))
+        [XML.XMLElement]$credElement = $fileElement.appendChild($doc.CreateElement("credentialsProfiles"))
+        [XML.XMLElement]$propertiesElement = $fileElement.appendChild($doc.CreateElement("properties"))
+        [XML.XMLElement]$expandedElement = $propertiesElement.appendChild($doc.CreateElement("expanded"))
         $expandedElement.InnerText = "True"
-        [XML.XMLElement]$nameElement =$propertiesElement.appendChild($doc.CreateElement("name"))
-        $nameElement.InnerText = $service.ServiceName
+        [XML.XMLElement]$nameElement = $propertiesElement.appendChild($doc.CreateElement("name"))
+        $nameElement.InnerText = "Azure RM $((Get-AzurermSubscription).Id)"
 
-        foreach($vm in Get-AzureVM -ServiceName $service.ServiceName)
+        $allVms = @(Find-AzureRmResource -ResourceType Microsoft.Compute/virtualMachines)
+
+        foreach($resourceGroup in Get-AzureRmResourceGroup)
         {
-        
-            $rdEndpoint = $vm.DNSName.Replace("http://","").Replace("/",":")
-            $rdEndpoint += (Get-AzureEndpoint -VM $vm | ? Name -match 'Remote.*Desktop').Port
+            write-host "resourcegroup:$($resourceGroup.ResourceGroupName)"
+            if(!($allVms.ResourceGroupName -imatch $resourceGroup.ResourceGroupName))
+            {
+                continue
+            }
 
-            [XML.XMLElement]$serverElement=$groupElement.appendChild($doc.CreateElement("server"))
-            [XML.XMLElement]$propertiesElement=$serverElement.appendChild($doc.CreateElement("properties"))
-            [XML.XMLElement]$displayNameElement=$propertiesElement.appendChild($doc.CreateElement("displayName"))
-            $displayNameElement.InnerText = $vm.Name
-            [XML.XMLElement]$nameElement=$propertiesElement.appendChild($doc.CreateElement("name"))
-            $nameElement.InnerText = $rdEndpoint
+            [XML.XMLElement]$groupElement = $fileElement.appendChild($doc.CreateElement("group"))
+            [XML.XMLElement]$propertiesElement = $groupElement.appendChild($doc.CreateElement("properties"))
+            [XML.XMLElement]$expandedElement = $propertiesElement.appendChild($doc.CreateElement("expanded"))
+            $expandedElement.InnerText = "True"
+            [XML.XMLElement]$nameElement = $propertiesElement.appendChild($doc.CreateElement("name"))
+            $nameElement.InnerText = $resourceGroup.ResourceGroupName
+
+            $publicIps = Get-AzureRmPublicIpAddress -ResourceGroupName $resourceGroup.ResourceGroupName
+            $allInterfaces = @(Get-AzureRmNetworkInterface -ResourceGroupName $resourceGroup.ResourceGroupName)
+            
+
+            foreach($interface in $allInterfaces)
+            {
+                write-host "`tinterface:$($interface.Name)"
+                # get vm
+                $vmName = [IO.Path]::GetFileName($interface.VirtualMachine.Id)
+
+                #if(!($allVms.Name -imatch $vmName))
+                #{
+                #    continue
+                #}
+
+                # get private address
+                $privateIpAddresses = @($interface.IpConfigurations.PrivateIpAddress)
+                # get public address
+                $publicIpAddresses = @($interface.IpConfigurations.PublicIpAddress)
+                
+                foreach($privateIpAddress in $privateIpAddresses)
+                {
+                    if(!$privateIpAddress)
+                    {
+                        continue
+                    }
+
+                    [XML.XMLElement]$serverElement = $groupElement.appendChild($doc.CreateElement("server"))
+                    [XML.XMLElement]$propertiesElement = $serverElement.appendChild($doc.CreateElement("properties"))
+                    [XML.XMLElement]$displayNameElement = $propertiesElement.appendChild($doc.CreateElement("displayName"))
+                    $displayNameElement.InnerText = "$($vmName) $($privateIpAddress)"
+                    [XML.XMLElement]$nameElement = $propertiesElement.appendChild($doc.CreateElement("name"))
+                    $nameElement.InnerText = $privateIpAddress
+                }
+
+                foreach($publicIpAddress in $publicIpAddresses)
+                {
+                    if(!$publicIpAddress)# -or !$publicIpAddress.IpAddress)
+                    {
+                        continue
+                    }
+
+                    $publicIpInterfaceName = [IO.Path]::GetFileName($interface.IpConfigurations.publicipaddress.Id)
+                    $publicIp = Get-AzureRmPublicIpAddress -ResourceGroupName $resourceGroup.ResourceGroupName -Name $publicIpInterfaceName
+
+                    [XML.XMLElement]$serverElement = $groupElement.appendChild($doc.CreateElement("server"))
+                    [XML.XMLElement]$propertiesElement = $serverElement.appendChild($doc.CreateElement("properties"))
+                    [XML.XMLElement]$displayNameElement = $propertiesElement.appendChild($doc.CreateElement("displayName"))
+                    $displayNameElement.InnerText = "$($vmName) PUBLIC $($publicIp.IpAddress)"
+                    [XML.XMLElement]$nameElement = $propertiesElement.appendChild($doc.CreateElement("name"))
+                    $nameElement.InnerText = $publicIp.IpAddress
+                }
+            }
+        }
+
+        [XML.XMLElement]$connectedElement = $element.appendChild($doc.CreateElement("connected"))
+        [XML.XMLElement]$favoritesElement = $element.appendChild($doc.CreateElement("favorites"))
+        [XML.XMLElement]$recentlyUsedElement = $element.appendChild($doc.CreateElement("recentlyUsed"))
+    
+        # Save File
+        if(!([IO.Directory]::Exists([IO.Path]::GetDirectoryName($rdgFile))))
+        {
+            [IO.Directory]::CreateDirectory([IO.Path]::GetDirectoryName($rdgFile))
+        }
+
+        $doc.Save($rdgFile)
+        Invoke-Item $rdgFile
+        write-host "finished"
+    }
+    catch
+    {
+        Write-Warning "exception:main:$($error)"
+    }
+    finally
+    {
+        if(test-path $profileContext)
+        {
+            Remove-Item -Path $profileContext -Force
         }
     }
-
-    [XML.XMLElement]$connectedElement=$element.appendChild($doc.CreateElement("connected"))
-    [XML.XMLElement]$favoritesElement=$element.appendChild($doc.CreateElement("favorites"))
-    [XML.XMLElement]$recentlyUsedElement=$element.appendChild($doc.CreateElement("recentlyUsed"))
-    
-    # Save File
-    if(!([IO.Directory]::Exists([IO.Path]::GetDirectoryName($rdgFile))))
-    {
-        [IO.Directory]::CreateDirectory([IO.Path]::GetDirectoryName($rdgFile))
-    }
-
-    $doc.Save($rdgFile)
-
-    Invoke-Item $rdgFile
-
-    write-host "finished"
 }
 # ----------------------------------------------------------------------------------------------------------------
 function authenticate-azureRm()
@@ -198,6 +268,8 @@ function authenticate-azureRm()
             exit 1
         }
     }
+
+    Save-AzureRmContext -Path $profileContext -Force
 }
 
 # ----------------------------------------------------------------------------------------------------------------
@@ -221,13 +293,11 @@ function get-workingDirectory()
     else
     {
         $retVal = (Get-Location).path
-        log-info "get-workingDirectory: Powershell Host $($Host.name) may not be compatible with this function, the current directory $retVal will be used."
+        write-host "get-workingDirectory: Powershell Host $($Host.name) may not be compatible with this function, the current directory $retVal will be used."
         
     } 
  
-    
     Set-Location $retVal | out-null
- 
     return $retVal
 }
 # ----------------------------------------------------------------------------------------------------------------
