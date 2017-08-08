b5ac1377c95a4738172330d816b5390dbfecbd71
Jason Gilbertson <jagilber@microsoft.com>
2017-08-08 02:02:18 -0400

    changed output formatting. no dupes

--- script-azurerm-module-enumerator.ps1
+++ script-azurerm-module-enumerator.ps1
@@ -1,48 +1,87 @@
 ﻿#script to scan other scripts for azure commands to list needed azure modules
 # 170518
 
+[CMDLETBINDING()]
 param(
+[string]$scriptFile,
 [string]$scriptDir
 )
 
 function main()
 {
-    
-    if(!$scriptDir)
+
+    if(!$scriptDir -and !$scriptFile)
     {
         $scriptDir = [IO.Path]::GetDirectoryName($MyInvocation.ScriptName)
     }
-   $files = [Io.Directory]::GetFiles($scriptDir,"*.ps1",[Io.SearchOption]::TopDirectoryOnly)
-   $neededAzureModules = new-object Collections.ArrayList
-   $availableModules = (get-module azurerm.* -ListAvailable)
 
-   foreach($file in $files)
-   {
+    if($scriptFile)
+    {
+        $files = @($scriptFile)
+    }
+    else
+    {
+        $files = [Io.Directory]::GetFiles($scriptDir,"*.ps1",[Io.SearchOption]::TopDirectoryOnly)
+    }
+
+    $neededAzureModules = new-object Collections.ArrayList
+    $availableModules = (get-module azurerm.* -ListAvailable) | Sort-Object -Unique
+
+    foreach($file in $files)
+    {
+        write-host "checking file: $($file)"
         $scriptSource = Get-Content -Raw -Path $file
-
-        foreach($match in ([regex]::Matches($scriptSource, "\W`([a-z]+?-azurerm[a-z]+?`)\W",[Text.RegularExpressions.RegexOptions]::IgnoreCase)))
+        $matches = ([regex]::Matches($scriptSource, "\W`([a-z]+?-azurerm[a-z]+?`)\W",[Text.RegularExpressions.RegexOptions]::IgnoreCase))
+        
+        foreach($match in ($matches| Sort-Object -Unique))
         {
-            $matchedValue = $match.Captures[0].Groups[1].Value
+            $matchedValue = $match.Captures[0].Groups[1].Value 
             
+            write-host "`t`tazure command: $($matchedValue)"
+            $module = $null
             foreach($availableModule in $availableModules)
             {
-                if($availableModule.ExportedCommands.Keys -imatch $matchedValue)
+                write-verbose "`t`t`tavailable module: $($availableModule)"
+
+                #if($availableModule.ExportedCommands.Keys -imatch $matchedValue)
+                if($availableModule.ExportedCmdlets.Keys -imatch $matchedValue)
                 {
                     $module = $availableModule.Name
 
                     if($module)
                     {
+                        write-verbose "`t`t`t`tchecking if module in list: $($module)"
                         if(!$neededAzureModules.Contains($module))
                         {
+                            write-host "`t`t`t`t`tadding module to list: $($module)" -ForegroundColor Green
                             [void]$neededAzureModules.Add($module)
                             continue
                         }
+                        else
+                        {
+                            write-host "`t`t`t`t`tmodule already in list: $($module)" -ForegroundColor DarkGray
+                        }
+
                     }
                 }
             }
+
+            if(!$module)
+            {
+                Write-Warning "unable to find module for command $($matchedValue)"
+            }
         }
 
-        write-host "needed modules for file: $($file)`r`n$($neededAzureModules | format-list * | out-string)"
+        if($neededAzureModules.Count -gt 0)
+        {
+            write-host "needed modules for file: $($file)"
+            write-host "$($neededAzureModules | format-list * | out-string)" -ForegroundColor Magenta
+        }
+        else
+        {
+            write-host "NO azure modules needed for file: $($file)" -ForegroundColor Gray
+        }
+
         $neededAzureModules.Clear()
         write-host "-----------------------------------------"
     }
