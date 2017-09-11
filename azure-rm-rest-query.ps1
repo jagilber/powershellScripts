38820c637b9b87312f606f0cca4af060760b8885
jason gilbertson <jagilber@microsoft.com>
2017-09-11 18:31:33 -0400

    sf rest testing

--- azure-rm-rest-query.ps1
+++ azure-rm-rest-query.ps1
@@ -4,7 +4,9 @@ $baseURI = "https://management.azure.com"
 $suffixURI = "?api-version=2016-09-01" 
 $SubscriptionURI = $baseURI + "/subscriptions/$($SubscriptionID)" + $suffixURI
 $uri = $SubscriptionURI 
-
+ $Body = @{
+        'client_id'     = $applicationId
+    }
 $params = @{ 
     ContentType = 'application/x-www-form-urlencoded'
     Headers     = @{
@@ -12,6 +14,7 @@ $params = @{
     }
     Method      = 'Get' 
     uri         = $uri
+    Body = $Body
 } 
 $response = Invoke-RestMethod @params 
 $response | convertto-json 
---
+++ azure-rm-sf-rest-query.ps1
@@ -0,0 +1,52 @@
+﻿<#
+# https://docs.microsoft.com/en-us/rest/api/servicefabric/sfrp-api-clusters_get
+#/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.ServiceFabric/clusters/{clusterName}?api-version=2016-09-01
+#>
+
+param(
+[object]$token = $global:token,
+[string]$SubscriptionID = "$((Get-AzureRmSubscription).SubscriptionId)",
+[string]$baseURI = "https://management.azure.com" ,
+[string]$suffixURI = "?api-version=2016-09-01" ,
+[string]$resourceGroup,
+[string]$clusterName
+)
+
+
+if($resourceGroup -and $clusterName)
+{
+    [string]$SubscriptionURI = $baseURI + "/subscriptions/$($SubscriptionID)/resourceGroups/$($resourceGroup)/providers/Microsoft.ServiceFabric/clusters/$($clusterName)" + $suffixURI
+}
+else
+{
+    [string]$SubscriptionURI = $baseURI + "/subscriptions/$($SubscriptionID)/providers/Microsoft.ServiceFabric/clusters" + $suffixURI
+}
+
+
+$uri = $SubscriptionURI 
+$uri 
+
+
+ $Body = @{
+        'client_id'     = $applicationId
+    }
+$params = @{ 
+    ContentType = 'application/x-www-form-urlencoded'
+    Headers     = @{
+        'authorization' = "Bearer $($token.access_token)" 
+    }
+    Method      = 'Get' 
+    uri         = $uri
+    Body = $Body
+} 
+
+$params
+$params.Body.client_id
+$params.Headers.authorization
+
+$response = Invoke-RestMethod @params 
+$response | convertto-json 
+
+$global:response = $response
+$global:response
+$global:response.value.properties
