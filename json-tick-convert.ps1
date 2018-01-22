e06a478f404b999d62edb4280574caaa2f5d6884
jason gilbertson <jagilber@microsoft.com>
2018-01-22 09:57:31 -0500

    add json tick convert

---
+++ json-tick-convert.ps1
@@ -0,0 +1,24 @@
+﻿# script to read sf json files and add comment for convert ticks to timestamp
+
+param(
+    $inputFile = "F:\cases\118010717420117\jarvis-nodes-of-a-cluster-180120.1.json"
+)
+
+clear-host
+$lines = @([io.file]::ReadAllText($inputFile) -split "\r\n")
+
+foreach($line in $lines)
+{
+    #look for ticks 131607604784229491   
+    $pattern = "[0-9]{18}"
+    if([regex]::IsMatch($line, $pattern))
+    {
+        $line -match $pattern | out-null
+        $date = (new-object datetime($matches[0].ToString())).ToString("yyMMdd--HH:mm:ss")
+        write-host "$($line) // timestamp:$($date)"
+    }
+    else
+    {
+        Write-Host $line
+    }
+}
