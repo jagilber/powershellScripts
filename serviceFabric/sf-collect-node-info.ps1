ae71112b6fa0e92afed7988faecec63b3624e45f
jason gilbertson <jagilber@microsoft.com>
2018-08-13 20:33:15 -0400

    add defined event logs list

--- serviceFabric/sf-collect-node-info.ps1
+++ serviceFabric/sf-collect-node-info.ps1
@@ -1,12 +1,11 @@
 # (new-object net.webclient).downloadfile("https://raw.githubusercontent.com/jagilber/powershellScripts/master/serviceFabric/sf-collect-node-info.ps1","c:\sf-collect-node-info.ps1")
-
 # Run the following from each sfnode in admin powershell:
 
 param(
     $workdir = "c:\temp",
     $startTime = (get-date).AddDays(-5).ToShortDateString(),
     $endTime = (get-date).ToShortDateString(),
-    $eventLogNames = "*",
+    $eventLogNames = "System|Application|Fabric|http|Firewall|Azure",
     [switch]$eventLogs
 )
 
@@ -29,30 +28,31 @@ if ((test-path $workdir))
 new-item $workdir -ItemType Directory
 Set-Location $parentworkdir
 
+if($win10)
+{
+    Get-WindowsUpdateLog -LogPath "$($workdir)\windowsupdate.txt"
+}
+else
+{
+    copy-item "$env:systemroot\windowsupdate.txt" "$($workdir)\windowsupdate.txt"
+}
+
+get-hotfix | out-file "$($workdir)\hotfixes.txt"
+$osVersion.tostring() | out-file "$($workdir)\os.txt"
+Get-psdrive | out-file "$($workdir)\drives.txt"
+Get-process | out-file "$($workdir)\pids.txt"
+Get-Service | out-file "$($workdir)\services.txt"
+start-process "powershell.exe" -ArgumentList "reg.exe export HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy\FirewallRules $($workDir)\firewallrules.reg"
+start-process "powershell.exe" -ArgumentList "netstat -bna > $($workdir)\netstat.txt"
+start-process "powershell.exe" -ArgumentList "netsh http show sslcert > $($workdir)\netshssl.txt"
+start-process "powershell.exe" -ArgumentList "ipconfig /all > $($workdir)\ipconfig.txt"
+
 if ($eventLogs)
 {
     (new-object net.webclient).downloadfile("http://aka.ms/event-log-manager.ps1", "$($parentWorkdir)\event-log-manager.ps1")
     $args = "$($parentWorkdir)\event-log-manager.ps1 -eventLogNamePattern $($eventlognames) -eventStartTime $($startTime) -eventStopTime $($endTime) -eventDetails -merge -uploadDir $($workdir)"
-    start-process -filepath "powershell.exe" -ArgumentList $args
+    start-process -filepath "powershell.exe" -ArgumentList $args -Wait
 }
-
-if($win10)
-{
-    Get-WindowsUpdateLog -LogPath "$($workdir)\windowsupdate.log"
-}
-else
-{
-    copy-item "$env:systemroot\windowsupdate.log" "$($workdir)\windowsupdate.log"
-}
-
-get-hotfix | out-file "$($workdir)\hotfixes.log"
-$osVersion.tostring() | out-file "$($workdir)\os.log"
-Get-psdrive | out-file "$($workdir)\drives.log"
-Get-process | out-file "$($workdir)\pids.log"
-start-process "powershell.exe" -ArgumentList "reg.exe export HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy\FirewallRules $($workDir)\firewallrules.reg"
-start-process "powershell.exe" -ArgumentList "netstat -bna > $($workdir)\netstat.log"
-start-process "powershell.exe" -ArgumentList "netsh http show sslcert > $($workdir)\netshssl.log"
-start-process "powershell.exe" -ArgumentList "ipconfig /all > $($workdir)\ipconfig.log"
 
 if ((test-path "$($workdir).zip"))
 {
