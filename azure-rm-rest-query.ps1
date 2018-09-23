ffc5b613906b49d7d4f635e77d81392143fbaa8e
Jason Gilbertson <jagilber@users.noreply.github.com>
2018-09-23 08:40:58 -0400

    update

---
+++ azure-rm-rest-query.ps1
@@ -0,0 +1,23 @@
+﻿$token = $Global:token
+$SubscriptionID = (Get-AzureRmSubscription).Id
+$baseURI = "https://management.azure.com" 
+$suffixURI = "?api-version=2016-09-01" 
+$SubscriptionURI = $baseURI + "/subscriptions/$($SubscriptionID)" + $suffixURI
+$uri = $SubscriptionURI 
+ $Body = @{
+        'client_id'     = $applicationId
+    }
+$params = @{ 
+    ContentType = 'application/x-www-form-urlencoded'
+    Headers     = @{
+        'authorization' = "Bearer $($Token.access_token)" 
+        'accept' = 'application/json'
+    }
+    Method      = 'Get' 
+    uri         = $uri
+    Body = $Body
+} 
+$response = Invoke-RestMethod @params -Verbose -Debug
+$response
+$global:response = $response
+$global:response.value.properties | ConvertTo-Json
