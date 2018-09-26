d3ed00a784cea60193f947f9d2b3d6b7842801e6
jason gilbertson <jagilber@microsoft.com>
2018-09-26 13:52:04 -0400

    fix applicationid/clientid arg

--- azure-rm-download-deployment-templates.ps1
+++ azure-rm-download-deployment-templates.ps1
@@ -35,42 +35,42 @@ function main()
     Get-AzureRmDeployment | Save-AzureRmDeploymentTemplate -Path $outputDir -Force
     set-location $outputDir
 
-    if($useGit -and !(git))
+    if ($useGit -and !(git))
     {
         write-host "git not installed"
         $useGit = $false
         $error.Clear()
     }
     
-    if($useGit -and !(git status))
+    if ($useGit -and !(git status))
     {
         git init
     }
 
-    if($resourceGroups.Count -lt 1)
+    if ($resourceGroups.Count -lt 1)
     {
         $resourceGroups = @((Get-AzureRmResourceGroup).ResourceGroupName)
     }
 
 
-    if(!$currentOnly)
+    if (!$currentOnly)
     {
         $deployments = (Get-AzureRmResourceGroup) | Where-Object ResourceGroupName -imatch ($resourceGroups -join "|") | Get-AzureRmResourceGroupDeployment
 
-        foreach($dep in ($deployments | sort-object -Property Timestamp))
+        foreach ($dep in ($deployments | sort-object -Property Timestamp))
         {
             $rg = $dep.ResourceGroupName
             $rgDir = "$($outputDir)\$($rg)"
             New-Item -ItemType Directory $rgDir -ErrorAction SilentlyContinue
 
             $baseFile = "$($rgDir)\$($dep.deploymentname)"
-            Save-AzureRmResourceGroupDeploymentTemplate -Path "$($baseFile).template.json" -ResourceGroupName $rg -DeploymentName ($dep.DeploymentName) -Force
-            out-file -Encoding ascii -InputObject ((convertto-json ($dep.Parameters) -Depth 99).Replace("    "," ")) -FilePath "$($baseFile).parameters.json" -Force
+            Save-AzureRmResourceGroupDeploymentTemplate -Path $baseFile -ResourceGroupName $rg -DeploymentName ($dep.DeploymentName) -Force
+            out-file -Encoding ascii -InputObject ((convertto-json ($dep.Parameters) -Depth 99).Replace("    ", " ")) -FilePath "$($baseFile).parameters.json" -Force
 
             $operations = Get-AzureRmResourceGroupDeploymentOperation -DeploymentName $($dep.DeploymentName) -ResourceGroupName $rg
-            out-file -Encoding ascii -InputObject ((convertto-json $operations -Depth 99).Replace("    "," ")) -FilePath "$($baseFile).operations.json" -Force
+            out-file -Encoding ascii -InputObject ((convertto-json $operations -Depth 99).Replace("    ", " ")) -FilePath "$($baseFile).operations.json" -Force
 
-            if($useGit)
+            if ($useGit)
             {
                 git add -A
                 git commit -a -m "$($rg) $($dep.deploymentname) $($dep.TimeStamp) $($dep.ProvisioningState)`n$($dep.outputs | fl * | out-string)" --date (($dep.TimeStamp).ToString("o"))
@@ -78,55 +78,55 @@ function main()
         }
     }
 
-    if($getCurrentConfig)
+    if ($getCurrentConfig)
     {
-        if(!(test-path $restLogonScript))
+        if (!(test-path $restLogonScript))
         {
             get-update -destinationFile $restLogonScript -updateUrl "$($repo)$([io.path]::GetFileName($restLogonScript))"
         }
       
-        if(!(test-path $restQueryScript))
+        if (!(test-path $restQueryScript))
         {
             get-update -destinationFile $restQueryScript -updateUrl "$($repo)$([io.path]::GetFileName($restQueryScript))"
         }
 
         $global:token = Invoke-Expression "$($restLogonScript) -clientSecret $($clientSecret) -applicationId $($clientId)" 
         $global:token
-   }
+    }
     
-    if($useGit -and !(git status))
+    if ($useGit -and !(git status))
     {
         git init
     }
 
-    if($resourceGroups.Count -lt 1)
+    if ($resourceGroups.Count -lt 1)
     {
         $resourceGroups = @((Get-AzureRmResourceGroup).ResourceGroupName)
     }
 
-    foreach($rg in $resourceGroups)
+    foreach ($rg in $resourceGroups)
     {
         $rgDir = "$($outputDir)\$($rg)"
         New-Item -ItemType Directory $rgDir -ErrorAction SilentlyContinue
-        if($getCurrentConfig)
+        if ($getCurrentConfig)
         {   
             $currentConfig = get-currentConfig -rg $rg
             #out-file -InputObject (convertto-json (get-currentConfig -rg $rg)) -FilePath "$($rgDir)\current.json" -Force
-            out-file -InputObject ([Text.RegularExpressions.Regex]::Unescape((convertto-json ($currentConfig.template)))) -FilePath "$($rgDir)\current.json" -Force
+            out-file -InputObject ([Text.RegularExpressions.Regex]::Unescape((convertto-json ($currentConfig.template) -Depth 99))) -FilePath "$($rgDir)\current.json" -Force
 
-            if($currentConfig.error)
+            if ($currentConfig.error)
             {
-                out-file -InputObject ([Text.RegularExpressions.Regex]::Unescape((convertto-json ($currentConfig.error)))) -FilePath "$($rgDir)\current.errors.json" -Force
+                out-file -InputObject ([Text.RegularExpressions.Regex]::Unescape((convertto-json ($currentConfig.error) -Depth 99))) -FilePath "$($rgDir)\current.errors.json" -Force
             }
         }
 
-        foreach($dep in (Get-AzureRmResourceGroupDeployment -ResourceGroupName $rg))
+        foreach ($dep in (Get-AzureRmResourceGroupDeployment -ResourceGroupName $rg))
         {
             Save-AzureRmResourceGroupDeploymentTemplate -Path $rgDir -ResourceGroupName $rg -DeploymentName ($dep.DeploymentName) -Force
-            out-file -Encoding ascii -InputObject (convertto-json $dep.Parameters) -FilePath "$($rgDir)\$($dep.deploymentname).parameters.json" -Force
+            out-file -Encoding ascii -InputObject (convertto-json ($dep.Parameters) -Depth 99) -FilePath "$($rgDir)\$($dep.deploymentname).parameters.json" -Force
             out-file -Encoding ascii -InputObject (convertto-json (Get-AzureRmResourceGroupDeploymentOperation -DeploymentName $($dep.DeploymentName) -ResourceGroupName $rg) -Depth 99) -FilePath "$($rgDir)\$($dep.deploymentname).operations.json" -Force
             
-            if($useGit)
+            if ($useGit)
             {
                 git add -A
                 git commit -a -m "$($rg) $($dep.deploymentname)) $($dep.TimeStamp) $($dep.ProvisioningState)`n$($outputs | out-string)" --date (($dep.TimeStamp).ToString("o"))
@@ -135,7 +135,7 @@ function main()
         }    
     }
 
-    if(!$getCurrentConfig)
+    if (!$getCurrentConfig)
     {
         write-warning "this information does *not* include the currently running confiruration, only the last deployments. example no changes made in portal after deployment"
         write-host "to get the current running configuration ('automation script' in portal), use portal, or"
@@ -162,7 +162,7 @@ function get-update($updateUrl, $destinationFile)
 
         # git may not have carriage return
         # reset by setting all to just lf
-        $git = [regex]::Replace($git, "`r`n","`n")
+        $git = [regex]::Replace($git, "`r`n", "`n")
         # add cr back
         $git = [regex]::Replace($git, "`n", "`r`n")
 
@@ -201,9 +201,9 @@ function get-currentConfig($rg)
     write-host $command 
     $results = invoke-expression $command
 
-    if($results.error)
+    if ($results.error)
     {
-        write-warning (convertto-json ($results.error))
+        write-warning (convertto-json ($results.error) -depth 99)
     }
     
     return $results
@@ -211,33 +211,33 @@ function get-currentConfig($rg)
 
 function check-authentication()
 {
-        # authenticate
+    # authenticate
+    try
+    {
+        $tenants = @(Get-AzureRmTenant)
+                        
+        if ($tenants)
+        {
+            write-host "auth passed $($tenants.Count)"
+        }
+        else
+        {
+            write-host "auth error $($error)" -ForegroundColor Yellow
+            exit 1
+        }
+    }
+    catch
+    {
         try
         {
-            $tenants = @(Get-AzureRmTenant)
-                        
-            if ($tenants)
-            {
-                write-host "auth passed $($tenants.Count)"
-            }
-            else
-            {
-                write-host "auth error $($error)" -ForegroundColor Yellow
-                exit 1
-            }
+            Add-AzureRmAccount
         }
         catch
         {
-            try
-            {
-                Add-AzureRmAccount
-            }
-            catch
-            {
-                write-host "exception authenticating. exiting $($error)" -ForegroundColor Yellow
-                exit 1
-            }
+            write-host "exception authenticating. exiting $($error)" -ForegroundColor Yellow
+            exit 1
         }
+    }
 }
 
 try
