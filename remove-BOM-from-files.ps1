bb11a029095b0cb61377ec1d08f35d358a934854
jason gilbertson <jagilber@microsoft.com>
2018-09-30 13:42:24 -0400

    cleanup

--- remove-BOM-from-files.ps1
+++ remove-BOM-from-files.ps1
@@ -1,7 +1,31 @@
+<#
 # cleans BOM from start of file in utf encoded files.
 # BOM affects use of iwr %script% | iex
 # this will convert files with BOM to utf-8 with no BOM
 # https://stackoverflow.com/questions/5596982/using-powershell-to-write-a-file-in-utf-8-without-the-bom
+
+example from iwr script download output
+
+PS C:\Users\cloudadmin> $t = iwr "http://aka.ms/directory-treesize.ps1"
+PS C:\Users\cloudadmin> $t | iex
+iex : At line:58 char:7
++     do not display output
+
+PS C:\Users\cloudadmin> $t | fl *
+Content           : {239, 187, 191, 60...}
+StatusCode        : 200
+StatusDescription : OK
+RawContentStream  : Microsoft.PowerShell.Commands.WebResponseContentMemoryStream
+RawContentLength  : 18585
+RawContent        : HTTP/1.1 200 OK
+...
+                    X-Powered-By: ASP.NET
+
+issue------>>>>     ???<#
+                    .SYNOPSIS
+                        powershell script to to enumerate directory summarizing in tree view directories over a given
+                    size
+#>
 
 param(
     $path = (get-location).path,
@@ -10,7 +34,6 @@ param(
 )
 
 $Utf8NoBom = New-Object Text.UTF8Encoding($False)
-$enc = [system.Text.Encoding]::UTF8
 
 function main()
 {
