b2710f7aae194aa4872a41b5262b4f6be93bd9f7
jason gilbertson <jagilber@microsoft.com>
2019-01-30 12:32:03 -0500

    update

--- serviceFabric/azure-rm-sf-rest-query.1.ps1
+++ serviceFabric/azure-rm-sf-rest-query.1.ps1
@@ -11,11 +11,11 @@ param(
     [string]$nodeTypeApiVersion = "?api-version=2018-06-01",
     [string]$resourceGroup,
     [string]$script:clusterName,
-    [string]$contentType = "application/json"
+    [string]$contentType = "application/json",
+    [bool]$verify = $true
 )
 
 $global:response = $null
-$script:headers = $null
 $params = $null
 $geturi = $null
 
@@ -29,18 +29,16 @@ $script:clientCertificateCommonNames = $null
 $script:reverseProxyCertificate = $null
 $script:sfnodeTypes = $null
 $script:nodeTypes = $null
-$script:nodeTypeExtensions = $null
+#$script:nodeTypeExtensions = $null
 $script:resourceGroup = $resourceGroup
+#$script:sfExtensionCertInfo = $null
+#$script:sfExtensionReverseProxyCertInfo = $null
+$script:vmExtensions = [collections.arraylist]@()
+$script:vmProfiles = [collections.arraylist]@()
 
 function main()
 {
     
-    $script:headers = @{
-        'authorization' = "Bearer $($token.access_token)" 
-        'accept'        = "*/*"
-        'ContentType'   = $contentType
-    }
-
     if (!(get-clusterInfo))
     {
         return 1
@@ -48,10 +46,15 @@ function main()
 
     foreach($nodeType in $script:sfnodeTypes)
     {
-        if (!(get-nodeTypes -nodeTypeName ($nodeType.Name)))
+        if (!(get-nodeTypInfo -nodeTypeName ($nodeType.Name)))
         {
             return 1
         }
+    }
+
+    if($verify -and !(verify-certConfig))
+    {
+
     }
 }
 
@@ -70,14 +73,7 @@ function get-clusterInfo()
     $geturi 
 
     # get
-    $params = @{ 
-        ContentType = $contentType
-        Headers     = $script:headers
-        Method      = "get"
-        uri         = $geturi
-    } 
-
-    $script:clustersResponse = (invoke-rest $params)
+    $script:clustersResponse = invoke-rest $geturi
     write-host "existing clusters:"
     $script:clusters = @($script:clustersResponse.value)
 
@@ -87,11 +83,11 @@ function get-clusterInfo()
         return $false
     }
 
-    $count = 0
+    $count = 1
     foreach ($script:cluster in $script:clusters)
     {
-        $count++
         write-host "$($count). $($script:cluster.Name)"
+        $count++
     }
     
     if (($number = read-host "enter number of the cluster to query or ctrl-c to exit:") -le $script:clusters.length)
@@ -102,12 +98,12 @@ function get-clusterInfo()
         write-host $script:resourceGroup
     }
 
-    write-host ($script:cluster | ConvertTo-Json)
+    write-verbose ($script:cluster | ConvertTo-Json)
     write-host ($script:cluster.properties)
 
     write-host "cluster cert:" -ForegroundColor Yellow
     $script:clusterCert = $script:cluster.properties.certificate
-    write-host ($script:clusterCert)
+    write-host ($script:clusterCert  | convertto-json)
     write-host "cluster cert primary thumbprint:" -ForegroundColor Yellow
     $script:clusterCertThumbprintPrimary = $script:clusterCert.thumbprint
     write-host $script:clusterCertThumbprintPrimary
@@ -117,38 +113,32 @@ function get-clusterInfo()
 
     write-host "client cert thumbs:" -ForegroundColor Yellow
     $script:clientCertificateThumbprints = $script:cluster.properties.clientCertificateThumbprints
-    write-host ($script:clientCertificateThumbprints)
+    write-host ($script:clientCertificateThumbprints | convertto-json)
     write-host "client cert common names:" -ForegroundColor Yellow
     $script:clientCertificateCommonNames = $script:cluster.properties.clientCertificateCommonNames
-    write-host ($script:clientCertificateCommonNames)
+    write-host ($script:clientCertificateCommonNames | convertto-json)
 
     write-host "reverse proxy cert:" -ForegroundColor Yellow
     $script:reverseProxyCertificate = $script:cluster.properties.reverseProxyCertificate
-    write-host ($script:reverseProxyCertificate)
+    write-host ($script:reverseProxyCertificate | convertto-json)
 
-    write-host "nodetypes:" -ForegroundColor Yellow
+    write-host "sf nodetypes:" -ForegroundColor Yellow
     $script:sfnodeTypes = $script:cluster.properties.nodeTypes
-    write-host ($script:sfnodeTypes)
+    write-verbose ($script:sfnodeTypes | convertto-json)
+    write-host ($script:sfnodeTypes| select name,vmInstanceCount,isPrimary | convertto-json -Depth 1)
 }
 
-function get-nodeTypes($nodeTypeName)
+function get-nodeTypeInfo($nodeTypeName)
 {
 
     $geturi = $baseURI + "/subscriptions/$($SubscriptionID)/resourceGroups/$($script:resourceGroup)/providers/Microsoft.Compute/virtualMachineScaleSets/$($nodeTypeName)"
     write-host $geturi 
     
-    # get
-    $params = @{ 
-        ContentType = $contentType
-        Headers     = $script:headers
-        Method      = "get"
-        uri         = $geturi + $nodeTypeApiVersion
-    } 
-
-    $nodeTypesResponse = (invoke-rest $params)
-    write-host " nodetypes:" -ForegroundColor Green
+    $nodeTypesResponse = (invoke-rest ($geturi + $nodeTypeApiVersion))
+    write-host "nodetypes:" -ForegroundColor Green
     $script:nodeTypes = @($nodeTypesResponse)
-    write-host ($script:nodeTypes)
+    write-host ($script:nodeTypes| select name,id,tags | convertto-json -Depth 1)
+    write-verbose ($script:nodeTypes | convertto-json -Depth 1)
 
     if ($nodeTypes.length -lt 1)
     {
@@ -158,6 +148,26 @@ function get-nodeTypes($nodeTypeName)
 
     foreach($nodeType in $script:nodeTypes)
     {
+
+        $vmProfile = $nodeType.properties.virtualMachineProfile
+        $script:vmProfiles.Add($vmProfile)
+        $osProfile = $vmProfile.osProfile
+        $script:nodeTypeSecrets = $osProfile.secrets
+        
+        write-verbose ($script:nodeTypeSecrets | convertto-json -Depth 5)
+        write-host "nodetype secrets info:" -ForegroundColor Green
+        $count = 1
+
+        foreach($secret in $script:nodeTypeSecrets)
+        {
+            write-host "nodetype secret $count" -ForegroundColor Green
+            write-host "nodetype secret source vault $count" -ForegroundColor Green
+            write-host $secret.sourceVault.id
+            write-host "nodetype secret source vault certificates $count" -ForegroundColor Green
+            write-host ($secret.vaultCertificates | convertto-json)
+            $count++
+        }
+
         get-nodeTypeExtensions $nodeType.Name
     }
 
@@ -167,36 +177,46 @@ function get-nodeTypeExtensions($nodeTypeName)
 {
     $geturi = $baseURI + "/subscriptions/$($SubscriptionID)/resourceGroups/$($script:resourceGroup)/providers/Microsoft.Compute/virtualMachineScaleSets/$($nodeTypeName)/extensions"
     write-host $geturi 
-    
-    # get
-    $params = @{ 
-        ContentType = $contentType
-        Headers     = $script:headers
-        Method      = "get"
-        uri         = $geturi + $nodeTypeApiVersion
-    } 
+    $nodeTypesResponse = (invoke-rest $geturi)
+    write-verbose "nodetype extensions:"
+    $nodeTypeExtensions = @($nodeTypesResponse)
+    write-verbose ($nodeTypeExtensions.value.properties | convertto-json)
 
-    $nodeTypesResponse = (invoke-rest $params)
-    write-host " nodetype extensions:" -ForegroundColor Green
-    $script:nodeTypeExtensions = @($nodeTypesResponse)
-    write-host ($script:nodeTypeExtensions)
-
-    if ($script:nodeTypeExtensions.length -lt 1)
+    if ($nodeTypeExtensions.length -lt 1)
     {
         write-host "unable to enumerate nodetype extensions. exiting"
         return $false
     }
 
-    foreach($nodeTypeExtension in $script:nodeTypeExtensions)
-    {
-        write-host ($nodeTypeExtension | convertto-json -Depth 5)
-    }
-
+    $sfExtension = $nodeTypeExtensions.value.properties |where-object type -imatch 'ServiceFabricNode'
+    $script:vmExtensions.Add($sfExtension)
+    $sfExtensionSettings = $sfExtension.settings
+    $sfExtensionCertInfo = $sfExtensionSettings.certificate
+    $sfExtensionReverseProxyCertInfo = $sfExtensionSettings.reverseProxycertificate
+    write-host "nodetype sf extension certificate info:" -ForegroundColor Green
+    write-host $sfExtensionSettings.nodeTypeRef
+    write-host "nodetype sf extension certificate:" -ForegroundColor Green
+    write-host ($sfExtensionCertInfo | convertto-json)
+    write-host "nodetype sf extension reverse proxy certificate:" -ForegroundColor Green
+    write-host ($sfExtensionReverseProxyCertInfo | convertto-json)
 }
 
 
-function invoke-rest($params)
+function invoke-rest($uri)
 {
+    $script:headers = @{
+        'authorization' = "Bearer $($token.access_token)" 
+        'accept'        = "*/*"
+        'ContentType'   = $contentType
+    }
+
+    $params = @{ 
+        ContentType = $contentType
+        Headers     = $script:headers
+        Method      = "get"
+        uri         = $uri
+    } 
+
     write-host $params
     $error.Clear()
     $response = Invoke-RestMethod @params -Verbose -Debug
@@ -205,6 +225,21 @@ function invoke-rest($params)
     write-host $error
     $global:response = $response
     return $response
+}
+
+function verify-certConfig()
+{
+    $retVal = $false
+    write-host "checking cert configuration"
+    # check local store and connect to cluster if cert exists?
+    # check certs on nodes?
+    # check cert expiration?
+    # check cert CA?
+    write-host "checking cert configuration key vault"
+    write-host "checking cert configuration key vault certificates"
+    write-host "checking cert configuration sf <-> vmss "
+
+    return $retVal
 }
 
 main
