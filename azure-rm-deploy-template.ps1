11b7bfa238ffdd04aec55137fa8455d2a3739d1b
Jason Gilbertson <jagilber@microsoft.com>
2019-02-07 12:51:33 -0500

    fix false error

--- azure-rm-deploy-template.ps1
+++ azure-rm-deploy-template.ps1
@@ -117,11 +117,11 @@ catch [management.automation.commandNotFoundException]
     }
 }
 
-if (!(Get-AzureRmResource | Out-Null))
+if (!(Get-AzureRmResource))
 {
     connect-azurermaccount
 
-    if (!(Get-AzureRmResource | Out-Null))
+    if (!(Get-AzureRmResource))
     {
         Write-Warning "unable to authenticate to azurerm. returning..."
         return 1
