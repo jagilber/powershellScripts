8e9337a94d1a0e8a7bbc7fa63c8890a6647d8b1d
Jason Gilbertson <jagilber@users.noreply.github.com>
2019-02-24 23:41:52 -0500

    add resolve-path

---
+++ remote-manager/resolve-path.ps1
@@ -0,0 +1,32 @@
+param(
+    $item = (get-location).path
+)
+
+function resolve-path($item)
+{
+    write-host "resolving $item"
+    $item = [environment]::ExpandEnvironmentVariables($item)
+    $sepChar = [io.path]::DirectorySeparatorChar
+
+    if($result = Get-Item $item -ErrorAction SilentlyContinue)
+    {
+        return $result
+    }
+
+    $paths = [collections.arraylist]@($env:Path.Split(";"))
+    [void]$paths.Add([io.path]::GetDirectoryName($MyInvocation.ScriptName))
+
+    foreach ($path in $paths)
+    {
+        if($result = Get-Item ($path.trimend($sepChar) + $sepChar + $item.trimstart($sepChar)) -ErrorAction SilentlyContinue)
+        {
+            return $result
+        }
+    }
+
+    Write-Warning "unable to find $item"
+    return $null
+}
+
+$result = resolve-path $item
+write-host "result $result"
