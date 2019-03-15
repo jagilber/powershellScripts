aa700bf26a93833fdc867286301acaaa202e83d4
Jason Gilbertson <jagilber@users.noreply.github.com>
2019-03-15 11:10:50 -0400

    testing

--- temp/cert.ps1
+++ temp/cert.ps1
@@ -59,7 +59,9 @@ log "logging onto azure account with app id = $appId ..."
 
 $creds = new-object Management.Automation.PSCredential ($appId, (convertto-securestring $appPassword -asplaintext -force))
 ## todo remove after test
-#login-azurermaccount -credential $creds -serviceprincipal -tenantid $tenantId -confirm:$false
+login-azurermaccount -credential $creds -serviceprincipal -tenantid $tenantId -confirm:$false
+
+log "rg: $(get-azurermresourcegroup)"
 
 #  get the secret from key vault
 #
