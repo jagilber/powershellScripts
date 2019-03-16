c3caed27b3680057007534dced56bde56d71bb4b
Jason Gilbertson <jagilber@users.noreply.github.com>
2019-03-16 00:07:28 -0400

    testing

---
+++ serviceFabric/azure-rm-sf-standalone-install.ps1
@@ -0,0 +1,168 @@
+<#
+https://serverfault.com/questions/642253/how-are-windows-machinekey-container-file-names-derived
+ script to install service fabric standalone in azure arm
+ # https://docs.microsoft.com/en-us/azure/service-fabric/service-fabric-cluster-creation-for-windows-server
+
+ <#
+    The CleanCluster.ps1 will clean these certificates or you can clean them up using script 'CertSetup.ps1 -Clean -CertSubjectName CN=ServiceFabricClientCert'.
+    Server certificate is exported to C:\temp\Microsoft.Azure.ServiceFabric.WindowsServer.latest\Certificates\server.pfx with the password 1230909376
+    Client certificate is exported to C:\temp\Microsoft.Azure.ServiceFabric.WindowsServer.latest\Certificates\client.pfx with the password 940188492
+    Modify thumbprint in C:\temp\Microsoft.Azure.ServiceFabric.WindowsServer.latest\ClusterConfig.X509.OneNode.json
+#>
+# https://github.com/Azure/AzureStack-QuickStart-Templates/blob/master/201-vm-windows-pushcertificate/azuredeploy.json
+# query keyvault during deployment
+# https://docs.microsoft.com/en-us/azure/azure-resource-manager/resource-manager-keyvault-parameter
+#-a--s-        3/15/2019   3:39 PM           1484 fa79c1fffdece54198063e1594c2913e_5cb78751-8a89-4086-8f76-ac082a5aced6 
+#>
+
+
+param(
+    [switch]$remove,
+    [switch]$force,
+    [string]$configurationFile = ".\ClusterConfig.X509.OneNode.json", # ".\ClusterConfig.X509.MultiMachine.json", #".\ClusterConfig.Unsecure.DevCluster.json",
+    [string]$packageUrl = "https://go.microsoft.com/fwlink/?LinkId=730690",
+    [string]$packageName = "Microsoft.Azure.ServiceFabric.WindowsServer.latest.zip",
+    [int]$timeout = 1200,
+    [string]$diagnosticShare,
+    [string]$thumbprint,
+    [string[]]$nodes
+)
+
+function main()
+{
+    $VerbosePreference = $DebugPreference = "continue"
+    $Error.Clear()
+    $scriptPath = ([io.path]::GetDirectoryName($MyInvocation.ScriptName))
+    $packagePath = "$(get-location)\$([io.path]::GetFileNameWithoutExtension($packageName))"
+    #$downloadPath = "$packagePath\Download"
+    $certPath = "$packagePath\Certificates"
+    Start-Transcript -Path "$scriptPath\install.log"
+    $currentLocation = (get-location).Path
+    $configurationFileMod = "$([io.path]::GetFileNameWithoutExtension($configurationFile)).mod.json"
+
+    if ($force -and (test-path $packagePath))
+    {
+        [io.directory]::Delete($packagePath, $true)
+    }
+
+    if (!(test-path $packagePath))
+    {
+        (new-object net.webclient).DownloadFile($packageUrl, "$(get-location)\$packageName")
+        Expand-Archive $packageName
+    }
+
+    Set-Location $packagePath
+
+    if(!(test-path $configurationFile))
+    {
+        Write-Error "$configurationFile does not exist"
+        return
+    }
+
+    # verify and acl cert
+    $cert = get-item Cert:\LocalMachine\My\$thumbprint
+    if($cert)
+    {
+        write-host "found cert: $cert"
+        $machineKeyFileName = [regex]::Match((certutil -store my $thumbprint),"Unique container name: (.+?)\s").groups[1].value
+
+        if(!$machineKeyFileName)
+        {
+            finish-script
+            return 1
+        }
+
+        #$certFile = "c:\programdata\microsoft\crypto\rsa\machinekeys\$machineKeyFileName"
+        $certFile = "c:\programdata\microsoft\crypto\keys\$machineKeyFileName"
+        write-host "cert file: $certFile"
+        write-host "cert file: $(cacls $certFile)"
+
+        $acl = get-acl $certFile
+        $rule = new-object security.accesscontrol.filesystemaccessrule "NT AUTHORITY\NETWORK SERVICE", "Read", allow
+        write-host "setting acl: $rule"
+        $acl.AddAccessRule($rule)
+        set-acl $certFile $acl
+        write-host "acl set"
+        write-host "cert file: $(cacls $certFile)"
+
+    }
+    else
+    {
+        write-error "unable to find cert: $thumbprint. exiting"
+        finish-script
+        return 1
+    }
+
+
+    # read and modify config with thumb and nodes if first node
+    if(@($nodes)[0] -inotmatch $env:COMPUTERNAME)
+    {
+        Write-Warning "$env:COMPUTERNAME is not first node. exiting..."
+        finish-script
+        return
+    }
+
+    $json = Get-Content -Raw $configurationFile
+    $json = $json.Replace("[Thumbprint]",$thumbprint)
+    Out-File -InputObject $json -FilePath $configurationFileMod -Force
+    # add nodes to json
+    $json = Get-Content -Raw $configurationFileMod | convertfrom-json
+    $nodeList = [collections.arraylist]@()
+    $count = 0
+
+    foreach($node in $nodes)
+    {
+        $nodeList.Add(@{
+            nodeName      = $node
+            iPAddress     = $node
+            nodeTypeRef   = "NodeType0"
+            faultDomain   = "fd:/dc1/r$count"
+            upgradeDomain = "UD$count"
+        })
+        
+        $count++
+    }
+
+    $json.nodes = $nodeList.toarray()
+    Out-File -InputObject ($json | convertto-json -Depth 99) -FilePath $configurationFileMod -Force
+
+    if ($remove)
+    {
+        .\RemoveServiceFabricCluster.ps1 -ClusterConfigFilePath $configurationFileMod -Force
+        .\CleanFabric.ps1
+    }
+    else
+    {
+        $error.Clear()
+        $result = .\TestConfiguration.ps1 -ClusterConfigFilePath $configurationFileMod
+        $result
+
+        if($result -imatch "false|fail|exception")
+        {
+            Write-Error "failed test: $($error | out-string)"
+            return 1
+        }
+
+        .\CreateServiceFabricCluster.ps1 -ClusterConfigFilePath $configurationFileMod `
+        -AcceptEULA `
+        -NoCleanupOnFailure `
+        -TimeoutInSeconds $timeout `
+        -MaxPercentFailedNodes 0
+        #-GeneratedX509CertClusterConfigPath $certPath
+        #-Force
+
+        Connect-ServiceFabricCluster -ConnectionEndpoint localhost:19000
+        Get-ServiceFabricNode |Format-Table
+    }
+
+    finish-script
+}
+
+function finish-script()
+{
+    Set-Location $currentLocation
+    Stop-Transcript
+    $VerbosePreference = $DebugPreference = "silentlycontinue"
+}
+
+main
