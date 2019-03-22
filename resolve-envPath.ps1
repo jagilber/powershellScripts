ee75444acfb720be00d3575e156429009a34528b
Jason Gilbertson <jagilber@users.noreply.github.com>
2019-03-22 09:44:02 -0400

    fix missing source

---
+++ resolve-envPath.ps1
@@ -0,0 +1,36 @@
+<#
+    resolves path to directory or file on local system using current and path variables
+#>
+
+param(
+    $item = (get-location).path
+)
+
+function resolve-envPath($item)
+{
+    write-host "resolving $item"
+    $item = [environment]::ExpandEnvironmentVariables($item)
+    $sepChar = [io.path]::DirectorySeparatorChar
+
+    if($result = Get-Item $item -ErrorAction SilentlyContinue)
+    {
+        return $result.FullName
+    }
+
+    $paths = [collections.arraylist]@($env:Path.Split(";"))
+    [void]$paths.Add([io.path]::GetDirectoryName($MyInvocation.ScriptName))
+
+    foreach ($path in $paths)
+    {
+        if($result = Get-Item ($path.trimend($sepChar) + $sepChar + $item.trimstart($sepChar)) -ErrorAction SilentlyContinue)
+        {
+            return $result.FullName
+        }
+    }
+
+    Write-Warning "unable to find $item"
+    return $item
+}
+
+$result = resolve-envPath $item
+write-host "result: $result"
