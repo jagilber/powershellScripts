2874daf969ab365304f492467ba8b6dedb5cfcf1
jason gilbertson <jagilber@microsoft.com>
2019-05-21 06:58:29 -0400

    sql script cleanup

--- azure-sql-query.ps1
+++ azure-sql-query.ps1
@@ -8,21 +8,30 @@ param(
 
 $VerbosePreference = $DebugPreference = "continue"
 $ErrorActionPreference = "continue"
-$sqlConnection = New-Object System.Data.SqlClient.SqlConnection
-$sqlConnection.ConnectionString = "Server=tcp:$database.database.windows.net,$port;Initial Catalog=$database;Persist Security Info=False;User ID=$username;Password=$password;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+$sqlConnection = new-object System.Data.SqlClient.SqlConnection
+$sqlConnection.ConnectionString = "Server=tcp:$database.database.windows.net,$port;
+    Initial Catalog=$database;
+    Persist Security Info=False;
+    User ID=$username;
+    Password=$password;
+    MultipleActiveResultSets=False;
+    Encrypt=True;
+    TrustServerCertificate=False;
+    Connection Timeout=30;"
+
 $sqlConnection.Open()
-$sqlCmd = New-Object System.Data.SqlClient.SqlCommand($query,$sqlConnection)
+$sqlCmd = new-object System.Data.SqlClient.SqlCommand($query,$sqlConnection)
 $sqlReader = $sqlCmd.ExecuteReader()
 $Counter = $sqlReader.FieldCount
 
-While ($sqlReader.Read()) 
+while ($sqlReader.Read()) 
 {
-	For ($i = 0; $i -lt $Counter; $i++) 
+	for ($i = 0; $i -lt $Counter; $i++) 
 	{
-		Write-Host $sqlReader.GetName($i), $sqlReader.GetValue($i)
+		write-host $sqlReader.GetName($i), $sqlReader.GetValue($i)
 	}
 }
 
 $sqlConnection.Close()
 $VerbosePreference = $DebugPreference = "silentlycontinue"
-Write-Host "finished"
+write-host "finished"
