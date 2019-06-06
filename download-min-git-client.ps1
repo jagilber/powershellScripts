ca063a6bd7afb78aed61ff77c2a1f27d20fed243
Jason Gilbertson <jagilber@users.noreply.github.com>
2019-06-06 11:16:59 -0400

    update iex usage with  -UseBasicParsing

--- download-min-git-client.ps1
+++ download-min-git-client.ps1
@@ -2,7 +2,7 @@
 downloads git client for use but no install
 
 to run with no arguments:
-iwr "https://raw.githubusercontent.com/jagilber/powershellScripts/master/download-min-git-client.ps1"|iex
+iwr "https://raw.githubusercontent.com/jagilber/powershellScripts/master/download-min-git-client.ps1" -UseBasicParsing|iex
 
 or use the following to save and pass arguments:
 (new-object net.webclient).downloadFile("https://raw.githubusercontent.com/jagilber/powershellScripts/master/download-min-git-client.ps1","$pwd/download-min-git-client.ps1");
