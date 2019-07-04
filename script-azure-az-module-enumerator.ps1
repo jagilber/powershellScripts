ae02c3b0f1eddb70dce439b0cd6d972f9ab2dd53
Jason Gilbertson <jagilber@microsoft.com>
2019-07-04 14:53:28 -0400

    az updates remove profile

---
+++ script-azure-az-module-enumerator.ps1
@@ -0,0 +1,95 @@
+#script to scan other scripts for azure commands to list needed azure modules
+# 190127
+
+[CMDLETBINDING()]
+param(
+[string]$scriptFile,
+[string]$scriptDir,
+[ValidateSet('azurerm', 'az')]
+[string[]]$module = "az"
+)
+
+function main()
+{
+
+    if(!$scriptDir -and !$scriptFile)
+    {
+        $scriptDir = [IO.Path]::GetDirectoryName($MyInvocation.ScriptName)
+    }
+
+    if($scriptFile)
+    {
+        $files = @($scriptFile)
+    }
+    else
+    {
+        $files = [Io.Directory]::GetFiles($scriptDir,"*.ps1",[Io.SearchOption]::TopDirectoryOnly)
+    }
+
+    $neededAzureModules = new-object Collections.ArrayList
+    $availableModules = (get-module "$module.*" -ListAvailable) | Sort-Object -Property Name,Version -Descending | Get-Unique 
+
+    foreach($file in $files)
+    {
+        write-host "checking file: $($file)"
+        $scriptSource = Get-Content -Raw -Path $file
+        $matches = ([regex]::Matches($scriptSource, '\W([a-z]+-az[a-z]+)\W',[Text.RegularExpressions.RegexOptions]::IgnoreCase))
+        
+        foreach($match in ($matches| Sort-Object -Unique))
+        {
+            $matchedValue = $match.Captures[0].Groups[1].Value 
+            
+            write-host "`t`taz command: $($matchedValue)"
+            $module = $null
+            foreach($availableModule in $availableModules)
+            {
+                write-verbose "`t`t`tavailable module: $($availableModule)"
+
+                if($availableModule.ExportedCmdlets.Keys -imatch $matchedValue `
+                    -or $availableModule.ExportedAliases.Keys -imatch $matchedValue)
+                {
+                    $module = $availableModule.Name
+
+                    if($module)
+                    {
+                        write-verbose "`t`t`t`tchecking if module in list: $($module)"
+                        if(!$neededAzureModules.Contains($module))
+                        {
+                            write-host "`t`t`t`t`tadding module to list: $($module)" -ForegroundColor Green
+                            [void]$neededAzureModules.Add($module)
+                            continue
+                        }
+                        else
+                        {
+                            write-host "`t`t`t`t`tmodule already in list: $($module)" -ForegroundColor DarkGray
+                        }
+
+                    }
+                }
+            }
+
+            if(!$module)
+            {
+                Write-Warning "unable to find module for command $($matchedValue)"
+            }
+        }
+
+        if($neededAzureModules.Count -gt 0)
+        {
+            write-host "needed modules for file: $($file)"
+            write-host "$($neededAzureModules | format-list * | out-string)" -ForegroundColor Magenta
+        }
+        else
+        {
+            write-host "NO azure modules needed for file: $($file)" -ForegroundColor Gray
+        }
+
+        $neededAzureModules.Clear()
+        write-host "-----------------------------------------"
+    }
+
+    $neededAzureModules
+}
+    
+main
+
