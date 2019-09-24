900baab3f1745bbfa6d74fb8551425aa6813c526
Jason Gilbertson <jagilber@users.noreply.github.com>
2019-09-24 17:55:35 -0400

    net48 test

---
+++ temp/install-netframework-48.ps1
@@ -0,0 +1,20 @@
+$url = "https://go.microsoft.com/fwlink/?linkid=2088631" 
+$registryPath = "HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full"
+$version = (Get-ItemProperty -Path $registryPath -Name Version).Version
+Write-Host $version
+
+New-Item -ItemType Directory -Force -Path C:\temp\net_framework_48
+
+$path = "C:\temp\net_framework_48\ndp48-x86-x64-allos-enu.exe" 
+      
+if(!(Split-Path -parent $path) -or !(Test-Path -pathType Container (Split-Path -parent $path))) { 
+    $path = Join-Path $pwd (Split-Path -leaf $path) 
+} 
+      
+"Downloading [$url]`nSaving at [$path]" 
+$client = new-object System.Net.WebClient 
+$client.DownloadFile($url, $path) 
+      
+$path
+
+Invoke-Command -ScriptBlock { Start-Process -FilePath $path -ArgumentList "/log c:\temp\net_framework_48\net48.log /norestart "  -Wait -PassThru} 
