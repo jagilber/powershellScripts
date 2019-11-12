ec81362a62509b78f543335289cae754c037c7c7
Jason Gilbertson <jagilber@users.noreply.github.com>
2019-11-12 15:21:47 -0500

    sf man working

--- serviceFabric/sf-download-man.ps1
+++ serviceFabric/sf-download-man.ps1
@@ -3,22 +3,29 @@
 param(
     [string]$sfversion,
     [switch]$all,
-    [string]$sfPackageUrl = "https://go.microsoft.com/fwlink/?LinkID=824848&clcid=0x409"
+    [string]$sfPackageUrl = "https://go.microsoft.com/fwlink/?LinkID=824848&clcid=0x409",
+    [switch]$force
 )
 
 $packages = @(invoke-restmethod $sfPackageUrl).Packages
 
-if(!$all){
-    $packages = @($packages[-1])
-}
-elseif($sfversion) {
+if($sfversion) {
     $packages = @($packages -imatch $sfversion)
+}
+elseif(!$all){
+    $packages = @($packages[-1])
 }
 
 $packages
 
 foreach($package in $packages) {
     $package
+
+    if((test-path "$pwd\$($package.Version)") -and !$force) {
+        write-warning "package $($package.Version) exists. use force to download"
+        continue
+    }
+
     mkdir "$pwd\$($package.Version)"
     invoke-webrequest -uri ($package.TargetPackageLocation) -outfile "$pwd\$($package.Version).cab"
     expand "$pwd\$($package.Version).cab" -F:*.man "$pwd\$($package.Version)"
