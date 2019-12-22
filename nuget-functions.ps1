a44bbfb15b58ac87a9ccc24077e3b8b17ab2f93b
Jason Gilbertson <jagilber@users.noreply.github.com>
2019-12-22 10:00:17 -0500

    add local and source search

--- nuget-functions.ps1
+++ nuget-functions.ps1
@@ -3,6 +3,7 @@
 
 class NugetObj {
     [hashtable]$sources = @{}
+    [hashtable]$locals = @{}
     [hashtable]$packages = @{}
     [string]$packageName = $null
     [string]$packageSource = $null
@@ -25,6 +26,45 @@ class NugetObj {
         }
 
         $this.EnumSources()
+        $this.EnumLocals()
+    }
+
+    [string[]] AddSource([string]$nugetSourceName,[string]$nugetSource,[string]$username,[string]$password) {
+        [void]$this.EnumSources()
+        write-host "nuget sources add -name $nugetSourceName -source $nugetSource -username $userName -password $password"
+        write-host (nuget sources add -name $nugetSourceName -source $nugetSource -username $userName -password $password)
+        return $this.EnumSources()
+    }
+
+    [string[]] AddSource([string]$nugetSourceName,[string]$nugetSource) {
+        [void]$this.EnumSources()
+        write-host "nuget sources add -name $nugetSourceName -source $nugetSource"
+        write-host (nuget sources add -name $nugetSourceName -source $nugetSource)
+        return $this.EnumSources()
+    }
+
+    [string[]] DisableSource([string]$nugetSourceName) {
+        write-host "nuget sources disable -name $nugetSourceName"
+        write-host (nuget sources disable -name $nugetSourceName)
+        return $this.EnumSources()
+    }
+
+    [string[]] EnableSource([string]$nugetSourceName) {
+        write-host "nuget sources enable -name $nugetSourceName"
+        write-host (nuget sources enable -name $nugetSourceName)
+        return $this.EnumSources()
+    }
+
+    [hashtable] EnumLocals() {
+        write-host "nuget locals all -List"
+        $this.locals = @{}
+
+        foreach($source in (nuget locals all -List)) {
+            [string[]]$sourceProperties = $source -split ": "
+            $this.locals.Add($sourceProperties[0],$sourceProperties[1])
+        }
+        write-host "$($this.locals | out-string)"
+        return $this.locals
     }
 
     [hashtable] EnumPackagesTag ([string]$packageName) {
@@ -47,10 +87,15 @@ class NugetObj {
         $this.packagesource = $packageSource
         #if(!$searchFilter) { $searchFilter = "packageid:" }
 
-        if($this.sources.Contains($packageSource)) {
-            write-host "converting $packageSource to $($this.sources[$packageSource])" -ForegroundColor cyan
-            $this.packageSource = $this.sources[$packageSource].trimend('\')
+        if($this.locals.Contains($packageSource)) {
+            write-host "converting local package source $packageSource to $($this.locals[$packageSource])" -ForegroundColor cyan
+            $this.packageSource = $this.locals[$packageSource].trimend('\')
         }
+        elseif($this.sources.Contains($packageSource)) {
+            write-host "converting source package source $packageSource to $($this.sources[$packageSource].sourcePath)" -ForegroundColor cyan
+            $this.packageSource = $this.sources[$packageSource].sourcePath.trimend('\')
+        }
+
         $this.packages = @{}
         $sourcePackages = $null
         [string]$all = ""
@@ -100,21 +145,40 @@ class NugetObj {
     }
 
     [hashtable] EnumSources() {
-        write-host "nuget locals all -List"
+        write-host "nuget sources List"
         $this.sources = @{}
+        $results = (nuget sources List)
+        $source = @{}
 
-        foreach($source in (nuget locals all -List)) {
-            [string[]]$sourceProperties = $source -split ": "
-            $this.sources.Add($sourceProperties[0],$sourceProperties[1])
+        foreach($result in $results) {
+            $result = $result.trim()
+            if(!$result) { continue }
+            $pattern = "(?<sourceNumber>\d+?)\.\s+?(?<sourceName>[^\s].+?)\s+?\[(?<sourceEnabled>.+?)\]"
+            $match = [regex]::match($result, $pattern)
+
+            if($match.success) {
+                $source = @{}
+                $source.sourceNumber = ($match.groups['sourceNumber'].value)
+                $source.sourceName = ($match.groups['sourceName'].value)
+                $source.sourceEnabled = ($match.groups['sourceEnabled'].value)
+            }
+            else {
+                try {
+                    new-object uri($result) | out-null
+                    $source.sourcePath = $result
+                    [void]$this.sources.Add($source.sourceName, $source)
+                }
+                catch {
+                    $error.clear()
+                    write-verbose $result
+                    $source = @{}
+                }
+            }
         }
-        write-host "$($this.sources | out-string)"
-        return $this.sources
-    }
 
-    [string[]] AddSource([string]$nugetSourceName,[string]$nugetSource) {
-        write-host "nuget sources add -name $nugetSourceName -source $nugetSource"
-        write-host (nuget sources add -name $nugetSourceName -source $nugetSource)
-        return $this.EnumSources()
+        #write-host "$($this.sources | out-string)"
+        write-host ($this.sources.values | select sourceNumber, sourceName, sourcePath, sourceEnabled| sort sourceNumber | out-string)
+        return $this.sources
     }
 
     [string[]] InstallPackage([string]$packageName) {
@@ -133,9 +197,10 @@ class NugetObj {
         return $this.EnumPackages($packageName, $packageSource)
     }
 
-    [string[]] RemoveSource([string]$nugetSourceName,[string]$nugetSource) {
-        write-host "nuget sources remove -name $nugetSourceName -source $nugetSource"
-        write-host (nuget sources remove -name $nugetSourceName -source $nugetSource)
+    [string[]] RemoveSource([string]$nugetSourceName) {
+        [void]$this.EnumSources()
+        write-host "nuget sources remove -name $nugetSourceName"
+        write-host (nuget sources remove -name $nugetSourceName)
         return $this.EnumSources()
     }
 
