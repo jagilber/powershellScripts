4cba07a64329670bf2912f7157b7fbcf73475b62
Jason Gilbertson <jagilber@users.noreply.github.com>
2020-01-07 08:20:04 -0500

    add param

--- process-monitor.ps1
+++ process-monitor.ps1
@@ -6,25 +6,25 @@
 # microsoft support
 #-------------------------------------------------------------------------------------------------
  
-$currentProcessList = @{}
-$tempList = @{}
-$newList = @{}
-$processName = "notepad" #"logonui" 
-$processArgs = ""
-$sleepMs = 10
-$debugProcess = "procdump.exe" #c:\temp\mon-logonui.bat"
-$debugArguments = "-accepteula -ma -e -t -n 10 "
+param(
+    $processName = "notepad", #"logonui" 
+    $processArgs = "",
+    $sleepMs = 10,
+    $debugProcess = "procdump.exe", #c:\temp\mon-logonui.bat"
+    $debugArguments = "-accepteula -ma -e -t -n 10 "    
+)
+
+$currentProcessList = @{ }
+$tempList = @{ }
+$newList = @{ }
 
 #-------------------------------------------------------------------------------------------------
-function main()
-{
+function main() {
     #init
     $currentProcessList = enum-processes -processName $processName
 
-    if($syinternalsProcess -and ![IO.File]::Exists($debugProcess))
-    {
-        if([string]::IsNullOrEmpty((get-sysInternalsUtility -utilityName ([IO.Path]::GetFileName($debugProcess)))))
-        {
+    if ($syinternalsProcess -and ![IO.File]::Exists($debugProcess)) {
+        if ([string]::IsNullOrEmpty((get-sysInternalsUtility -utilityName ([IO.Path]::GetFileName($debugProcess))))) {
             "unable to download $($debugProcess). exiting"
             return
         }
@@ -32,56 +32,49 @@ function main()
 
     $count = 0
 
-    while($true)
-    {
+    while ($true) {
         $newList = enum-processes -processName $processName
  
-        foreach($id in $newList.GetEnumerator())
-        {
-            if(!$currentProcessList.ContainsKey($id.Key))
-            {
+        foreach ($id in $newList.GetEnumerator()) {
+            if (!$currentProcessList.ContainsKey($id.Key)) {
                 write-host "Adding process to list: $($id.key):$($id.Value)"
                 $currentProcessList.Add($id.Key, $process);
                 
-                if($processArgs -ne $null)
-                {
+                if ($processArgs -ne $null) {
                     $cmdline = Get-WmiObject Win32_Process -Filter "ProcessId like `'$($id.key)`'" 
-                    if(!$cmdline.CommandLine.Contains($processArgs))
-                    {
+                    if (!$cmdline.CommandLine.Contains($processArgs)) {
                         continue
-                   }
+                    }
                 }
  
-                #windbg
-                #Start-Process -process $debugProcess -arguments "$($debugArguments) -p $($id.Key)"
+                if ($debugProcess) {
+                    #windbg
+                    #Start-Process -process $debugProcess -arguments "$($debugArguments) -p $($id.Key)"
                 
-                #procdump
-                Start-Process -process $debugProcess -arguments "$($debugArguments) $($id.Key)"
+                    #procdump
+                    Start-Process -process $debugProcess -arguments "$($debugArguments) $($id.Key)"
                 
-                #tttracer
-                #Start-Process -process $debugProcess -arguments "$($debugArguments) $($id.Key)"
+                    #tttracer
+                    #Start-Process -process $debugProcess -arguments "$($debugArguments) $($id.Key)"
+                }
             }
         }
  
         $tempList = $currentProcessList.Clone();
  
-        foreach($id in $tempList.GetEnumerator())
-        {
-            if(!$newList.ContainsKey($id.Key))
-            {
+        foreach ($id in $tempList.GetEnumerator()) {
+            if (!$newList.ContainsKey($id.Key)) {
                 write-host "Removing process from list: $($id.key):$($id.Value)"
                 $currentProcessList.Remove($id.Key);
             }
         }
  
         Start-Sleep -Milliseconds $sleepMs
-        if($count -ge 100)
-        {
+        if ($count -ge 100) {
             write-host ""
             $count = 0
         }
-        else
-        {
+        else {
             write-host "." -NoNewline
             $count++
         }
@@ -90,26 +83,32 @@ function main()
 }
  
 #-------------------------------------------------------------------------------------------------
-function enum-processes($processName)
-{
-    $tempL = @{}
+function enum-processes($processName) {
+    $tempList = @{ }
     
-    foreach($process in [System.Diagnostics.Process]::GetProcessesByName($processName))
+    if($processName -eq "*")
     {
-        $tempL.Add($process.Id, $process)
+        foreach ($process in [diagnostics.Process]::GetProcesses()) {
+            $tempList.Add($process.Id, $process)
+        }
+    }
+    else 
+    {
+        foreach ($process in [diagnostics.Process]::GetProcessesByName($processName)) {
+            $tempList.Add($process.Id, $process)
+        }
     }
  
-    return $tempL
+    return $tempList
 }
  
 #-------------------------------------------------------------------------------------------------
-function start-process($process, $arguments)
-{
+function start-process($process, $arguments) {
     write-host "Starting $($process)"
     $processStartInfo = New-Object System.Diagnostics.ProcessStartInfo;
     $processStartInfo.FileName = $process;
     $processStartInfo.WorkingDirectory = (Get-Location).Path;
-    if($arguments) { $processStartInfo.Arguments = $arguments }
+    if ($arguments) { $processStartInfo.Arguments = $arguments }
     #$processStartInfo.UseShellExecute = $false;
     #$processStartInfo.RedirectStandardOutput = $true;
  
@@ -120,34 +119,28 @@ function start-process($process, $arguments)
 }
  
 #-------------------------------------------------------------------------------------------------
-function get-sysInternalsUtility ([string] $utilityName)
-{
-    try
-    {
+function get-sysInternalsUtility ([string] $utilityName) {
+    try {
         $destFile = "$(get-location)\$utilityName"
         [System.Net.ServicePointManager]::Expect100Continue = $true;
         [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
-        if(![IO.File]::Exists($destFile))
-        {
+        if (![IO.File]::Exists($destFile)) {
             $sysUrl = "https://live.sysinternals.com/$($utilityName)"
 
             write-host "Sysinternals process psexec.exe is needed for this option!" -ForegroundColor Yellow
-            if((read-host "Is it ok to download $($sysUrl) ?[y:n]").ToLower().Contains('y'))
-            {
+            if ((read-host "Is it ok to download $($sysUrl) ?[y:n]").ToLower().Contains('y')) {
                 $webClient = new-object System.Net.WebClient
                 [void]$webClient.DownloadFile($sysUrl, $destFile)
                 write-host "sysinternals utility $($utilityName) downloaded to $($destFile)"
             }
-            else
-            {
+            else {
                 return [string]::Empty
             }
         }
 
         return $destFile
     }
-    catch
-    {
+    catch {
         "Exception downloading $($utilityName): $($error)"
         $error.Clear()
         return [string]::Empty
