91aac13bbedc404e9137f4270f153d6d0e94b34e
Jason Gilbertson <jagilber@users.noreply.github.com>
2020-01-07 12:33:39 -0500

    orig test-tcp-listener

---
+++ test-tcp-listener.ps1
@@ -0,0 +1,42 @@
+# powershell test tcp listener for troubleshooting
+
+param(
+    $port = 9999,
+    $count = 0,
+    $hostName = 'localhost'
+)
+
+try {
+    $listener = [net.sockets.tcpListener]$port;
+    $listener.Start();
+    write-host "use following client commands to test:" -ForegroundColor Green
+    write-host "`$client = new-object net.sockets.tcpClient"
+    write-host "`$client.Connect('$hostName', $port)"
+    $iteration = 0
+
+    while ($iteration -lt $count -or $count -eq 0) {
+        Write-Host "$(get-date) listening on port $port";
+        #$server = $listener.AcceptTcpClient();
+        $server = $listener.AcceptSocket();        
+        $server | ConvertTo-Json -Depth 99
+        $listener | ConvertTo-Json -Depth 99
+
+        $responseString = "$(get-date) client connected on port $port";
+        [byte[]]$sendBytes = [text.encoding]::ASCII.GetBytes($responseString);
+        $i = $server.Send($sendBytes);
+        Write-Host "$i : $responseString"
+
+        $server.Close();
+        $iteration++
+    }
+
+    Write-Host "$(get-date) Finished!";
+}
+finally {
+    if ($server) {
+        $server.Dispose();
+    }
+    if ($listener) {
+        $listener.Stop();
+    }
+}
