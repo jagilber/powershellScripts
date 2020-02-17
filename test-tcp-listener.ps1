70547a3df09d85297f34a5bbea86da7db8b3a823
Jason Gilbertson <jagilber@users.noreply.github.com>
2020-02-17 13:57:04 -0500

    add client and server loop

--- test-tcp-listener.ps1
+++ test-tcp-listener.ps1
@@ -1,44 +1,100 @@
 # powershell test tcp listener for troubleshooting
+# $script:client.Client.Shutdown([net.sockets.socketshutdown]::Both)
+# do a final client connect to free up close
 
 param(
-    $port = 9999,
-    $count = 0,
-    $hostName = 'localhost'
+    [int]$port = 9999,
+    [int]$count = 0,
+    [string]$hostName = 'localhost',
+    [string]$testClientMessage = 'test message from client',
+    [switch]$isClient
 )
 
-try {
-    $listener = [net.sockets.tcpListener]$port;
-    $listener.Start();
-    write-host "use following client commands to test:" -ForegroundColor Green
-    write-host "`$client = new-object net.sockets.tcpClient"
-    write-host "`$client.Connect('$hostName', $port)"
-    #write-host "`$sendBytes = [text.encoding]::ASCII.GetBytes('client connection');"
-    #write-host "`$client.Client.Send(`$sendBytes)"
+$script:server = $null
+$script:client = $null
+
+function main() {
+    try {
+        write-host "use following client commands to test:" -ForegroundColor Green
+        write-host "`$script:client = new-object net.sockets.tcpClient"
+        write-host "`$script:client.Connect('$hostName', $port)"
+        #write-host "`$sendBytes = [text.encoding]::ASCII.GetBytes('client connection');"
+        #write-host "`$script:client.Client.Send(`$sendBytes)"
+
+        if($isClient) {
+            start-client
+        }
+        else {
+            start-server
+        }
+
+        Write-Host "$(get-date) Finished!";
+    }
+    finally {
+        if($script:client) {
+            $script:client.Close()
+            $script:client.Dispose();
+        }
+        if ($script:server) {
+            $script:server.Close()
+            $script:server.Dispose();
+        }
+        if ($listener) {
+            $listener.Stop();
+        }
+    }
+}
+
+function start-client() {
     $iteration = 0
+    $script:client = new-object net.sockets.tcpClient
+    $sendBytes = [text.encoding]::ASCII.GetBytes($testClientMessage);
+    [byte[]]$buffer = @(0) * $script:client.ReceiveBufferSize
+    $script:client.Connect($hostName, $port);
 
     while ($iteration -lt $count -or $count -eq 0) {
-        Write-Host "$(get-date) listening on port $port";
-        #$server = $listener.AcceptTcpClient();
-        $server = $listener.AcceptSocket();        
-        $server | ConvertTo-Json -Depth 99
-        $listener | ConvertTo-Json -Depth 99
+        
+        $script:client.Client.Send($sendBytes)
 
-        $responseString = "$(get-date) client connected on port $port";
-        [byte[]]$sendBytes = [text.encoding]::ASCII.GetBytes($responseString);
-        $i = $server.Send($sendBytes);
-        Write-Host "$i : $responseString"
-
-        $server.Close();
+        $script:client.Client.receive($buffer)
+        Write-Host "$i : $(([text.encoding]::ASCII.GetString($buffer)).Trim())"
+        $buffer.Clear()
+        #$script:client.Close()
+        start-sleep -Seconds 1
         $iteration++
     }
 
-    Write-Host "$(get-date) Finished!";
+    $script:client.Close()
 }
-finally {
-    if ($server) {
-        $server.Dispose();
+
+function start-server() {
+    $iteration = 0
+    $listener = [net.sockets.tcpListener]$port;
+    $listener.Start();
+    $script:server = $listener.AcceptSocket();     
+
+    while ($iteration -lt $count -or $count -eq 0) {
+        Write-Host "$(get-date) listening on port $port";
+        #$script:server = $listener.AcceptTcpClient();
+        $responseString = "$(get-date) client connected on port $port";
+        $script:server | ConvertTo-Json -Depth 99
+        $listener | ConvertTo-Json -Depth 99
+        $responseString += "`r`nwaiting for client send bytes to be sent...`r`n"
+
+        [byte[]]$sendBytes = [text.encoding]::ASCII.GetBytes($responseString);
+        $i = $script:server.Send($sendBytes);
+        Write-Host "$i : $responseString"
+
+        [byte[]]$buffer = @(0) * $script:server.ReceiveBufferSize
+        $script:server.receive($buffer)
+        $i = $script:server.Send($buffer);
+        Write-Host "$i : $(([text.encoding]::ASCII.GetString($buffer)).Trim())"
+        ([text.encoding]::ASCII.GetString($buffer)).Trim()
+        $buffer.Clear()
+        $iteration++
     }
-    if ($listener) {
-        $listener.Stop();
-    }
+    
+    $script:server.Close();
 }
+
+main
