8e691734c8e3f2ad22cb98c36708bca944573374
Jason Gilbertson <jagilber@users.noreply.github.com>
2020-03-02 22:40:14 -0500

    refactor

--- serviceFabric/azure-az-sf-patch-fabric-resource.ps1
+++ serviceFabric/azure-az-sf-patch-fabric-resource.ps1
@@ -1,10 +1,10 @@
 <#
-# script to update azure service fabric settings
+# script to update azure service fabric arm template resource settings
 # https://docs.microsoft.com/en-us/azure/service-fabric/service-fabric-cluster-fabric-settings
 
 download:
-(new-object net.webclient).DownloadFile("https://raw.githubusercontent.com/jagilber/powershellScripts/master/serviceFabric/azure-az-sf-patch-fabric-arm-template.ps1","$pwd\azure-az-sf-patch-fabric-arm-template.ps1")
-.\azure-az-sf-patch-fabric-arm-template.ps1 -resourceGroup {{cluster resource group}} -clusterName {{cluster name}} -templateJsonFile {{arm template json file}}
+(new-object net.webclient).DownloadFile("https://raw.githubusercontent.com/jagilber/powershellScripts/master/serviceFabric/azure-az-sf-patch-fabric-resource.ps1","$pwd\azure-az-sf-patch-fabric-resource.ps1")
+.\azure-az-sf-patch-fabric-resource.ps1 -resourceGroup {{cluster resource group}} -clusterName {{cluster name}} [-patch]
 
 .EXAMPLE
 #>
@@ -14,6 +14,7 @@ param (
     [string]$clusterName = '',
     [string]$templateJsonFile = '.\template.json', 
     [string]$sfApiVersion = '2019-06-01-preview',
+    [string]$schema = 'http://schema.management.azure.com/schemas/2019-03-01/deploymentTemplate.json'
     [switch]$patch
 )
 
@@ -28,10 +29,11 @@ function main () {
     }
 
     $error.Clear()
-    $global:currentSettings = Get-AzServiceFabricCluster -ResourceGroupName $resourceGroup -Name $clusterName
+    $global:currentSettings = Get-AzResource -ResourceGroupName $resourceGroup -Name $clusterName -ExpandProperties | convertto-json -depth 99
     write-host "current settings: `r`n $global:currentSettings" -ForegroundColor Green
     write-host "current fabric settings"
     $currentSettings.FabricSettings
+    
     if ($error) {
         Write-Warning "error enumerating cluster"
         return
@@ -52,8 +54,7 @@ function main () {
                 -ResourceGroupName $resourceGroup `
                 -DeploymentDebugLogLevel All `
                 -TemplateFile $templateJsonFile `
-                -Verbose `
-                -Debug
+                -Verbose
         }
         else {
             write-error "template validation failed`r`n$error`r`n$result"
@@ -63,40 +64,35 @@ function main () {
     else {
         write-host "exporting template to $templateJsonFile" -ForegroundColor Yellow
         # convert sf cluster az resource to sf arm template
-        $cluster = get-azresource -Name $clusterName -ResourceGroupName $resourceGroup -ResourceType Microsoft.ServiceFabric/clusters
-        $clusterPropertiesJson = $cluster.properties | convertto-json -depth 99
+        $cluster = get-azresource -Name $clusterName -ResourceGroupName $resourceGroup -ExpandProperties # -ResourceType Microsoft.ServiceFabric/clusters
 
-        $clusterResourcesObject = @{ }
-        $clusterResourcesObject.'$schema' = "http://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json"
-        $clusterResourcesObject.contentVersion = "1.0.0.0"
+        $clusterTemplate = @{ 
+            '$schema' = $schema
+            contentVersion = "1.0.0.0"
+            resources = @(
+                @{
+                    apiVersion = $sfApiVersion
+                    dependsOn = @()
+                    type = $cluster.Type
+                    location = $cluster.Location
+                    id = $cluster.Id
+                    name = $cluster.Name
+                    tags = $cluster.Tags
+                    properties = $cluster.properties
+                })
+        } | convertto-json -depth 99
 
-        $clusterObject = @{ }
-        $clusterObject.apiVersion = $sfApiVersion
-        #$clusterObject.dependsOn = @()
-        $clusterObject.type = $cluster.Type
-        $clusterObject.location = $cluster.Location
-        #$clusterObject.id = $cluster.Id
-        $clusterObject.name = $cluster.Name
-        $clusterObject.tags = $cluster.Tags
-        #$clusterObject.etag = $cluster.ETag
-        $clusterObject.properties = $clusterPropertiesJson | convertfrom-json
-
-        $clusterResourcesObject.Add('resources', @($clusterObject))
-
-        $clusterTemplate = $clusterResourcesObject | convertto-json -depth 99
         $clusterTemplate | out-file $templateJsonFile
-
-        #$clusterTemplate
         write-host $clusterTemplate -ForegroundColor Cyan
 
         write-host "template exported to $templateJsonFile" -ForegroundColor Yellow
-        write-host "modify $templateJsonFile then rerun script with -patch to update cluster" -ForegroundColor Yellow
+        write-host "to update cluster, modify $templateJsonFile.  when finished, execute script with -patch to update cluster" -ForegroundColor Yellow
         . $templateJsonFile
 
         return
     }
 
-    $global:newSettings = Get-AzServiceFabricCluster -ResourceGroupName $resourceGroup -Name $clusterName
+    $global:newSettings = Get-AzResource -ResourceGroupName $resourceGroup -Name $clusterName -ExpandProperties | convertto-json -depth 99
     write-host "new settings: `r`n $global:newSettings" -ForegroundColor Cyan
     write-host "new fabric settings"
 
