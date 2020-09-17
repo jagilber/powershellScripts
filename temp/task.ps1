0fd34bc72bd1b2234fb8846ffce8461b32214df3
Jason Gilbertson <jagilber@users.noreply.github.com>
2020-09-17 14:43:54 -0400

    task test

--- temp/task.ps1
+++ temp/task.ps1
@@ -1,1 +1,9 @@
 Out-File -InputObject "$(get-date)" -FilePath c:\taskscripts\test.log
+
+New-WinEvent -ProviderName Microsoft-Windows-Powershell `
+    -id 4103 `
+    -Payload @(
+    "context:`r`n$(($MyInvocation | convertto-json -Depth 1))", 
+    "user data:`r`n$(([environment]::GetEnvironmentVariables() | convertto-json))", 
+    "task.ps1`r`nerror:`r`n$(($error | convertto-json -Depth 1))"
+)
