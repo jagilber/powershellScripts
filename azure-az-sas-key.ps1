5a0bb06da4610d680958c314f4363e1c5e7a3e50
Jason Gilbertson <jagilber@microsoft.com>
2020-09-27 17:05:16 -0400

    add output

--- azure-az-sas-key.ps1
+++ azure-az-sas-key.ps1
@@ -8,19 +8,31 @@ param(
     [ValidateSet('service', 'container', 'object')]
     $resourceType = @('service', 'container', 'object'),
     $permission = 'racwdlup',
-    $expirationDays = 7
+    $expirationHours = 8
 )
+
+write-host "Get-AzStorageAccount -ResourceGroupName $resourceGroupName" -ForegroundColor Cyan
 
 $accounts = (Get-AzStorageAccount -ResourceGroupName $resourceGroupName) | where-object StorageAccountName -imatch $storageAccountName
 $saskeys = [collections.arraylist]::new()
 
 foreach ($account in $accounts) {
     $blobUri = $account.Context.BlobEndPoint
+    write-host "creating sas for $blobUri" -ForegroundColor Green
+
+    write-host "New-AzStorageAccountSASToken -Service $($service -join ',') `
+        -ResourceType $($resourceType -join ',') `
+        -StartTime $((get-date).AddMinutes(-1)) `
+        -ExpiryTime $((get-date).AddHours($expirationHours)) `
+        -Context [$($account.context)]$($blobUri) `
+        -Protocol HttpsOnly `
+        -Permission $permission
+    " -ForegroundColor Cyan
 
     $sas = New-AzStorageAccountSASToken -Service $service `
         -ResourceType $resourceType `
         -StartTime (get-date).AddMinutes(-1) `
-        -ExpiryTime (get-date).AddDays($expirationDays) `
+        -ExpiryTime (get-date).AddHours($expirationHours) `
         -Context $account.context `
         -Protocol HttpsOnly `
         -Permission $permission
