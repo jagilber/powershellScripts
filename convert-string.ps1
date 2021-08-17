d702f0ef3e7a34a355aa5b7168b08defe8d69dd3
Jason Gilbertson <jagilber@microsoft.com>
2021-08-17 10:22:22 -0400

    add logging

--- convert-string.ps1
+++ convert-string.ps1
@@ -13,34 +13,42 @@ param(
 
 
 if($stringToBase64){
+    write-host "stringToBase64: [convert]::ToBase64String([text.encoding]::UTF8.GetBytes(`$inputString))" -ForegroundColor Green
     write-host "stringToBase64: $([convert]::ToBase64String([text.encoding]::UTF8.GetBytes($inputString)))"
 }
 
 if($base64ToString){
+    write-host "base64ToString: [text.encoding]::UTF8.GetString([convert]::FromBase64String(`$inputString))" -ForegroundColor Green
     write-host "base64ToString: $([text.encoding]::UTF8.GetString([convert]::FromBase64String($inputString)))"
 }
 
 if($urlEncodeToString) {
+    Write-Host "urlEncodeToString: [web.httpUtility]::UrlDecode(`$inputString)" -ForegroundColor Green
     Write-Host "urlEncodeToString: $([web.httpUtility]::UrlDecode($inputString))"
 }
 
 if($stringToUrlEncode) {
+    Write-Host "stringToUrlEncode: [web.httpUtility]::UrlEncode(`$inputString)" -ForegroundColor Green
     Write-Host "stringToUrlEncode: $([web.httpUtility]::UrlEncode($inputString))"
 }
 
 if($stringToRegexEscaped) {
+    Write-Host "stringToRegexEscaped: [regex]::Escape(`$inputString)" -ForegroundColor Green
     Write-Host "stringToRegexEscaped: $([regex]::Escape($inputString))"
 }
 
 if($regexToStringUnescaped) {
+    Write-Host "regexToStringUnescaped: [regex]::Unescape(`$inputString)" -ForegroundColor Green
     Write-Host "regexToStringUnescaped: $([regex]::Unescape($inputString))"
 }
 
 if($stringToCharArray) {
+    write-host "stringToCharArray: `$inputString.ToCharArray() | foreach-object {[int][char]`$_}" -ForegroundColor Green
     write-host "stringToCharArray: $($inputString.ToCharArray() | foreach-object {[int][char]$_})"
 }
 
 if($charArrayToString) {
+    write-host "charArrayToString: [string]::new(`$inputString)" -ForegroundColor Green
     write-host "charArrayToString: $([string]::new($inputString))"
 }
 
