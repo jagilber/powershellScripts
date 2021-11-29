465e430343fb5e1d33a4168cc5e90a7695e939fd
Jason Gilbertson <jagilber@microsoft.com>
2021-11-28 19:55:38 -0500

    base

--- serviceFabric/sf-docker-prune.ps1
+++ serviceFabric/sf-docker-prune.ps1
@@ -5,32 +5,59 @@
 .LINK
     [net.servicePointManager]::Expect100Continue = $true;[net.servicePointManager]::SecurityProtocol = [net.SecurityProtocolType]::Tls12;
     invoke-webRequest "https://raw.githubusercontent.com/jagilber/powershellScripts/master/serviceFabric/sf-docker-prune.ps1" -outFile "$pwd\sf-docker-prune.ps1";
-    .\sf-docker-prune.ps1 -whatIf -stopServices -stopProcesses;
+    .\sf-docker-prune.ps1 -whatIf;
 #>
 
 #[cmdletbinding()]
 param(
-    [string]$dockerRoot = 'c:\programdata\docker',
-    [ValidateSet('continue', 'stop', 'silentlycontinue')]
-    [string]$errorAction = 'continue',
+    [string]$imageFilter = '',
     [ValidateSet('system', 'image', 'container', 'volume', 'network')]
     [string]$action = 'image',
     [string]$lastUsedHoursCron = '72h',
-    [switch]$stopServices,
-    [string[]]$services = @('service fabric host service', 'Azure Service Fabric Node Bootstrap Agent'),
-    [switch]$stopProcesses,
+    [string]$dockerRoot = 'c:\programdata\docker',
+    [ValidateSet('continue', 'stop', 'silentlycontinue')]
+    [string]$errorAction = 'continue',
+    [string]$services = '[
+        {
+          "name": "FabricHostSvc",
+          "restart": true,
+          "startuptype": "manual"
+        },
+        {
+          "name": "FabricInstallerSvc",
+          "restart": false,
+          "startuptype": "manual"
+        },
+        {
+            "name": "ServiceFabricNodeBootstrapAgent",
+            "restart": false,
+            "startuptype": "automatic"
+        },
+        {
+            "name": "ServiceFabricNodeBootstrapUpgradeAgent",
+            "restart": false,
+            "startuptype": "automatic"
+        }
+      ]',
     [string[]]$processes = @('fabricdeployer', 'dockerd'),
     [switch]$whatIf,
     [switch]$force,
     [string]$dockerHost = 'http://localhost:2375/info'
 )
 
-$filter = "--filter until=$lastUsedHoursCron"
+$filter = "--filter `"until=$lastUsedHoursCron`""
+$forceSwitch = $null
+$servicesTable = convertfrom-json $services
 
 function main() {
     clear-host;
     $error.Clear()
     write-output "$(get-date):starting"
+
+    if ($force) {
+        $forceSwitch = "--force "
+    }
+    
     $dockerInfo = invoke-webRequest -UseBasicParsing $dockerHost
     $isAdmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
     write-output "isadmin:$isAdmin"
@@ -45,44 +72,43 @@ function main() {
         $dockerRoot = $dockerObj.DockerRootDir
     }
 
-    write-output "docker info: $(docker system info)"
-    write-output "docker version: $(docker version)"
-    write-output "docker volume ls: $(docker volume ls)"
-    write-output "docker disk space used: $(docker system df)"
-    write-output "docker container: $(docker ps -a)"
-    write-output "docker image: $(docker images -a)"
-    write-output "docker network: $(docker network ls)"
-    write-output "dir windowsFilter: $(Get-ChildItem "$dockerRoot\windowsFilter" -Directory)"
-    write-output "dir containers: $(Get-ChildItem "$dockerRoot\containers" -Directory)"
-    write-output "dir image: $(Get-ChildItem "$dockerRoot\image" -Directory)"
-    write-output "process list: $(get-process | out-string)"
+    write-config
 
     if ((Get-ChildItem "\\?\$pwd")) { 
         $dockerRoot = "\\?\$dockerRoot"
     }
     
-    write-output "getting images: docker images $filter"
-    $images = docker images $filter
-    
+    write-output "getting images: docker images -a -q $imageFilter"
+    $images = Invoke-Expression "docker images -a -q $imageFilter"
+    $images
+
     if (!$images) {
-        write-output "no images found matching: docker images $filter"
-        if(!$whatIf) {
+        write-output "no images found matching: docker images -a -q $imageFilter"
+        if (!$whatIf -and !$force) {
             return
         }
     }
 
-    if ($stopServices) {
-        foreach ($service in $services) {
-            write-output "stopping $service"
-            stop-service -name $service -WhatIf:$whatIf -ErrorAction $errorAction -Force:$force
-        }
+    foreach ($service in $servicesTable) {
+        write-output "setting $($service.name) to $($service.startuptype)"
+        set-service -name $service.name -StartupType Disabled -WhatIf:$whatIf -ErrorAction $errorAction
+
+        write-output "stopping $($service.name)"
+        stop-service -name $service.name -WhatIf:$whatIf -ErrorAction $errorAction -Force:$force
     }
 
-    if ($stopProcesses) {
-        foreach ($process in $processes) {
-            write-output "stopping $process"
-            stop-process -name $process -WhatIf:$whatIf -ErrorAction $errorAction -Force:$force
-        }
+    foreach ($process in $processes) {
+        write-output "stopping $process"
+        stop-process -name $process -WhatIf:$whatIf -ErrorAction $errorAction -Force:$force
+    }
+
+    write-output "start-service docker"
+    start-service docker -WhatIf:$whatIf
+    $result = $null
+
+    while ((get-service -Name docker).Status -ine 'running') {
+        write-output "waiting for docker to start"
+        Start-Sleep -seconds 1
     }
 
     switch ($action) {
@@ -103,119 +129,103 @@ function main() {
         }
     }
 
-    if ($stopServices) {
-        foreach ($service in ($services | sort-object)) {
-            write-output "starting $service"
-            start-service -name $service -WhatIf:$whatIf -ErrorAction $errorAction
+    write-output "stop-service docker"
+    stop-service docker -WhatIf:$whatIf -Force:$force
+    $result = $null
+
+    while ((get-service -Name docker).Status -ine 'stopped') {
+        write-output "waiting for docker to stop"
+        Start-Sleep -seconds 1
+    }
+
+    foreach ($service in ($servicesTable | sort-object)) {
+        write-output "setting $($service.name) to $($service.startuptype)"
+        set-service -name $service.name -StartupType $service.startuptype -WhatIf:$whatIf -ErrorAction $errorAction
+
+        if ($service.restart) {
+            write-output "starting $($service.name)"
+            start-service -name $service.name -WhatIf:$whatIf -ErrorAction $errorAction
         }
     }
 
+    while (!(get-process -Name dockerd)) {
+        write-output "waiting for dockerd to start from fabrichost"
+        Start-Sleep -seconds 1
+    }
+
     write-output "process list: $(get-process | out-string)"
+    write-config
     write-output "$(get-date):finished"
 }
 
 function prune-container() {
-    write-output "checking containers"
+    write-output "checking container"
+    write-output "docker container prune $forceSwitch$filter"
 
     if (!$whatIf) {
-        if ($force) {
-            write-output "docker $action prune --force $filter"
-            $result = docker $action prune --force $filter
-            write-output $result
-        }
-        else {
-            write-output "docker $action prune $filter"
-            $result = docker $action prune $filter
-            write-output $result
-        }
+        Invoke-Expression "docker container prune $forceSwitch$filter"
     }
-
-    write-output "docker ps -a"
-    $result = docker ps -a
-    write-output $result
-
 }
 
 function prune-image() {
     write-output "checking images"
+    write-output "docker image prune -a $forceSwitch$filter"
 
     if (!$whatIf) {
-        if ($force) {
-            write-output "docker $action prune -a --force $filter"
-            $result = docker $action prune -a --force $filter
-            write-output $result
-        }
-        else {
-            write-output "docker $action prune -a $filter"
-            $result = docker $action prune -a $filter
-            write-output $result
-        }
+        Invoke-Expression "docker image prune -a $forceSwitch$filter"
     }
-
-    write-output "docker $action -a"
-    $result = docker $action -a
-    write-output $result
 }
 
 function prune-network() {
     write-output "checking network"
+    write-output "docker network prune $forceSwitch$filter"
 
     if (!$whatIf) {
-        if ($force) {
-            write-output "docker $action prune --force $filter"
-            $result = docker $action prune --force $filter
-            write-output $result
-        }
-        else {
-            write-output "docker $action prune $filter"
-            $result = docker $action prune $filter
-            write-output $result
-        }
+        Invoke-Expression "docker network prune $forceSwitch$filter"
     }
-
-    write-output "docker $action ls"
-    $result = docker $action ls
-    write-output $result
 }
 function prune-system() {
     write-output "pruning system"
+    write-output "docker system prune -a --volumes $forceSwitch"
 
     if (!$whatIf) {
-        if ($force) {
-            write-output "docker $action prune -a --volumes --force $filter"
-            $result = docker $action prune -a --volumes --force $filter
-            write-output $result
-        }
-        else {
-            write-output "docker $action prune -a --volumes $filter"
-            $result = docker $action prune -a --volumes $filter
-            write-output $result
-        }
+        Invoke-Expression "docker system prune -a --volumes $forceSwitch"
     }
-
-    write-output "docker $action info"
-    $result = docker $action info
-    write-output $result
 }
 function prune-volume() {
     write-output "checking volumes"
+    write-output "docker volume prune $forceSwitch$filter"
 
-    if (!$whatIf) {
-        if ($force) {
-            write-output "docker $action prune --force $filter"
-            $result = docker $action prune --force $filter
-            write-output $result
-        }
-        else {
-            write-output "docker $action prune $filter"
-            $result = docker $action volume $filter
-            write-output $result
-        }
+    if (!$whatIf) {        
+        Invoke-Expression "docker volume prune $forceSwitch$filter"
     }
+}
 
-    write-output "docker $action ls"
-    $result = docker $action ls
-    write-output $result
+function write-config() {
+    write-output "docker info: "
+    docker system info
+    write-output "docker version: "
+    docker version
+    write-output "docker volume ls: "
+    docker volume ls
+    write-output "docker disk space used: "
+    docker system df
+    write-output "docker container: "
+    docker ps -a
+    write-output "docker image: "
+    docker images -a
+    write-output "docker network: "
+    docker network ls
+
+    write-output "dir windowsFilter: "
+    (Get-ChildItem "$dockerRoot\windowsFilter" -Directory).FullName
+    write-output "dir containers: "
+    (Get-ChildItem "$dockerRoot\containers" -Directory).FullName
+    write-output "dir image: "
+    (Get-ChildItem "$dockerRoot\image" -Directory).FullName
+    write-output "process list: "
+    get-process | out-string
+
 }
 
 main
