69840afad5e4280f585b40b0e5f0574afc7686b9
jason gilbertson <jagilber@microsoft.com>
2022-04-27 08:20:51 -0400

    add documentation

--- serviceFabric/sf-hns-tracing.ps1
+++ serviceFabric/sf-hns-tracing.ps1
@@ -1,25 +1,55 @@
 <#
 .SYNOPSIS 
-    service fabric hns etl tracing script
+service fabric hns etl tracing script
+
+.DESCRIPTION
+script will create a permanent ETL tracing session using powershell Autologger cmdlets.
+default destination ($traceFilePath) is configured location used by FabricDCA for log staging.
+files saved in D:\SvcFab\Log\CrashDumps\ will by uploaded by FabricDCA to 'sflogs' storage account.
+after upload, local files will be deleted by FabricDCA automatically.
+default argument values should work for most scenarios.
+add / remove etw tracing guids as needed. see get-etwtraceprovider / logman query providers
+to remove tracing use -remove switch.
+
+https://docs.microsoft.com/powershell/module/eventtracingmanagement/new-autologgerconfig
 
 .LINK
 [net.servicePointManager]::Expect100Continue = $true;[net.servicePointManager]::SecurityProtocol = [net.SecurityProtocolType]::Tls12;
-    invoke-webRequest "https://raw.githubusercontent.com/jagilber/powershellScripts/master/serviceFabric/sf-hns-tracing.ps1" -outFile "$pwd\sf-hns-tracing.ps1";
-    .\sf-hns-tracing.ps1
+invoke-webRequest "https://raw.githubusercontent.com/jagilber/powershellScripts/master/serviceFabric/sf-hns-tracing.ps1" -outFile "$pwd\sf-hns-tracing.ps1";
+.\sf-hns-tracing.ps1
 
 #>
+
 [cmdletbinding()]
 param(
-    $logFileMode = 8, # new file
+    # new file https://docs.microsoft.com/windows/win32/etw/logging-mode-constants
+    $logFileMode = 8,
+
+    # output file name and path
     $traceFilePath = 'D:\SvcFab\Log\CrashDumps\hns%d.etl',
+    
+    # output file size in MB
     $maxFileSizeMb = 64,
+    
+    # max ETW trace buffers
     $maxBuffers = 16,
+    
+    # buffer size in MB
     $bufferSize = 1024,
+    
+    # ETW trace session name
     $traceName = 'hns',
+    
+    # 6 == everything
     $level = 6,
-    $keyword = 18446744073709551615, # 0xFFFFFFFFFFFFFFFF
+    
+    # 0xFFFFFFFFFFFFFFFF == everything
+    $keyword = 18446744073709551615,
+    
+    # remove tracing session
     [switch]$remove
 )
+
 $error.Clear()
 
 $guids = @(
@@ -60,6 +90,7 @@ New-AutologgerConfig -Name $traceName ``
     -MaximumBuffers $maxBuffers ``
     -BufferSize $bufferSize
 "
+
 New-AutologgerConfig -Name $traceName `
     -LogFileMode $logFileMode `
     -LocalFilePath $traceFilePath `
@@ -82,7 +113,7 @@ foreach ($guid in $guids) {
 
 }
 
-Get-AutologgerConfig -Name $traceName | fl *
-logman query -ets $traceName
+Get-AutologgerConfig -Name $traceName | format-list *
+logman query -ets #$traceName
 write-host "finished"
 
