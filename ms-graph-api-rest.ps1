553324ff1dfb9e3960c82e4084cd6233a63f1f1b
jason gilbertson <jagilber@microsoft.com>
2022-06-19 15:21:19 -0400

    cleanup

--- ms-graph-api-rest.ps1
+++ ms-graph-api-rest.ps1
@@ -7,12 +7,9 @@ test graph rest script
     have to set 'api permissions' on app registration. add 'microsoft graph' 'application permissions'
     https://docs.microsoft.com/en-us/graph/auth-v2-user
     https://login.microsoftonline.com/common/adminconsent?client_id={client-id}
-    https://login.microsoftonline.com/common/adminconsent?client_id=
     https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-auth-code-flow
     https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-client-creds-grant-flow
-    https://login.microsoftonline.com/common/adminconsent?client_id=6731de76-14a6-49ae-97bc-6eba6914391e&state=12345&redirect_uri=http://localhost/myapp/permissions
-    https://stackoverflow.com/questions/66106927/webview2-in-powershell-winform-gui
-    BASE64URL-ENCODE(SHA256(ASCII(code_verifier)))
+    
     https://docs.microsoft.com/en-us/azure/active-directory/develop/sample-v2-code
 
     https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-protocols-oidc
@@ -45,7 +42,7 @@ param(
         'consistencylevel' = 'eventual'
         'content-type'     = $contentType
     },
-    $scope = 'user.read openid profile Application.ReadWrite.All User.ReadWrite.All', # 'https://graph.microsoft.com/.default', #'Application.Read.All offline_access user.read mail.read',
+    $scope = 'user.read openid profile Application.ReadWrite.All User.ReadWrite.All Directory.ReadWrite.All', # 'https://graph.microsoft.com/.default', #'Application.Read.All offline_access user.read mail.read',
     [ValidateSet('urn:ietf:params:oauth:grant-type:device_code', 'client_credentials', 'authorization_code')]
     $grantType = 'urn:ietf:params:oauth:grant-type:device_code', #'client_credentials', #'authorization_code'
     #$redirectUrl = 'http://localhost',
@@ -74,7 +71,7 @@ function get-restAuth() {
     # so cannot use internally
     write-host "auth request"
     $error.clear()
-    $endpoint = "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/devicecode"
+    $uri = "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/devicecode"
 
     $Body = @{
         'client_id' = $clientId
@@ -85,15 +82,16 @@ function get-restAuth() {
         ContentType = 'application/x-www-form-urlencoded'
         Body        = $Body
         Method      = 'post'
-        URI         = $endpoint
+        URI         = $uri
     }
     
-    write-host ($params | convertto-json)
+    Write-Verbose ($params | convertto-json)
     $error.Clear()
-
+    write-host "invoke-restMethod $uri" -ForegroundColor Cyan
     $global:authresult = Invoke-RestMethod @params -Verbose -Debug
     write-host "auth result: $($global:authresult | convertto-json)"
     write-host "rest auth finished"
+
     return ($global:authresult -ne $null)
 }
 
@@ -106,7 +104,7 @@ function get-restToken() {
     write-host "rest logon"
     $global:logonResult = $null
     $error.clear()
-    $endpoint = "https://login.windows.net/$tenantId/oauth2/v2.0/token"
+    $uri = "https://login.windows.net/$tenantId/oauth2/v2.0/token"
     $headers = @{
         'content-type' = 'application/x-www-form-urlencoded'
         'accept'       = $contentType
@@ -122,10 +120,11 @@ function get-restToken() {
         Headers = $headers 
         Body    = $Body
         Method  = 'Post'
-        URI     = $endpoint
+        URI     = $uri
     }
 
     write-verbose ($params | convertto-json)
+    write-host "invoke-restMethod $uri" -ForegroundColor Cyan
 
     while ($startTime -gt (get-date)) {
         $error.Clear()
@@ -162,7 +161,7 @@ function call-graphQuery () {
     #
     # call graph api
     #
-    write-host "getting service fabric service connection"
+    write-host "executing graph query"
     $adoAuthHeader = $headers.Clone()
     $adoAuthHeader.authorization = "Bearer $global:accessToken"
     $uri = $graphApiUrl + $query
@@ -177,22 +176,22 @@ function call-graphQuery () {
             Body        = $body
         }
 
-        write-host "graph connection parameters: $($parameters | convertto-json)"
-        write-host "invoke-restMethod -uri $([system.web.httpUtility]::UrlDecode($parameters.Uri)) -headers $adoAuthHeader"
+        Write-Verbose "graph connection parameters: $($parameters | convertto-json)"
+        write-host "invoke-restMethod $uri" -ForegroundColor Cyan
         $error.clear()
         $global:restQuery = invoke-restMethod @parameters
-        $global:restResults.Add($global:restQuery.value)
+        [void]$global:restResults.Add($global:restQuery.value)
         write-host "rest result: $($global:restQuery| convertto-json)"
 
         if ($error) {
             write-error "exception: $($error | out-string)"
             return $null
         }
-        if(!($restQuery.'@odata.nextLink')){
+        if (!($restQuery.'@odata.nextLink')) {
             write-verbose "no next link"
             break
         }
-        else{
+        else {
             $uri = $restQuery.'@odata.nextLink'
         }
     }
