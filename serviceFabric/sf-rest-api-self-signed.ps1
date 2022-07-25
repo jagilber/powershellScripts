0aaad2a1f55689b242ba55e6cad393cb24098be2
jason gilbertson <jagilber@microsoft.com>
2022-07-25 18:24:48 -0400

    add store name and output

--- serviceFabric/sf-rest-api-self-signed.ps1
+++ serviceFabric/sf-rest-api-self-signed.ps1
@@ -1,19 +1,25 @@
+<#
 # example script to query service fabric api on localhost using self signed cert
 # docs.microsoft.com/en-us/rest/api/servicefabric/sfclient-index
 
+[net.servicePointManager]::Expect100Continue = $true;[net.servicePointManager]::SecurityProtocol = [net.SecurityProtocolType]::Tls12;
+invoke-webRequest "https://raw.githubusercontent.com/jagilber/powershellScripts/master/serviceFabric/sf-rest-api-self-signed.ps1" -outFile "$pwd/sf-rest-api-self-signed.ps1";
+./sf-rest-api-self-signed.ps1
+
+#>
 param(
     $gatewayCertThumb = "xxxxx",
     $startTime = (get-date).AddDays(-7).ToString("yyyy-MM-ddTHH:mm:ssZ"),
     $endTime = (get-date).ToString("yyyy-MM-ddTHH:mm:ssZ"),
     $timeoutSec = 100,
     $apiVer = "6.2-preview",
-    $gatewayHost = "https://localhost:19080"
+    $gatewayHost = "https://localhost:19080",
+    [ValidateSet('CurrentUser', 'LocalMachine')]
+    $store = 'CurrentUser'
 )
 
 Clear-Host
 $ErrorActionPreference = "continue"
-$result = $Null
-$cert = Get-ChildItem -Path cert: -Recurse | Where-Object Thumbprint -eq $gatewayCertThumb
 
 # to bypass self-signed cert 
 add-type @"
@@ -32,56 +38,68 @@ add-type @"
 
 [System.Net.ServicePointManager]::CertificatePolicy = new-object IDontCarePolicy 
 
-$eventArgs = "api-version=$($apiVer)&timeout=$($timeoutSec)&StartTimeUtc=$($startTime)&EndTimeUtc=$($endTime)"
+function main() {
+    $result = $Null
+    $cert = Get-ChildItem -Path cert:\$store -Recurse | Where-Object Thumbprint -eq $gatewayCertThumb
 
-$url = "$($gatewayHost)/EventsStore/Cluster/Events?$($eventArgs)"
-$result = Invoke-RestMethod  -TimeoutSec 30 -UseBasicParsing -Method Get -Certificate $cert -Uri $url
-$result |fl *
-$result = $Null
+    $eventArgs = "api-version=$($apiVer)&timeout=$($timeoutSec)&StartTimeUtc=$($startTime)&EndTimeUtc=$($endTime)"
 
-
-$url = "$($gatewayHost)/EventsStore/Nodes/Events?$($eventArgs)"
-$result = Invoke-RestMethod  -TimeoutSec 30 -UseBasicParsing -Method Get -Certificate $cert -Uri $url
-$result |fl *
-$result = $Null
-
-
-$url = "$($gatewayHost)/EventsStore/Applications/Events?$($eventArgs)"
-$result = Invoke-RestMethod  -TimeoutSec 30 -UseBasicParsing -Method Get -Certificate $cert -Uri $url
-$result |fl *
-$result = $Null
+    $url = "$($gatewayHost)/EventsStore/Cluster/Events?$($eventArgs)"
+    $result = call-rest -url $url -cert $cert
+    $result = call-rest -url $url -cert $cert
+    $result | Format-List *
+    $result = $Null
 
 
-$url = "$($gatewayHost)/EventsStore/Services/Events?$($eventArgs)"
-$result = Invoke-RestMethod  -TimeoutSec 30 -UseBasicParsing -Method Get -Certificate $cert -Uri $url
-$result |fl *
-$result = $Null
+    $url = "$($gatewayHost)/EventsStore/Nodes/Events?$($eventArgs)"
+    $result = call-rest -url $url -cert $cert
+    $result | Format-List *
+    $result = $Null
 
 
-$url = "$($gatewayHost)/EventsStore/Partitions/Events?$($eventArgs)"
-$result = Invoke-RestMethod  -TimeoutSec 30 -UseBasicParsing -Method Get -Certificate $cert -Uri $url
-$result |fl *
-$result = $Null
+    $url = "$($gatewayHost)/EventsStore/Applications/Events?$($eventArgs)"
+    $result = call-rest -url $url -cert $cert
+    $result | Format-List *
+    $result = $Null
 
-$eventArgs = "api-version=$($apiVer)&timeout=$($timeoutSec)"
-$url = "$($gatewayHost)/ImageStore?$($eventArgs)"
-$result = Invoke-RestMethod  -TimeoutSec 30 -UseBasicParsing -Method Get -Certificate $cert -Uri $url
-$result |fl *
-$result.StoreFiles
-$result.StoreFolders
-$result = $Null
 
-$url = "$($gatewayHost)/$/GetClusterManifest?$($eventArgs)"
-$result = Invoke-RestMethod  -TimeoutSec 30 -UseBasicParsing -Method Get -Certificate $cert -Uri $url
-#$result |fl *
-$result.manifest
-$result = $Null
+    $url = "$($gatewayHost)/EventsStore/Services/Events?$($eventArgs)"
+    $result = call-rest -url $url -cert $cert
+    $result | Format-List *
+    $result = $Null
 
-$url = "$($gatewayHost)/$/GetClusterHealth?$($eventArgs)"
-$result = Invoke-RestMethod  -TimeoutSec 30 -UseBasicParsing -Method Get -Certificate $cert -Uri $url
-$result |fl *
-$result = $Null
 
-$url = "$($gatewayHost)/Nodes?$($eventArgs)"
-$result = Invoke-RestMethod  -TimeoutSec 30 -UseBasicParsing -Method Get -Certificate $cert -Uri $url
-$result.items | fl *
+    $url = "$($gatewayHost)/EventsStore/Partitions/Events?$($eventArgs)"
+    $result = call-rest -url $url -cert $cert
+    $result | Format-List *
+    $result = $Null
+
+    $eventArgs = "api-version=$($apiVer)&timeout=$($timeoutSec)"
+    $url = "$($gatewayHost)/ImageStore?$($eventArgs)"
+    $result = call-rest -url $url -cert $cert
+    $result | Format-List *
+    $result.StoreFiles
+    $result.StoreFolders
+    $result = $Null
+
+    $url = "$($gatewayHost)/$/GetClusterManifest?$($eventArgs)"
+    $result = call-rest -url $url -cert $cert
+    #$result |fl *
+    $result.manifest
+    $result = $Null
+
+    $url = "$($gatewayHost)/$/GetClusterHealth?$($eventArgs)"
+    $result = call-rest -url $url -cert $cert
+    $result | Format-List *
+    $result = $Null
+
+    $url = "$($gatewayHost)/Nodes?$($eventArgs)"
+    $result = call-rest -url $url -cert $cert
+    $result.items | Format-List *
+}
+
+function call-rest($url,$cert){
+    write-host "Invoke-RestMethod  -TimeoutSec 30 -UseBasicParsing -Method Get -Certificate $cert -Uri $url" -ForegroundColor Cyan
+    return Invoke-RestMethod  -TimeoutSec 30 -UseBasicParsing -Method Get -Certificate $cert -Uri $url
+}
+main
