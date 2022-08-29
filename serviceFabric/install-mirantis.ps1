4770659bb118792bbba5afbb715024bd771481b8
Jason Gilbertson <jagilber@users.noreply.github.com>
2022-08-29 17:10:59 -0400

    add MIT

--- serviceFabric/install-mirantis.ps1
+++ serviceFabric/install-mirantis.ps1
@@ -5,7 +5,29 @@
     save script file to url that vmss nodes have access to during provisioning
     
 .NOTES
-    v 1.0.2
+v 1.0.3
+
+# The MIT License (MIT)
+#
+# Copyright (c) 2015 Microsoft Azure
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 
 parameters.json :
@@ -111,11 +133,11 @@ function main() {
     $version = set-dockerVersion -dockerVersion $dockerVersion
     $installedVersion = get-dockerVersion
 
-    if($hypervIsolation) {
+    if ($hypervIsolation) {
         $hypervInstalled = (get-windowsFeature -name hyper-v).Installed
         write-host "hyper-v feature installed:$hypervInstalled"
         
-        if(!$uninstall -and !$hypervInstalled) {
+        if (!$uninstall -and !$hypervInstalled) {
             write-host "installing hyper-v features"
             install-windowsfeature -name hyper-v
             install-windowsfeature -name rsat-hyper-v-tools
