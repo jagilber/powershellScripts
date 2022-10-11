d46b3b7f31e4f5a9e3d2aff9122a14e335c51f04
Jason Gilbertson <jagilber@users.noreply.github.com>
2022-10-11 15:42:17 -0400

    cleanup

--- temp/task.ps1
+++
@@ -1,9 +0,0 @@
-Out-File -InputObject "$(get-date)" -FilePath c:\task-scripts\test.log
-
-New-WinEvent -ProviderName Microsoft-Windows-Powershell `
-    -id 4102 `
-    -Payload @(
-    "context:`r`n$(($MyInvocation | convertto-json -Depth 1))", 
-    "user data:`r`n$(([environment]::GetEnvironmentVariables() | convertto-json))", 
-    "task.ps1`r`nerror:`r`n$(($error | convertto-json -Depth 1))"
-)
