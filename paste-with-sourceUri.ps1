fbf41ce7e7c15c1b7cd40ef2ff4858042246554d
Jason Gilbertson <jagilber@users.noreply.github.com>
2022-10-11 10:01:04 -0400

    init

---
+++ paste-with-sourceUri.ps1
@@ -0,0 +1,31 @@
+<#
+update clipboard contents to add sourceuri similar to onenote
+example autohotkey:
+f8::
+	RunWait, pwsh -WindowStyle Hidden -NonInteractive -NoProfile -File i:\githubshared\jagilber\powershellscripts\paste-with-sourceUri.ps1
+	SendInput ^V
+	return
+#>
+
+[cmdletbinding()]
+param(
+
+)
+
+add-type -assemblyname system.windows.forms
+
+$html = [System.Windows.Forms.Clipboard]::GetText([System.Windows.Forms.TextDataFormat]::Html)
+$text = [System.Windows.Forms.Clipboard]::GetText([System.Windows.Forms.TextDataFormat]::Text)
+
+write-verbose $html
+
+if([regex]::isMatch($html,'SourceURL:')){
+    $sourceUrl = [regex]::match($html,'(SourceURL:.+)').Groups[1].value
+    $sourceUrl
+}
+
+if($sourceUrl -and !([regex]::isMatch($text,'SourceURL:'))){
+    $text = $text + [environment]::newLine + [environment]::newLine + $sourceUrl + [environment]::newLine
+    write-verbose $text
+    [System.Windows.Forms.Clipboard]::SetText($text)
+}
