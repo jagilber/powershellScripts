9450d58d893fb903b86510f8078f70553f1e3adc
Jason Gilbertson <jagilber@users.noreply.github.com>
2022-10-13 09:59:57 -0400

    add mit

--- serviceFabric/vmss-cse-tls.ps1
+++ serviceFabric/vmss-cse-tls.ps1
@@ -8,8 +8,31 @@
     You can use the -SetCipherOrder (or -sco) option to also set the TLS cipher
     suite order. Change the cipherorder variable below to the order you want to set on the
     server. Setting this requires a reboot to take effect.
-    v 1.0
     
+    v1.0
+
+    # The MIT License (MIT)
+    #
+    # Copyright (c) 2015 Microsoft Azure
+    #
+    # Permission is hereby granted, free of charge, to any person obtaining a copy
+    # of this software and associated documentation files (the "Software"), to deal
+    # in the Software without restriction, including without limitation the rights
+    # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    # copies of the Software, and to permit persons to whom the Software is
+    # furnished to do so, subject to the following conditions:
+    # 
+    # The above copyright notice and this permission notice shall be included in all
+    # copies or substantial portions of the Software.
+    # 
+    # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    # SOFTWARE.
+
 .LINK
 [net.servicePointManager]::Expect100Continue = $true;[net.servicePointManager]::SecurityProtocol = [net.SecurityProtocolType]::Tls12;
 invoke-webRequest "https://raw.githubusercontent.com/jagilber/powershellScripts/master/serviceFabric/vmss-cse-tls.ps1" -outFile "$pwd/vmss-cse-tls.ps1";
