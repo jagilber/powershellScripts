2f284e0e4a0d23a9e4f7ffeb674d29a27c65dd7c
Jason Gilbertson <jagilber@users.noreply.github.com>
2023-03-06 13:51:15 -0500

    add $global:output

--- convert-string.ps1
+++ convert-string.ps1
@@ -14,44 +14,52 @@ param(
 
 if($stringToBase64){
     write-host "stringToBase64: [convert]::ToBase64String([text.encoding]::UTF8.GetBytes(`$inputString))" -ForegroundColor Green
-    write-host "stringToBase64: $([convert]::ToBase64String([text.encoding]::UTF8.GetBytes($inputString)))"
+    $global:output = [convert]::ToBase64String([text.encoding]::UTF8.GetBytes($inputString))
+    write-host "stringToBase64: $($global:output)"
 }
 
 if($base64ToString){
     write-host "base64ToString: [text.encoding]::UTF8.GetString([convert]::FromBase64String(`$inputString))" -ForegroundColor Green
-    write-host "base64ToString: $([text.encoding]::UTF8.GetString([convert]::FromBase64String($inputString)))"
+    $global:output = [text.encoding]::UTF8.GetString([convert]::FromBase64String($inputString))
+    write-host "base64ToString: $($global:output)"
 }
 
 if($urlEncodeToString) {
     Write-Host "urlEncodeToString: [web.httpUtility]::UrlDecode(`$inputString)" -ForegroundColor Green
-    Write-Host "urlEncodeToString: $([web.httpUtility]::UrlDecode($inputString))"
+    $global:output = [web.httpUtility]::UrlDecode($inputString)
+    Write-Host "urlEncodeToString: $($global:output)"
 }
 
 if($stringToUrlEncode) {
     Write-Host "stringToUrlEncode: [web.httpUtility]::UrlEncode(`$inputString)" -ForegroundColor Green
-    Write-Host "stringToUrlEncode: $([web.httpUtility]::UrlEncode($inputString))"
+    $global:output = [web.httpUtility]::UrlEncode($inputString)
+    Write-Host "stringToUrlEncode: $($global:output)"
 }
 
 if($stringToRegexEscaped) {
     Write-Host "stringToRegexEscaped: [regex]::Escape(`$inputString)" -ForegroundColor Green
-    Write-Host "stringToRegexEscaped: $([regex]::Escape($inputString))"
+    $global:output = [regex]::Escape($inputString)
+    Write-Host "stringToRegexEscaped: $($global:output)"
 }
 
 if($regexToStringUnescaped) {
     Write-Host "regexToStringUnescaped: [regex]::Unescape(`$inputString)" -ForegroundColor Green
-    Write-Host "regexToStringUnescaped: $([regex]::Unescape($inputString))"
+    $global:output = [regex]::Unescape($inputString)
+    Write-Host "regexToStringUnescaped: $($global:output)"
 }
 
 if($stringToCharArray) {
     write-host "stringToCharArray: `$inputString.ToCharArray() | foreach-object {[int][char]`$_}" -ForegroundColor Green
-    write-host "stringToCharArray: $($inputString.ToCharArray() | foreach-object {[int][char]$_})"
+    $global:output = $inputString.ToCharArray() | foreach-object {[int][char]$_}
+    write-host "stringToCharArray: $($global:output)"
 }
 
 if($charArrayToString) {
     write-host "charArrayToString: [string]::new(`$inputString)" -ForegroundColor Green
-    write-host "charArrayToString: $([string]::new($inputString))"
+    $global:output = [string]::new($inputString)
+    write-host "charArrayToString: $($global:output)"
 }
 
-#write-host "finished"
+write-host "output stored in `$global:output" -ForegroundColor Cyan
 
 
