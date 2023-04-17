2f03ca5c5cd153bcbaffd44790c6a03ac6b8f233
Jason Gilbertson <jagilber@users.noreply.github.com>
2023-04-17 00:17:50 -0400

    add dockerce switch

--- temp/install-mirantis.ps1
+++ temp/install-mirantis.ps1
@@ -123,13 +123,14 @@ param(
     [switch]$noRestart,
     [switch]$noExceptionOnError,
     [bool]$registerEvent = $true,
-    [string]$registerEventSource = 'CustomScriptExtension'
+    [string]$registerEventSource = 'CustomScriptExtension',
+    [string]$offlineFile = "$psscriptroot/Docker.zip"
 )
 
 #$PSModuleAutoLoadingPreference = 2
 #$ErrorActionPreference = 'continue'
-[System.Net.ServicePointManager]::Expect100Continue = $true;
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
+[Net.ServicePointManager]::Expect100Continue = $true;
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
 
 $eventLogName = 'Application'
 $dockerProcessName = 'dockerd'
@@ -138,14 +139,19 @@ $transcriptLog = "$psscriptroot\transcript.log"
 $defaultDockerExe = 'C:\Program Files\Docker\dockerd.exe'
 $nullVersion = '0.0.0.0'
 $versionMap = @{}
+$mirantisRepo = 'https://repos.mirantis.com'
 $dockerCeRepo = 'https://download.docker.com'
+$dockerPackageAbsolutePath = 'win/static/stable/x86_64'
 
+$global:currentDockerVersions = @{}
+$global:currentContainerDVersions = @{}
+$global:downloadUrl = $mirantisRepo
 $global:restart = !$noRestart
 $global:result = $true
 
 function Main() {
 
-    $isAdmin = ([System.Security.Principal.WindowsPrincipal][System.Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([System.Security.Principal.WindowsBuiltInRole] "Administrator")
+    $isAdmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
 
     if (!$isAdmin) {
         Write-Error "Restart script as administrator."
@@ -156,18 +162,11 @@ function Main() {
     Start-Transcript -Path $transcriptLog
     $error.Clear()
 
-    $installFile = "$psscriptroot\$([System.IO.Path]::GetFileName($mirantisInstallUrl))"
+    $installFile = "$psscriptroot\$([IO.Path]::GetFileName($mirantisInstallUrl))"
     Write-Host "Installation file:$installFile"
 
     if (!(Test-Path $installFile)) {
-        "Downloading [$url]`nSaving at [$installFile]" 
-        Write-Host "$result = [System.Net.WebClient]::New().DownloadFile($mirantisInstallUrl, $installFile)"
-        $global:result = [System.Net.WebClient]::new().DownloadFile($mirantisInstallUrl, $installFile)
-        Write-Host "DownloadFile result:$($result | Format-List *)"
-        if ($error) {
-            Write-Error "failure downloading file:$($error | out-string)"
-            $global:result = $false
-        }
+        Download-File -url $mirantisInstallUrl -outputFile $installFile
     }
 
     # temp fix
@@ -214,20 +213,25 @@ function Main() {
             $noServiceStarts = "-NoServiceStarts "
         }
 
-        $downloadUrl = $null
+        $global:downloadUrl = $mirantisRepo
         if ($dockerCe) {
-            $downloadUrl = "-DownloadUrl $dockerCeRepo "
+            $global:downloadUrl = $dockerCeRepo
         }
+
+        # download docker outside mirantis script
+        $downloadFile = $global:currentDockerVersions.Item($version)
+
+        Download-File -url "$global:downloadUrl/$dockerPackageAbsolutePath/$downloadFile" -outputFile $offlineFile
 
         # docker script will always emit errors checking for files even when successful
         Write-Host "Installing docker."
         $scriptResult = Invoke-Script -script $installFile `
-            -arguments "-DockerVersion $($versionMap.($version.tostring())) $downloadUrl$engineOnly$noServiceStarts-Verbose 6>&1" `
+            -arguments "-DockerVersion $($version.tostring()) -OffLine -OffLinePackagesPath $psscriptroot $engineOnly$noServiceStarts-Verbose 6>&1" `
             -checkError $false
         
         $error.Clear()
         $finalVersion = Get-DockerVersion
-        if($finalVersion -eq $nullVersion) {
+        if ($finalVersion -eq $nullVersion) {
             $global:result = $false
         }
 
@@ -261,7 +265,7 @@ function Main() {
         Restart-Computer -Force
     }
 
-    if(!$noExceptionOnError -and !$global:result) {
+    if (!$noExceptionOnError -and !$global:result) {
         throw [Exception]::new("Exception $($MyInvocation.ScriptName)`n$($transcript)")
     }
     return $global:result
@@ -271,16 +275,16 @@ function Main() {
 function Add-UseBasicParsing($scriptFile) {
     $newLine
     $updated = $false
-    $scriptLines = [System.IO.File]::ReadAllLines($scriptFile)
-    $newScript = [System.Collections.ArrayList]::new()
+    $scriptLines = [IO.File]::ReadAllLines($scriptFile)
+    $newScript = [Collections.ArrayList]::new()
     Write-Host "Updating $scriptFile to use -UseBasicParsing for Invoke-WebRequest"
 
     foreach ($line in $scriptLines) {
         $newLine = $line
-        if ([System.Text.RegularExpressions.Regex]::IsMatch($line, 'Invoke-WebRequest', [Text.RegularExpressions.RegexOptions]::IgnoreCase)) {
+        if ([Text.RegularExpressions.Regex]::IsMatch($line, 'Invoke-WebRequest', [Text.RegularExpressions.RegexOptions]::IgnoreCase)) {
             Write-Host "Found command $line"
-            if (![System.Text.RegularExpressions.Regex]::IsMatch($line, '-UseBasicParsing', [Text.RegularExpressions.RegexOptions]::IgnoreCase)) {
-                $newLine = [System.Text.RegularExpressions.Regex]::Replace($line, 'Invoke-WebRequest', 'Invoke-WebRequest -UseBasicParsing', [Text.RegularExpressions.RegexOptions]::IgnoreCase)
+            if (![Text.RegularExpressions.Regex]::IsMatch($line, '-UseBasicParsing', [Text.RegularExpressions.RegexOptions]::IgnoreCase)) {
+                $newLine = [Text.RegularExpressions.Regex]::Replace($line, 'Invoke-WebRequest', 'Invoke-WebRequest -UseBasicParsing', [Text.RegularExpressions.RegexOptions]::IgnoreCase)
                 Write-Host "Updating command $line to $newLine"
                 $updated = $true
             }
@@ -289,7 +293,7 @@ function Add-UseBasicParsing($scriptFile) {
     }
 
     if ($updated) {
-        $newScriptContent = [string]::Join([System.Environment]::NewLine, $newScript.ToArray())
+        $newScriptContent = [string]::Join([Environment]::NewLine, $newScript.ToArray())
         $tempFile = "$scriptFile.oem"
         if ((Test-Path $tempFile)) {
             Remove-Item $tempFile -Force
@@ -301,22 +305,34 @@ function Add-UseBasicParsing($scriptFile) {
     }
 }
 
+function Download-File($url, $outputFile) {
+    Write-Host "$result = [Net.WebClient]::New().DownloadFile($url, $outputFile)"
+    $global:result = [Net.WebClient]::new().DownloadFile($url, $outputFile)
+    Write-Host "DownloadFile result:$($result | Format-List *)"
+
+    if ($error -or !(Test-Path $outputFile)) {
+        Write-Error "failure downloading file:$($error | out-string)"
+        $global:result = $false
+    }
+    return $global:result
+}
+
 # Get the docker version
 function Get-DockerVersion() {
-    $installedVersion = [System.Version]::new($nullVersion)
+    $installedVersion = [version]::new($nullVersion)
 
     if (Test-IsDockerRunning) {
         $path = (Get-Process -Name $dockerProcessName).Path
         Write-Host "Docker installed and running: $path"
         $dockerInfo = (docker version)
-        $installedVersion = [System.Version][System.Text.RegularExpressions.Regex]::Match($dockerInfo, 'Version:\s+?(\d.+?)\s').Groups[1].Value
+        $installedVersion = [version][Text.RegularExpressions.Regex]::Match($dockerInfo, 'Version:\s+?(\d.+?)\s').Groups[1].Value
     }
     elseif (Test-DockerIsInstalled) {
         $path = Get-WmiObject win32_service | Where-Object { $psitem.Name -like $dockerServiceName } | Select-Object PathName
         Write-Host "Docker exe path:$path"
-        $path = [System.Text.RegularExpressions.Regex]::Match($path.PathName, "`"(.+)`"").Groups[1].Value
+        $path = [Text.RegularExpressions.Regex]::Match($path.PathName, "`"(.+)`"").Groups[1].Value
         Write-Host "Docker exe clean path:$path"
-        $installedVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($path)
+        $installedVersion = [Diagnostics.FileVersionInfo]::GetVersionInfo($path)
         Write-Warning "Warning: docker installed but not running: $path"
     }
     else {
@@ -329,15 +345,15 @@ function Get-DockerVersion() {
 
 # Get the latest docker version
 function Get-LatestVersion([string[]] $versions) {
-    $latestVersion = [System.Version]::new()
+    $latestVersion = [version]::new()
     
     if (!$versions) {
-        return [System.Version]::new($nullVersion)
+        return [version]::new($nullVersion)
     }
 
     foreach ($version in $versions) {
         try {
-            $currentVersion = [System.Version]::new($version)
+            $currentVersion = [version]::new($version)
             if ($currentVersion -gt $latestVersion) {
                 $latestVersion = $currentVersion
             }
@@ -391,41 +407,53 @@ function Invoke-Script([string]$script, [string] $arguments, [bool]$checkError =
 # Set docker version parameter (script internally)
 function Set-DockerVersion($dockerVersion) {
     # install.ps1 using Write-Host to output string data. have to capture with 6>&1
-    $currentVersions = Invoke-Script -script $installFile -arguments '-ShowVersions 6>&1'
-    Write-Host "Current versions: $currentVersions"
-    
-    $version = [System.Version]::New($nullVersion)
-    $currentdockerVersions = @($currentVersions[0].ToString().TrimStart('docker:').Replace(" ", "").Split(","))
-    
-    # map string to [version] for 0's
-    foreach ($stringVersion in $currentdockerVersions) {
-        [void]$versionMap.Add([System.Version]::New($stringVersion).ToString(), $stringVersion)
+    # for docker ce and mirantis compat, query versions outside install.ps1
+
+    $result = Invoke-WebRequest -Uri "$global:downloadUrl/$dockerPackageAbsolutePath" -UseBasicParsing
+
+    $filePattern = '(?<file>(?<filetype>docker|containerd)-(?<major>\d+?)\.(?<minor>\d+?)\.(?<build>\d+?)\.zip)'
+    $linkMatches = [regex]::matches($result.Links.href, $filePattern, [text.regularexpressions.regexoptions]::IgnoreCase)
+
+    foreach ($match in $linkMatches) {
+        $major = $match.groups['major'].value
+        $minor = $match.groups['minor'].value
+        $build = $match.groups['build'].value
+        $version = [version]::new($major, $minor, $build)
+
+        $file = $match.groups['file'].value
+        $filetype = $match.groups['filetype'].value
+        
+        if ($filetype -ieq 'docker') {
+            [void]$global:currentDockerVersions.Add($version, $file)
+        }
+        else {
+            [void]$global:currentContainerDVersions.Add($version, $file)
+        }
     }
-    
-    Write-Host "Version map:`r`n$($versionMap | Out-String)"
-    Write-Host "Current docker versions: $currentdockerVersions"
-    
-    $latestdockerVersion = Get-LatestVersion -versions $currentdockerVersions
+
+    Write-Host "Current docker versions: $($global:currentDockerVersions | out-string)"
+
+    $latestDockerVersion = Get-LatestVersion -versions $global:currentDockerVersions.Keys
     Write-Host "Latest docker version: $latestdockerVersion"
-    
-    $currentContainerDVersions = @($currentVersions[1].ToString().TrimStart('containerd:').Replace(" ", "").Split(","))
-    Write-Host "Current containerd versions: $currentContainerDVersions"
+
+    #$currentContainerDVersions = @($currentVersions[1].ToString().TrimStart('containerd:').Replace(" ", "").Split(","))
+    Write-Host "Current containerd versions: $($currentContainerDVersions | out-string)"
 
     if ($dockerVersion -ieq 'latest' -or $allowUpgrade) {
         Write-Host "Setting version to latest"
-        $version = $latestdockerVersion
+        $version = $latestDockerVersion
     }
     else {
         try {
-            $version = [System.Version]::new($dockerVersion)
+            $version = [version]::new($dockerVersion)
             Write-Host "Setting version to `$dockerVersion ($dockerVersion)"
         }
         catch {
-            $version = [System.Version]::new($nullVersion)
+            $version = [version]::new($nullVersion)
             Write-Warning "Exception setting version to `$dockerVersion ($dockerVersion)`r`n$($error | Out-String)"
         }
     
-        if ($version -ieq [System.Version]::new($nullVersion)) {
+        if ($version -ieq [version]::new($nullVersion)) {
             $version = $latestdockerVersion
             Write-Host "Setting version to latest docker version $latestdockerVersion"
         }
