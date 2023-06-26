9ced9626ca896596400dcfab213ff1e3b6f40f3d
Jason Gilbertson <jagilber@users.noreply.github.com>
2023-06-26 11:07:40 -0400

    cleanup add start backup and other functions. needs restore function

--- serviceFabric/drafts/azure-az-sf-restore-backup.ps1
+++ serviceFabric/drafts/azure-az-sf-restore-backup.ps1
@@ -1,61 +1,107 @@
 <#
 .Synopsis
-    script to restore service fabric cluster stateful applications from backup
-
+    ##DRAFT## script will be to restore service fabric cluster stateful applications from backup
+.NOTES
+    File Name: azure-az-sf-restore-backup.ps1
+    Author   : jagilber
+    Requires : azure az modules
+  todo: add restore functions
 .LINK
     [net.servicePointManager]::Expect100Continue = $true;[net.servicePointManager]::SecurityProtocol = [net.SecurityProtocolType]::Tls12;
     invoke-webRequest "https://raw.githubusercontent.com/jagilber/powershellScripts/master/serviceFabric/drafts/azure-az-sf-restore-backup.ps1" -outFile "$pwd/azure-az-sf-restore-backup.ps1";
     ./azure-az-sf-restore-backup.ps1
 
+    https://learn.microsoft.com/en-us/rest/api/servicefabric/
+    https://learn.microsoft.com/en-us/rest/api/servicefabric/sfclient-index-backuprestore
     https://github.com/MicrosoftDocs/azure-docs/blob/main/articles/service-fabric/service-fabric-backup-restore-service-trigger-restore.md
+
+.DESCRIPTION
+    script to restore service fabric cluster stateful applications from backup
+.PARAMETER connectionEndpoint
+    service fabric cluster connection endpoint
+.PARAMETER applicationName
+    service fabric application name
+.PARAMETER applicationId
+    service fabric application id
+.PARAMETER backupId
+    service fabric backup id
+.PARAMETER backupStorageAccountName
+    service fabric backup storage account name
+.PARAMETER thumbprint
+    service fabric cluster certificate thumbprint
+.PARAMETER containerName
+    service fabric backup container name
+.PARAMETER backupStorageAccountKey
+    service fabric backup storage account key
+.PARAMETER resourceGroupName
+    azure resource group name
+.PARAMETER apiVersion
+    service fabric api version
+.PARAMETER timeoutSeconds
+    service fabric api timeout in seconds
+.PARAMETER backupTimeoutMinutes
+    service fabric backup timeout in minutes
+.PARAMETER enableBackups
+    enable service fabric backups
+.PARAMETER startBackup
+    start service fabric backup
+.PARAMETER loadFunctionsOnly
+    load functions only
+.EXAMPLE
+    ./azure-az-sf-restore-backup.ps1 -connectionEndpoint 'https://sfcluster.eastus.cloudapp.azure.com:19080' -applicationName 'fabric:/Voting' -applicationId '' -backupId '' -backupStorageAccountName '' -thumbprint '' -containerName 'backup' -backupStorageAccountKey '' -resourceGroupName 'sfcluster' -apiVersion '9.1' -timeoutSeconds '3' -backupTimeoutMinutes '10' -enableBackups -startBackup -loadFunctionsOnly
+.EXAMPLE
+    ./azure-az-sf-restore-backup.ps1 -connectionEndpoint 'https://sfcluster.eastus.cloudapp.azure.com:19080' -applicationName 'fabric:/Voting' -applicationId '' -backupId '' -backupStorageAccountName '' -thumbprint '' -containerName 'backup' -backupStorageAccountKey '' -resourceGroupName 'sfcluster' -apiVersion '9.1' -timeoutSeconds '3' -backupTimeoutMinutes '10' -enableBackups -startBackup
 #>
 
 [cmdletbinding()]
 param(
-  $connectionEndpoint = 'https://sfjagilber1nt5.eastus.cloudapp.azure.com:19080',
+  $connectionEndpoint = 'https://sfcluster.eastus.cloudapp.azure.com:19080',
   $applicationName = 'fabric:/Voting',
-  $applicationId = 'Voting~VotingData',
-  $backupLocation = "https://$backupStorageAccountName.blob.core.windows.net/$containerName",
+  $applicationId = '', #'Voting~VotingData',
   $backupId = '',
   $backupStorageAccountName = '',
   $thumbprint = '',
   $containerName = 'backup',
   $backupStorageAccountKey = '',
-  $resourceGroupName = 'sfjagilber1nt5',
+  $resourceGroupName = 'sfcluster',
   $apiVersion = '9.1',
   $timeoutSeconds = '3',
-  [bool]$enableBackups = $true
+  $backupTimeoutMinutes = '10', # default is 10 minutes
+  [switch]$enableBackups,
+  [switch]$startBackup,
+  [switch]$loadFunctionsOnly
 )
 
 $PSModuleAutoLoadingPreference = 'auto'
 $global:httpStatusCode = 0
-$sfHttpModule = 'Microsoft.ServiceFabric.Powershell.Http'
+$maxResults = 100
 $baseUrl = "$connectionEndpoint{0}?api-version=$apiVersion&timeout=$timeoutSeconds{1}"
 
 function main() {
   write-host "starting $((get-date).tostring())" -foregroundColor yellow
 
-  if (!(get-azresourcegroup)) {
-    connect-azaccount
-  }
-
-  if (!(get-azresourcegroup)) {
-    write-host "failed to connect to azure" -foregroundColor red
-    return
-  }
-
   #get key if not provided
   if (!$backupStorageAccountKey) {
+    if (!(get-azresourcegroup)) {
+      connect-azaccount
+    }
+
+    if (!(get-azresourcegroup)) {
+      write-host "failed to connect to azure" -foregroundColor red
+      return
+    }
+
     $backupStorageAccountKey = (Get-AzStorageAccountKey -ResourceGroupName $resourceGroupName -Name $backupStorageAccountName).Value[0]
-    
+
   }
+
   if (!$backupStorageAccountKey) {
     write-host "failed to get storage account key" -foregroundColor red
     return
   }
 
   write-host "get-childitem -Path Cert:\CurrentUser -Recurse | Where-Object Thumbprint -eq $thumbprint"
-  $cert = Get-ChildItem -Path Cert:\CurrentUser -Recurse | Where-Object Thumbprint -eq $thumbprint
+  $cert = Get-ChildItem -Path Cert:\ -Recurse | Where-Object Thumbprint -eq $thumbprint
   if (!$cert) {
     write-host "failed to get certificate for thumbprint: $thumbprint" -foregroundColor red
     return
@@ -67,7 +113,7 @@ function main() {
     write-host "issue date: $($cert.NotBefore)" -foregroundColor yellow
     write-host "expiration date: $($cert.NotAfter)" -foregroundColor yellow
   }
-  
+
   if (!(get-clusterHealth)) {
     write-host "failed to get cluster health" -foregroundColor red
     return
@@ -78,13 +124,40 @@ function main() {
     #return
   }
 
+  if(!$enableBackups -or !$startBackup) {
+    return
+  }
+
+  if (!$applicationName) {
+    $applicationList = get-clusterApplicationList
+    if (!$applicationList) {
+      write-host "failed to get application list" -foregroundColor red
+      return
+    }
+    write-host "select application to backup: $($applicationList | convertto-json)"
+    write-host "application name is required" -foregroundColor red
+    return
+  }
+  if(!$applicationId){
+    $applicationId = (get-clusterApplicationList $applicationName).Id
+  }
+
   if ($enableBackups) {
     if (!(create-clusterDailyAzureBackupPolicy)) {
       write-host "failed to create cluster backup policy" -foregroundColor red
       return
     }
+
+
     if (!(enable-clusterApplicationBackup $applicationId)) {
       write-host "failed to enable cluster backups" -foregroundColor red
+      return
+    }
+  }
+
+  if($startBackup) {
+    if (!(start-partitionBackup $partitionId)) {
+      write-host "failed to start partition backup" -foregroundColor red
       return
     }
   }
@@ -118,7 +191,7 @@ function create-clusterDailyAzureBackupPolicy() {
         RetentionDuration      = 'P3M'
       }
     })
-  
+
   if ($global:httpStatusCode -eq 201) {
     write-host "created backup policy" -foregroundColor Green
     return $true
@@ -133,6 +206,13 @@ function enable-clusterApplicationBackup($applicationId) {
   $response = invoke-rest -method 'POST' -absolutePath "/Applications/$applicationId/$/EnableBackup" -body (convertto-json @{
       BackupPolicyName = "DailyAzureBackupPolicy"
     })
+  return $response
+}
+
+function get-clusterApplicationBackupConfiguration($applicationId) {
+  $response = invoke-rest -method 'GET' -absolutePath "/Applications/$applicationId/$/GetBackupConfigurationInfo" -attributes @{
+    MaxResults = $maxResults
+  }
   return $response
 }
 
@@ -155,7 +235,7 @@ function get-clusterBackups() {
     write-host ($response.Items | convertto-json) -foregroundColor Green
     $BackupPoints = (ConvertFrom-Json $response.Items.BackupMetadata.BackupChainInfo.BackupPoints)
     $BackupPoints.Items
-  
+
   }
   else {
     write-host "no backups found" -foregroundColor red
@@ -171,16 +251,37 @@ function get-clusterHealth() {
   return $response
 }
 
+function get-clusterApplicationList($applicationName = "`"`"") {
+
+  $response = invoke-rest -method 'GET' -absolutePath "/Applications" -attributes @{
+    MaxResults                   = $maxResults
+    ApplictionTypeName           = $applicationName
+    ExcludeApplicationParameters = $true
+  }
+  return $response
+}
+
+function get-clusterPartitionList($serviceId) {
+  $response = invoke-rest -method 'GET' -absolutePath "/Services/$serviceId/$/GetPartitions"
+  return $response
+}
+
+function get-clusterServiceList($applicationId) {
+  $response = invoke-rest -method 'GET' -absolutePath "/Applications/$applicationId/$/GetServices"
+  return $response
+}
+
 function invoke-rest($method = 'GET', $absolutePath, $body = $null, $attributes = @{}) {
   $response = $null
   $attributesString = $attributes.GetEnumerator() | ForEach-Object { "&$($_.Name)=$($_.Value)" } | Join-String
+  $resultContent = $null
 
   try {
     $error.Clear()
     $url = $baseUrl -f $absolutePath, $attributesString
 
     if ($method -ieq 'POST') {
-      write-host "Invoke-WebRequest -Uri $url ``
+      write-host "Invoke-WebRequest -Uri '$url' ``
         -Method $method ``
         -ContentType 'application/json' ``
         -CertificateThumbprint $thumbprint ``
@@ -200,7 +301,7 @@ function invoke-rest($method = 'GET', $absolutePath, $body = $null, $attributes 
         -Body $body
     }
     else {
-      write-host "Invoke-WebRequest -Uri $url ``
+      write-host "Invoke-WebRequest -Uri '$url' ``
         -Method $method ``
         -CertificateThumbprint $thumbprint ``
         -UseBasicParsing ``
@@ -220,14 +321,20 @@ function invoke-rest($method = 'GET', $absolutePath, $body = $null, $attributes 
       return $null
     }
 
-    #format response
-    $responseJson = $response.Content | convertfrom-json | convertto-json
-    write-host "response: $responseJson" -foregroundColor Green
+    $resultContent = $response.Content | convertfrom-json
 
-    if ($response.Content.ContinuationToken -ne $null) {
-      #write-host "continuation token: $($response.ContinuationToken)" -foregroundColor Green
-      $attributes.continuationToken = $response.Content.ContinuationToken
-      return invoke-rest -method $method -absolutePath $absolutePath -body $body -attributes $attributes -continuationToken $response.ContinuationToken
+    if (![string]::IsNullOrEmpty($resultContent.ContinuationToken)) {
+      write-verbose "continuation token: $($resultContent.ContinuationToken)"
+      if (!$attributes.ContainsKey('continuationToken')) {
+        $attributes.ContinuationToken = $response.ContinuationToken
+      }
+      else {
+        $attributes.ContinuationToken = $response.Content.ContinuationToken
+      }
+
+      $recurseResult = invoke-rest -method $method -absolutePath $absolutePath -body $body -attributes $attributes -continuationToken $response.ContinuationToken
+      $resultContent.Items += $recurseResult.Content.Items
+      #return $response.Content
     }
 
     $global:httpStatusCode = $response.StatusCode
@@ -267,7 +374,14 @@ function invoke-rest($method = 'GET', $absolutePath, $body = $null, $attributes 
       }
     }
 
-    return $responseJson
+    #format response
+    $responseJson = $response.Content | convertfrom-json | convertto-json
+    write-verbose "response: $responseJson"
+
+    if ($resultContent.Items -ne $null) {
+      return $resultContent.Items
+    }
+    return $response.Content
   }
   catch [Exception] {
     write-host "exception calling rest method:$url" -foregroundColor red
@@ -276,43 +390,24 @@ function invoke-rest($method = 'GET', $absolutePath, $body = $null, $attributes 
     write-host $error.Exception.InnerException.StackTrace -ForegroundColor Red
     return $null
   }
-  
-}
-
-function get-sfHttpModule() {
-  if (!(get-module -ListAvailable $sfHttpModule -ErrorAction SilentlyContinue)) {
-    write-host "installing service fabric powershell module" -foregroundColor yellow
-    Install-Module -Name $sfHttpModule -Scope CurrentUser -Force -AllowPrerelease
-  }
-  write-host "importing service fabric powershell module" -foregroundColor yellow
-  Import-Module -Name $sfHttpModule -Force
-  write-host "connecting to service fabric cluster" -foregroundColor yellow
-  write-host "connect-sfcluster -connectionEndpoint $connectionEndpoint -ServerCertThumbprint $thumbprint -X509Credential -FindType FindByThumbprint -FindValue $thumbprint -StoreLocation CurrentUser -StoreName My" -foregroundColor yellow
-  Connect-SFCluster -ConnectionEndpoint $connectionEndpoint -ServerCertThumbprint $thumbprint -X509Credential -FindType FindByThumbprint -FindValue $thumbprint -StoreLocation CurrentUser -StoreName My
-  
-  $backups = Get-SFBackupsFromBackupLocation -Application `
-    -ApplicationName $applicationName `
-    -AzureBlobStore `
-    -ConnectionString "DefaultEndpointsProtocol=https;AccountName=$backupStorageAccountName;AccountKey=$backupStorageAccountKey;EndpointSuffix=blob.core.windows.net" `
-    -ContainerName $containerName
-
-  $backups | ConvertTo-Json -Depth 99
-
-  if ($?) {
-    write-host "connected to service fabric cluster" -foregroundColor yellow
-    write-host "restoring $applicationName from $backupLocation/$backupId" -foregroundColor yellow
-    Restore-ServiceFabricBackup -BackupId $backupId -BackupLocation $backupLocation -BackupStorage $backupStorageAccountName -ApplicationName $applicationName
-    if ($?) {
-      write-host "restored $applicationName from $backupLocation/$backupId" -foregroundColor yellow
-    }
-    else {
-      write-host "failed to restore $applicationName from $backupLocation/$backupId" -foregroundColor red
-    }
-  }
-  else {
-    write-host "failed to connect to service fabric cluster" -foregroundColor red
-  }
 
 }
 
-main
+function start-partitionBackup($partitionId) {
+  $response = invoke-rest -method 'POST' -absolutePath "/Partitions/$partitionId/$/Backup" -body (convertto-json @{
+      BackupStorage = @{
+        StorageKind      = 'AzureBlobStore'
+        ConnectionString = "DefaultEndpointsProtocol=https;AccountName=$backupStorageAccountName;AccountKey=$backupStorageAccountKey;EndpointSuffix=core.windows.net"
+        ContainerName    = $containerName
+      }
+    }) `
+    -attributes @{
+    BackupTimeout = $backupTimeoutMinutes
+  }
+  return $response
+}
+
+if (!$loadFunctionsOnly) {
+  main
+}
+
