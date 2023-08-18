797263c3dc84520d0116d1fdd8cadb83b5bd5595
jagilber <jagilber@microsoft.com>
2023-08-18 10:20:21 -0400

    add cache

--- prompt.ps1
+++ prompt.ps1
@@ -1,27 +1,51 @@
 <#
 in ps open $PROFILE and add the following:
+code $PROFILE
 #>
 $global:path = $null
 $global:branch = $null
-$global:diff = $null
+$global:remotes = @()
+$global:status = $null
 $global:ps = if ($IsCoreCLR) { 'pwsh' } else { 'ps' }
+$global:cacheTimer = [datetime]::MinValue
 #[console]::ForegroundColor = 'Magenta'
 
 function prompt() {
     $path = "'$pwd'".ToLower()
-    if ($path -ne $global:path) {
+    if ($path -ne $global:path `
+        -or (((get-date) - $global:cacheTimer).TotalMinutes) -gt 1) { #`
+        #-or ($global:branch -and (get-history -count 1) -imatch 'git')) {
+        $global:cacheTimer = get-date
         $global:path = $path
         $global:branch = git branch --show-current
         if ($global:branch) {  
-            $global:branch = " $global:branch"
-            $global:diff = @(git status --porcelain).count
-            if ($global:diff -gt 0) { 
-                $global:branch = "$global:branch*($global:diff)" 
-            }
+            $global:status = get-gitInfo
         }
     }
 
     $date = (get-date).ToString('HH:mm:ss')
-    write-host "$global:ps@$date$global:branch $path" -ForegroundColor Cyan
+    write-host "$global:ps@$date$global:status $path" -ForegroundColor Cyan
     return ">"
+}
+
+function get-gitInfo(){
+    $global:remotes = @(git remote)
+    $status = " ($global:branch)"
+    $diff = @(git status --porcelain).count
+    if ($diff -gt 0) { 
+        $status = " ($global:branch*$diff)" 
+    }
+
+    foreach($remote in $global:remotes){
+        #write-host "git rev-list --left-right --count $($remote)/$($global:branch)...$($global:branch)" 
+        try{
+            $aheadBehind = git rev-list --left-right --count "$($remote)/$($global:branch)...$($global:branch)"
+        } catch {
+            $aheadBehind = "0 0"
+        }
+        $aheadBehind = [regex]::replace($aheadBehind, '(\d+)\s+(\d+)', 'v$1/^$2')
+        $status += "[$($remote):$($aheadBehind)]"
+        #write-host "status:$($status)"
+    }
+    return $status
 }
