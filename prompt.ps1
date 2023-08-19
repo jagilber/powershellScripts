587281fff7c0aa62080db3ca2ac90d095327dac4
jagilber <jagilber@microsoft.com>
2023-08-18 20:15:19 -0400

    make promptInfo property object

--- prompt.ps1
+++ prompt.ps1
@@ -12,53 +12,70 @@
     code $PROFILE
     code .\prompt.ps1
 #>
-$global:path = $null
-$global:branch = $null
-$global:remotes = @()
-$global:status = $null
-$global:ps = if ($IsCoreCLR) { 'pwsh' } else { 'ps' }
-$global:cacheTimer = [datetime]::MinValue
+$global:promptInfo = @{
+    path       = $null
+    branch     = $null
+    branches   = @()
+    remotes    = @()
+    status     = $null
+    ps         = if ($IsCoreCLR) { 'pwsh' } else { 'ps' }
+    cacheTimer = [datetime]::MinValue
+    enableGit  = $true
+    cacheMinutes = 1
+}
 #[console]::ForegroundColor = 'Magenta'
 
 function prompt() {
     $path = "'$pwd'".ToLower()
-    try{
-    if ($path -ne $global:path `
-        -or (((get-date) - $global:cacheTimer).TotalMinutes) -gt 1 `
-        -or ($global:branch -and ($^ -and $^.startswith('git')))) {
-        $global:cacheTimer = get-date
-        $global:path = $path
-        $global:branch = git branch --show-current
-        if ($global:branch) {  
-            $global:status = get-gitInfo
+    try {
+        if ($path -ne $promptInfo.path `
+                -or (((get-date) - $promptInfo.cacheTimer).TotalMinutes) -gt $promptInfo.cacheMinutes `
+                -or ($promptInfo.branch -and ($^ -and $^.startswith('git')))) {
+            $promptInfo.cacheTimer = get-date
+            $promptInfo.path = $path
+            $promptInfo.status = get-gitInfo
         }
-    }
 
-    $date = (get-date).ToString('HH:mm:ss')
-    write-host "$global:ps@$date$global:status $path" -ForegroundColor Cyan
-    return ">"
-    } catch {
+        $date = (get-date).ToString('HH:mm:ss')
+        write-host "$($promptInfo.ps)@$date$($promptInfo.status) $path" -ForegroundColor Cyan
+        return ">"
+    }
+    catch {
         write-host "Error: $($psitem.Exception.Message)`r`n$($psitem.scriptStackTrace)" -ForegroundColor Red
         return ">"
     }
 }
 
-function get-gitInfo(){
-    $global:remotes = @(git remote)
-    $status = " ($global:branch)"
-    $diff = @(git status --porcelain).count
-    if ($diff -gt 0) { 
-        $status = " ($global:branch*$diff)" 
+function get-gitInfo() {
+    $status = ""
+    if (!$promptInfo.enableGit) {
+        return $status
     }
 
-    foreach($remote in $global:remotes){
-        #write-host "git rev-list --left-right --count $($remote)/$($global:branch)...$($global:branch)" 
-        try{
-            $aheadBehind = git rev-list --left-right --count "$($remote)/$($global:branch)...$($global:branch)"
-        } catch {
+    $promptInfo.branches = @(git branch)
+    if (!$promptInfo.branches) {
+        return $status
+    }
+
+    $promptInfo.branch = (($promptInfo.branches) -imatch '\*').TrimStart('*').Trim()
+    $promptInfo.remotes = @(git remote)
+    
+    $status = " ($($promptInfo.branch))"
+    $diff = @(git status --porcelain).count
+    
+    if ($diff -gt 0) {
+        $status = " ($($promptInfo.branch)*$diff)"
+    }
+
+    foreach ($remote in $promptInfo.remotes) {
+        #write-host "git rev-list --left-right --count $($remote)/$($promptInfo.branch)...$($promptInfo.branch)"
+        try {
+            $aheadBehind = git rev-list --left-right --count "$($remote)/$($promptInfo.branch)...$($promptInfo.branch)"
+        }
+        catch {
             $aheadBehind = "0 0"
         }
-        if($aheadBehind){
+        if ($aheadBehind) {
             $aheadBehind = [regex]::replace($aheadBehind, '(\d+)\s+(\d+)', "$([char]0x2193)`$1/$([char]0x2191)`$2")
             $status += "[$($remote):$($aheadBehind)]"
         }
