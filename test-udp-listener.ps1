7395f879733811990d5826ceb241fae3eb4d4018
jason <>
2024-03-01 11:32:36 -0500

    add ability to bind to specific address or any

--- test-udp-listener.ps1
+++ test-udp-listener.ps1
@@ -1,15 +1,23 @@
-﻿# powershell test udp listener for troubleshooting
+﻿<#
+# powershell test udp listener for troubleshooting
 # do a final client connect to free up server receive
 
+# usage:
+# .\test-udp-listener.ps1 -server
+# .\test-udp-listener.ps1 -port 514 -count 10 -server
+# .\test-udp-listener.ps1 -port 514 -count 10 -remoteAddress
+
+#>
+
+[cmdletbinding()]
 param(
-    [int]$port = 9999,
+    [int]$port = 514,
     [int]$count = 0,
-    [string]$hostName = 'localhost', # todo:currently needs to be an ip address
+    [string]$remoteAddress = $env:computername, 
+    [string]$localAddress,
     [switch]$server,
     [string]$message = "Hello World"
 )
-
-$ErrorActionPreference = "Stop"
 
 function main() {
     if ($server) {
@@ -20,14 +28,38 @@ function main() {
     }
 }
 
+function get-localAddress() {
+    if (!$localAddress) {
+        $ipAddress = [net.ipaddress]::Any
+    }
+    else {
+        $resolution = [net.dns]::Resolve($localAddress)
+        write-verbose "resolution: $($resolution | out-string)"
+        $addressList = $resolution.AddressList
+        write-verbose "addressList: $($addressList | out-string)"
+        $ipAddress = $addressList | Where-Object ipaddresstostring -ieq $localAddress
+        write-verbose "ipAddress: $($ipAddress | out-string)"
+
+        if (!$ipAddress -or ($ipAddress.gettype() -ne [net.ipaddress])) {
+            write-host "local ip address '$localAddress' not resolved. using 'Any'`r`n$($addressList.IPAddressToString | out-string)"
+            $ipAddress = [net.ipaddress]::Any
+        }
+    }
+    write-host "binding to ipAddress: $($ipAddress.IPAddressToString)"
+    return $ipAddress
+}
+
 function startClient() {
     try {    
+        $localIpEndPoint = [net.ipEndPoint]::new((get-localAddress), 0)
         while ($iteration -lt $count -or $count -eq 0) {
             $iteration++
-            $client = [net.sockets.udpClient]::new($hostName, $port);
-            write-host "count: $iteration sending $message"
-            [byte[]]$sendBytes = [text.encoding]::ASCII.GetBytes($message);
-            [void]$client.Send($sendBytes, $sendBytes.Length);
+            $client = [net.sockets.udpClient]::new($localIpEndPoint)
+            $iterationMessage = "count: $iteration sending $message"
+            write-host $iterationMessage
+
+            [byte[]]$sendBytes = [text.encoding]::ASCII.GetBytes($iterationMessage);
+            [void]$client.Send($sendBytes, $sendBytes.Length, $remoteAddress, $port);
             Start-Sleep -Seconds 1
         }
            
@@ -46,17 +78,13 @@ function startClient() {
 
 function startServer() {
     try {
-        #$remoteIpEndPoint = [net.ipEndPoint]::new([net.IPAddress]::Any, $port)
-        #$localAddress = [net.ipAddress]::new(@(0, 0, 0, 0));
-        $remoteIpEndPoint = [net.ipEndPoint]::new([net.ipAddress]::Any, 0)
-        #$remoteIpEndPoint = [net.ipEndPoint]::new($localAddress, 0)
-
+        $localIpEndPoint = [net.ipEndPoint]::new((get-localAddress), 0)
         $server = [net.sockets.udpClient]::new($port)
         write-host "server started on port $port"
 
         while ($iteration -lt $count -or $count -eq 0) {
             $iteration++
-            [byte[]]$receiveBytes = $server.receive([ref]$remoteIpEndPoint)
+            [byte[]]$receiveBytes = $server.receive([ref]$localIpEndPoint)
             Write-Verbose "$(get-date) received message on port $port";
 
             $received = [text.encoding]::ASCII.GetString($receiveBytes)
@@ -73,8 +101,8 @@ function startServer() {
     }
     finally {
         if ($server) {
-           # $server.Stop();
-           $server.Close();
+            # $server.Stop();
+            $server.Close();
         }
     }
 }
