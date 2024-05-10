4b25d6071a662d41fdbbb7e4213bdc3fc60f3c28
jagilber <jagilber@microsoft.com>
2024-05-10 19:40:37 -0400

    init

---
+++ developer-prompt.ps1
@@ -0,0 +1,59 @@
+<#
+.SYNOPSIS
+  This script opens a Visual Studio Developer Command Prompt in the current shell.
+.DESCRIPTION
+  This script opens a Visual Studio Developer Command Prompt in the current shell.
+  The script imports the Visual Studio module and calls the Enter-VsDevShell function.
+  The script sets the working directory to the Visual Studio installation directory.
+  The script sets the working directory back to the original location after the Visual Studio Developer Command Prompt is closed.
+.PARAMETER vsVersion
+  The Visual Studio version to use. The default value is "2022".
+.PARAMETER vsBasePath
+  The Visual Studio base path. The default value is "C:\Program Files\Microsoft Visual Studio".
+.EXAMPLE
+  .\developer-prompt.ps1 -vsVersion "2022" -vsBasePath "C:\Program Files\Microsoft Visual Studio"
+  This example opens a Visual Studio Developer Command Prompt for Visual Studio 2022.
+
+#>
+param(
+  $vsVersion = "2022",
+  $vsBasePath = "C:\Program Files\Microsoft Visual Studio"
+)
+
+function main() {
+  $currentLocation = (Get-Location).Path
+  
+  $vsModulePath = "$vsBasePath\$vsVersion\Enterprise\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
+  if (-not (Test-Path $vsModulePath)) {
+    write-error "Visual Studio module not found at $vsModulePath"
+    return
+  }
+  try {
+
+    $workingDir = "$vsBasePath\$vsVersion\Enterprise\"
+    Set-Location $workingDir
+
+    write-host "Import-Module -name $vsModulePath -PassThru"
+    $result = Import-Module -name $vsModulePath -PassThru
+
+    if ($result -eq $null) {
+      write-error "Failed to import module $vsModulePath"
+      return
+    }
+    
+    $error.clear()
+    write-host "Enter-VsDevShell -VsInstallPath $vsBasePath\$vsVersion\Enterprise -SkipAutomaticLocation"
+    Enter-VsDevShell -VsInstallPath $vsBasePath\$vsVersion\Enterprise -SkipAutomaticLocation
+    if ($error) {
+      write-error "Failed to enter Visual Studio Developer Command Prompt"
+      return
+    }
+  }
+  catch {
+  }
+  finally {
+    Set-Location $currentLocation
+  }
+}
+
+main
