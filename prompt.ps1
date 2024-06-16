09e27057e10b8d7cd7e313a9d2c8dc62eaae5da0
jason <>
2024-06-15 22:05:10 -0400

    set tmie back

--- prompt.ps1
+++ prompt.ps1
@@ -19,6 +19,7 @@ $PSModuleAutoLoadingPreference = 2
 #$DebugPreference = "Continue"
 $global:promptInfo = $null
 # set terminal tab completion same as editor
+#Set-PSReadLineKeyHandler -Chord Tab -Function TabCompleteNext # default
 #Set-PSReadLineKeyHandler -Chord Tab -Function AcceptSuggestion
 #Set-PSReadLineKeyHandler -Chord Tab -Function MenuComplete
 
@@ -138,50 +139,24 @@ function get-commandDuration() {
     else {
         $durationMs = [int]$lastCommand.Duration.TotalMilliseconds.toString('.')
     }
-    $result = '{0:N0}' -f $durationMs
-    # $timespan = [timespan]::fromMilliseconds($durationMs)
+
+    if ($durationMs -gt 1000) {
+        $durationMs = [math]::Round($durationMs / 1000, 2)
+        $precision = 's'
+    
+        if ($durationMs -gt 60) {
+            $durationMs = [math]::Round($durationMs / 60, 2)
+            $precision = 'm'
+    
+            if ($durationMs -gt 60) {
+                $durationMs = [math]::Round($durationMs / 60, 2)
+                $precision = 'h'
+            }
+        }
+    }
+    
     $promptInfo.commandDurationMs = $durationMs
-    # $hours = $null
-    # $minutes = $null
-    # $seconds = $null
-    # $milliseconds = $durationMs
-
-    # if ($timespan.TotalMilliseconds -gt 1000) {
-    #     $precision = 's'
-    #     # $seconds = [int]($durationMs / 1000)
-    #     # $milliseconds = $durationMs % 100
-    
-    #     if ($timespan.TotalSeconds -gt 60) {
-    #         $precision = 'm'
-    #         # $minutes = [int]($durationMs / 1000 / 60)
-    #         # $seconds = $durationMs /1000 % 60
-    #         # $milliseconds = $durationMs /1000 /60 % 60
-    
-    #         if ($timespan.TotalMinutes -gt 60) {
-    #             $precision = 'h'
-    #             # $hours = [int]($durationMs / 3600)
-    #             # $minutes = $durationMs % 60
-    #             # $seconds = $durationMs % 60
-    #             # $milliseconds = $durationMs % 60
-    #         }
-    #     }
-    # }
-
-    # $result = $null
-    # if ($timespan.hours) {
-    #     $result = "$($timespan.hours):$($timespan.minutes):$($timespan.seconds).$($timespan.milliseconds)" #h"
-    # }
-    # elseif ($timespan.minutes) {
-    #     $result = "$($timespan.minutes):$($timespan.seconds).$($timespan.milliseconds)" #m"
-    # }
-    # elseif ($timespan.seconds) {
-    #     $result = "$($timespan.seconds).$($timespan.milliseconds)" #s"
-    # }
-    # elseif ($timespan.milliseconds -or $timespan.milliseconds -eq 0) {
-    #     $result = "$($timespan.milliseconds)" #ms"
-    # }
-
-    return " $($deltaSymbol)$($result)$($precision)"
+    return " $($deltaSymbol)$($promptInfo.commandDurationMs)$($precision)"
 }
 
 function get-currentBranch() {
