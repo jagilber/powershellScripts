d1267db5b1284bc5c36044e01857368f47943000
jagilber <jagilber@microsoft.com>
2024-10-10 16:11:48 -0400

    add header

--- serviceFabric/azure-az-sf-test-managedIdentityTokenService.ps1
+++ serviceFabric/azure-az-sf-test-managedIdentityTokenService.ps1
@@ -1,5 +1,42 @@
 <#
+.SYNOPSIS
 tests service fabric managed identity token service and azure metadata instance
+
+.DESCRIPTION
+This script is used to test the service fabric managed identity token service and the azure metadata instance. It will attempt to retrieve a secret from a key vault using the managed identity token service.
+
+.PARAMETER secretUrl
+  The URL of the secret in the key vault.
+
+.PARAMETER identityHeader
+  The identity header to use for the request.
+
+.PARAMETER identityEndpoint
+  The identity endpoint to use for the request.
+
+.PARAMETER identityServerThumbprint
+  The thumbprint of the identity server certificate.
+
+.PARAMETER identityApiVersion
+  The version of the identity API to use.
+
+.PARAMETER resource
+  The resource to request a token for.
+
+.PARAMETER useMetadataEndpoint
+  Use the metadata endpoint instead of the identity endpoint.
+
+.EXAMPLE
+  .\azure-az-sf-test-managedIdentityTokenService.ps1 -secretUrl 'https://<keyvault>.vault.azure.net/secrets/<secretName>/<secretVersion>'
+
+.EXAMPLE
+  .\azure-az-sf-test-managedIdentityTokenService.ps1 -useMetadataEndpoint
+
+.LINK
+    [net.servicePointManager]::Expect100Continue = $true;[net.servicePointManager]::SecurityProtocol = [net.SecurityProtocolType]::Tls12;
+    invoke-webRequest "https://raw.githubusercontent.com/jagilber/powershellScripts/master/serviceFabric/azure-az-sf-test-managedIdentityTokenService.ps1" -outFile "$pwd/azure-az-sf-test-managedIdentityTokenService.ps1";
+    ./azure-az-sf-test-managedIdentityTokenService.ps1
+
 #>
 param(
   $secretUrl = 'https://<keyvault>.vault.azure.net/secrets/<secretName>/<secretVersion>',
@@ -14,9 +51,11 @@ param(
 #$cert = (dir Cert:\LocalMachine\My\$env:IDENTITY_SERVER_THUMBPRINT)
 $useCore = $PSVersionTable.PSEdition -ieq 'core'
 $metadataIp = '169.254.169.254'
+$irmArgs = @{}
+[environment]::GetEnvironmentVariables().getEnumerator()| sort-object Name
 
 if (!$useCore) {
-  add-type @"
+    add-type @"
 using System;
 using System.Net;
 using System.Security.Cryptography.X509Certificates;
@@ -32,58 +71,68 @@ public class IDontCarePolicy : ICertificatePolicy {
     }
 }
 "@
-  [System.Net.ServicePointManager]::CertificatePolicy = new-object IDontCarePolicy 
+    [System.Net.ServicePointManager]::CertificatePolicy = new-object IDontCarePolicy 
 }
   
 if ($useMetadataEndpoint) {
-  # container will need a static route to the host to reach the metadata endpoint
-  if (!(tnc $metadataIp -p 80).TcpTestSucceeded) {
-    route print
-    $ipconfiguration = Get-NetIPConfiguration
-    $interfaceAlias = $ipconfiguration.InterfaceAlias
-    $defaultGateway = $ipconfiguration.IPv4DefaultGateway.NextHop
-        
-    Write-Warning "unable to connect to $metadataIp . adding static route to host with new-netRoute.
-        New-NetRoute -DestinationPrefix '$metadataIp/32' -AddressFamily IPv4 -InterfaceAlias '$($interfaceAlias)' -NextHop '$($defaultGateway)'"
-    New-NetRoute -DestinationPrefix "$metadataIp/32" -AddressFamily IPv4 -InterfaceAlias "$($interfaceAlias)" -NextHop "$($defaultGateway)"
-        
+    # container will need a static route to the host to reach the metadata endpoint
     if (!(tnc $metadataIp -p 80).TcpTestSucceeded) {
-      Write-Warning "unable to connect to $metadataIp. returning."
-      return
+        route print
+        $ipconfiguration = Get-NetIPConfiguration
+        $interfaceAlias = $ipconfiguration.InterfaceAlias
+        $defaultGateway = $ipconfiguration.IPv4DefaultGateway.NextHop
+        
+        Write-Warning "unable to connect to $metadataIp . adding static route to host with new-netRoute.
+        New-NetRoute -DestinationPrefix '$metadataIp/32' -AddressFamily IPv4 -InterfaceAlias '$($interfaceAlias)' -NextHop '$($defaultGateway)'"
+        New-NetRoute -DestinationPrefix "$metadataIp/32" -AddressFamily IPv4 -InterfaceAlias "$($interfaceAlias)" -NextHop "$($defaultGateway)"
+        
+        if (!(tnc $metadataIp -p 80).TcpTestSucceeded) {
+            Write-Warning "unable to connect to $metadataIp. returning."
+            return
+        }
     }
-  }
 
-  $identityEndpoint = "http://$metadataIp/metadata/identity/oauth2/token"
-  $identityApiVersion = '2018-02-01'
-  $response = invoke-restmethod -Uri "$($identityEndpoint)?api-version=$($identityApiVersion)&resource=$($resource)" `
-    -Method 'get' `
-    -Headers @{'Metadata' = 'true' }
+    $identityEndpoint = "http://$metadataIp/metadata/identity/oauth2/token"
+    $identityApiVersion = '2018-02-01'
+  
+    $irmArgs = @{
+        uri     = "$($identityEndpoint)?api-version=$($identityApiVersion)&resource=$($resource)"
+        method  = 'get'
+        headers = @{'Metadata' = 'true' } 
+    }
 }
 else {
-  $header = @{
-    "Secret" = $env:IDENTITY_HEADER
-  }
+    $header = @{
+        "Secret" = $env:IDENTITY_HEADER
+    }
     
-  $irmArgs = @{
-    method  = 'get'
-    uri     = "$($identityEndpoint)?api-version=$($identityApiVersion)&resource=$($resource)"
-    headers = $header
-    #certificateThumbprint = $cert
-  }
-  if ($useCore) {
-    [void]$irmArgs.Add("SkipCertificateCheck", $true)
-    [void]$irmArgs.Add("SkipHttpErrorCheck", $true)
-  }
-  
-  $response = invoke-restmethod @irmArgs
+    $irmArgs = @{
+        method  = 'get'
+        uri     = "$($identityEndpoint)?api-version=$($identityApiVersion)&resource=$($resource)"
+        headers = $header
+        #certificateThumbprint = $cert
+    }
+    if ($useCore) {
+        [void]$irmArgs.Add("SkipCertificateCheck", $true)
+        [void]$irmArgs.Add("SkipHttpErrorCheck", $true)
+    }
 }
+
+write-host "invoke-restMethod $($irmArgs | convertto-json)" -foregroundColor Cyan
+$response = invoke-restmethod @irmArgs
 
 $response
 $bearertoken = "Bearer " + $response.access_token
 write-host "$bearertoken" -ForegroundColor Cyan
 
+write-host "
+Invoke-RestMethod -Uri "$($secretUrl)?api-version=2016-10-01" `
+    -Method GET `
+    -Headers @{Authorization = $bearertoken }
+" -foregroundColor Cyan
+
 $result = Invoke-RestMethod -Uri "$($secretUrl)?api-version=2016-10-01" `
-  -Method GET `
-  -Headers @{Authorization = $bearertoken }
+    -Method GET `
+    -Headers @{Authorization = $bearertoken }
 
 write-host "result $($result | convertto-json)" -ForegroundColor Cyan
