d7d0faa1960ff54ccafed4da4f9e15f251474df6
jagilber <jagilber@microsoft.com>
2025-01-08 18:18:54 -0500

    add -disableAuditing

--- enable-file-auditing.ps1
+++ enable-file-auditing.ps1
@@ -1,7 +1,7 @@
 <#
 # This script enables auditing of object access success/failure 
 # and adds an audit rule for the 'Everyone' group on a specified folder and subfolders.
-# 250106
+# 250108
 
 [net.servicePointManager]::Expect100Continue = $true;[net.servicePointManager]::SecurityProtocol = [net.SecurityProtocolType]::Tls12;
 invoke-webRequest "https://raw.githubusercontent.com/jagilber/powershellScripts/master/enable-file-auditing.ps1" -outFile "$pwd\enable-file-auditing.ps1";
@@ -9,32 +9,72 @@ invoke-webRequest "https://raw.githubusercontent.com/jagilber/powershellScripts/
 #>
 
 param(
-    [Parameter(Mandatory=$true)]
-    [string]$Path
+    [Parameter(Mandatory = $true)]
+    [string]$Path,
+    [switch]$disableAuditing
 )
 
-# Enable auditing of object access at the OS level
-Write-Host "Enabling Object Access auditing..." -ForegroundColor Yellow
-write-host "auditpol /set /category:'Object Access' /success:enable /failure:enable" -ForegroundColor Cyan
-auditpol /set /category:'Object Access' /success:enable /failure:enable
+if (!(Test-Path $Path)) {
+    Write-Host "Path '$Path' does not exist." -ForegroundColor Red
+    return
+}
 
-write-host "auditpol /get /category:'Object Access'" -ForegroundColor Cyan
-auditpol /get /category:'Object Access'
-
-# Retrieve ACL from specified path
+write-host "Retrieve ACL from specified path" -ForegroundColor Cyan
+write-host "Get-Acl -Path $Path"
 $acl = Get-Acl -Path $Path
 
-# Create new audit rule for 'Everyone', applying to all subfolders and files
-$auditRule = New-Object System.Security.AccessControl.FileSystemAuditRule(
-    "Everyone",
-    "FullControl",
-    ([System.Security.AccessControl.InheritanceFlags]::ContainerInherit -bor [System.Security.AccessControl.InheritanceFlags]::ObjectInherit),
-    [System.Security.AccessControl.PropagationFlags]::None,
-    ([System.Security.AccessControl.AuditFlags]::Success -bor [System.Security.AccessControl.AuditFlags]::Failure)
-)
+write-host "current ACL" -ForegroundColor Cyan
+$acl | Format-List
 
-# Add rule to ACL and apply
-$acl.AddAuditRule($auditRule)
-Set-Acl -Path $Path -AclObject $acl
+write-host "Object Access policy before changes: auditpol /get /category:'Object Access'" -ForegroundColor Yellow
+auditpol /get /category:'Object Access'
 
-Write-Host "Auditing enabled for '$Path' with new rule for 'Everyone'."
+if (!$disableAuditing) {
+    # Enable auditing of object access at the OS level
+    Write-Host "Enabling Object Access auditing..." -ForegroundColor Yellow
+    write-host "auditpol /set /category:'Object Access' /success:enable /failure:enable" -ForegroundColor Cyan
+    auditpol /set /category:'Object Access' /success:enable /failure:enable
+
+    write-host "auditpol /get /category:'Object Access'" -ForegroundColor Cyan
+    auditpol /get /category:'Object Access'
+
+    # Create new audit rule for 'Everyone', applying to all subfolders and files
+    $auditRule = New-Object Security.AccessControl.FileSystemAuditRule(
+        "Everyone",
+        "FullControl",
+    ([Security.AccessControl.InheritanceFlags]::ContainerInherit -bor [Security.AccessControl.InheritanceFlags]::ObjectInherit),
+        [Security.AccessControl.PropagationFlags]::None,
+    ([Security.AccessControl.AuditFlags]::Success -bor [Security.AccessControl.AuditFlags]::Failure)
+    )
+
+    # Add rule to ACL and apply
+    $acl.AddAuditRule($auditRule)
+    write-host "Set-Acl -Path $Path -AclObject $acl" -ForegroundColor Cyan
+    Set-Acl -Path $Path -AclObject $acl
+    Write-Host "Auditing enabled for '$Path' with new rule for 'Everyone'." -ForegroundColor Green
+}
+else {
+    # Disable auditing of object access at the OS level
+    Write-Host "Disabling Object Access auditing..." -ForegroundColor Yellow
+    write-host "auditpol /set /category:'Object Access' /success:disable /failure:disable" -ForegroundColor Cyan
+    auditpol /set /category:'Object Access' /success:disable /failure:disable
+
+    $acl = Get-Acl -Path $Path
+    $references = $acl.Audit | Where-Object IdentityReference -eq "Everyone"
+    foreach ($reference in $references) {
+        write-host "Removing audit rule for 'Everyone'..." -ForegroundColor Yellow
+        $acl.RemoveAuditRule($reference)
+    }
+    
+    Set-Acl -Path $Path -AclObject $acl
+    Write-Host "Auditing disabled for '$Path'." -ForegroundColor Green
+}
+
+write-host "Current Object Access policy:auditpol /get /category:'Object Access'" -ForegroundColor Cyan
+auditpol /get /category:'Object Access'
+
+write-host "current ACL" -ForegroundColor Cyan
+$global:acl = Get-Acl -Path $Path
+$global:acl | Format-List
+
+write-host "finished" -ForegroundColor Cyan
