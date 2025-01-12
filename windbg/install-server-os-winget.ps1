69922637bd36a7ac2fd472996d1a4465bf052da7
jagilber <jagilber@microsoft.com>
2025-01-12 11:08:34 -0500

    modify to send commands to ps 5.1

--- windbg/install-server-os-winget.ps1
+++ windbg/install-server-os-winget.ps1
@@ -11,61 +11,98 @@ param(
   [string]$appPackagePath = ".\microsoft.ui.xaml.2.8.6\tools\AppX\x64\Release\Microsoft.UI.Xaml.2.8.appx",
   [string]$vcLibsPath = "C:\Program Files (x86)\Microsoft SDKs\Windows Kits\10\ExtensionSDKs\Microsoft.VCLibs.Desktop\14.0\AppX\Retail\x64\Microsoft.VCLibs.x64.14.00.Desktop.appx",
   [string]$vcLibsUrl = "https://raw.githubusercontent.com/jagilber/powershellScripts/refs/heads/master/windbg/Microsoft.VCLibs.x64.14.00.Desktop.appx",
-  [string]$wingetUrl = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
+  [string]$wingetUrl = "https://api.github.com/repos/microsoft/winget-cli/releases/latest",
+  [int]$sleepSeconds = 5
 )
 
 $ErrorActionPreference = 'continue'
+$script = $MyInvocation.MyCommand.Definition
+$scriptParams = $MyInvocation.BoundParameters
 
-if ($PSVersionTable.PSVersion.Major -gt 5) {
-  Write-Warning "This script requires PowerShell 5. use powershell.exe"
-  return
-}
-
-$vcLibsAppx = $vcLibsPath.Split("\")[-1]
-if (!(test-path $vcLibsPath)) {
-  $vcLibsPath = "$pwd\$vcLibsAppx"
-  if (!(test-path $vcLibsPath)) {
-    write-host "downloading file $vcLibsUrl to $vcLibsPath"
-    [net.webclient]::new().DownloadFile($vcLibsUrl, $vcLibsPath)
+function main() {
+  write-host "executing $script with parameters $($scriptParams | out-string)" -ForegroundColor Green
+  
+  if(is-wingetInstalled) {
+    write-host "winget is already installed" -ForegroundColor Green
+    return
   }
+
+  if ($PSVersionTable.PSVersion.Major -gt 5) {
+    Write-Warning "This script requires PowerShell 5. switching to powershell.exe"
+    powershell $script @scriptParams
+    return
+  }
+
+  $vcLibsAppx = $vcLibsPath.Split("\")[-1]
+  if (!(test-path $vcLibsPath)) {
+    $vcLibsPath = "$pwd\$vcLibsAppx"
+    if (!(test-path $vcLibsPath)) {
+      write-host "downloading file $vcLibsUrl to $vcLibsPath"
+      [net.webclient]::new().DownloadFile($vcLibsUrl, $vcLibsPath)
+    }
+  }
+
+  if (!(Test-Path $vcLibsPath)) {
+    Write-Error "VC Libs not found at $vcLibsPath. Please install the Windows SDK or Visual Studio 2022."
+    return
+  }
+
+  if (!(test-path $xamlPath)) {
+    [net.webclient]::new().DownloadFile($xamlDownloadUrl, $xamlPath)
+    Expand-Archive $xamlPath
+  }
+
+  write-host "Add-AppxPackage $appPackagePath"
+  Add-AppxPackage $appPackagePath
+
+  $latestWingetMsixBundleUri = $(Invoke-RestMethod $wingetUrl).assets.browser_download_url | Where-Object { $psitem.EndsWith(".msixbundle") }
+  $latestWingetMsixBundle = "$pwd\$($latestWingetMsixBundleUri.Split("/")[-1])"
+
+  write-host "latestWingetMsixBundleUri: $latestWingetMsixBundleUri"
+  write-host "latestWingetMsixBundle: $latestWingetMsixBundle"
+
+  $latestWingetMsixLicenseUri = $(Invoke-RestMethod $wingetUrl).assets.browser_download_url | Where-Object { $psitem -imatch ".+license.+.xml" }
+  $latestWingetMsixLicense = "$pwd\$($latestWingetMsixLicenseUri.Split("/")[-1])"
+
+  write-host "latestWingetMsixLicenseUri: $latestWingetMsixLicenseUri"
+  write-host "latestWingetMsixLicense: $latestWingetMsixLicense"
+
+  if (!(test-path $latestWingetMsixBundle) -or !(test-path $latestWingetMsixLicense)) {
+
+    write-host "Downloading winget to $latestWingetMsixBundle"
+    [net.webclient]::new().DownloadFile($latestWingetMsixBundleUri, $latestWingetMsixBundle)
+
+    write-host "Downloading winget license to $latestWingetMsixLicense"
+    [net.webclient]::new().DownloadFile($latestWingetMsixLicenseUri, $latestWingetMsixLicense)
+  }
+
+  write-host "Add-AppxPackage $vcLibsAppx"
+  Add-AppxPackage $vcLibsAppx
+
+  write-host "Add-AppxProvisionedPackage -Online -PackagePath $latestWingetMsixBundle -LicensePath $latestWingetMsixLicense -Verbose"
+  Add-AppxProvisionedPackage -Online -PackagePath $latestWingetMsixBundle -LicensePath $latestWingetMsixLicense -Verbose
+  write-host "sleeping for $sleepSeconds seconds"
+  start-sleep -Seconds $sleepSeconds
+
+  if (is-wingetInstalled) {
+  
+    write-host "winget search microsoft.windbg"
+    winget search microsoft.windbg
+
+    write-host "winget install microsoft.windbg --source winget"
+    winget install microsoft.windbg --source winget
+  }
+  else {
+    Write-Error "winget not found. Please restart your shell."
+  }
+
+  write-host "finished"
 }
 
-if (!(Test-Path $vcLibsPath)) {
-  Write-Error "VC Libs not found at $vcLibsPath. Please install the Windows SDK or Visual Studio 2022."
-  return
+function is-wingetInstalled() {
+  $isInstalled = get-command winget -ErrorAction SilentlyContinue
+  write-host "is-wingetInstalled: $isInstalled" -ForegroundColor Magenta
+    return $isInstalled
 }
 
-if (!(test-path $xamlPath)) {
-  [net.webclient]::new().DownloadFile($xamlDownloadUrl, $xamlPath)
-  Expand-Archive $xamlPath
-}
-
-write-host "Add-AppxPackage $appPackagePath"
-Add-AppxPackage $appPackagePath
-
-$latestWingetMsixBundleUri = $(Invoke-RestMethod $wingetUrl).assets.browser_download_url | Where-Object { $psitem.EndsWith(".msixbundle") }
-$latestWingetMsixBundle = "$pwd\$($latestWingetMsixBundleUri.Split("/")[-1])"
-
-write-host "latestWingetMsixBundleUri: $latestWingetMsixBundleUri"
-write-host "latestWingetMsixBundle: $latestWingetMsixBundle"
-
-$latestWingetMsixLicenseUri = $(Invoke-RestMethod $wingetUrl).assets.browser_download_url | Where-Object { $psitem -imatch ".+license.+.xml" }
-$latestWingetMsixLicense = "$pwd\$($latestWingetMsixLicenseUri.Split("/")[-1])"
-
-write-host "latestWingetMsixLicenseUri: $latestWingetMsixLicenseUri"
-write-host "latestWingetMsixLicense: $latestWingetMsixLicense"
-
-if (!(test-path $latestWingetMsixBundle) -or !(test-path $latestWingetMsixLicense)) {
-
-  write-host "Downloading winget to $latestWingetMsixBundle"
-  [net.webclient]::new().DownloadFile($latestWingetMsixBundleUri, $latestWingetMsixBundle)
-
-  write-host "Downloading winget license to $latestWingetMsixLicense"
-  [net.webclient]::new().DownloadFile($latestWingetMsixLicenseUri, $latestWingetMsixLicense)
-}
-
-write-host "Add-AppxPackage $vcLibsAppx"
-Add-AppxPackage $vcLibsAppx
-
-write-host "Add-AppxProvisionedPackage -Online -PackagePath $latestWingetMsixBundle -LicensePath $latestWingetMsixLicense -Verbose"
-Add-AppxProvisionedPackage -Online -PackagePath $latestWingetMsixBundle -LicensePath $latestWingetMsixLicense -Verbose
+main
