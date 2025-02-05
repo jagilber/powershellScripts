f4da663f51f7bd52af3d436882c36a10fec020fd
jagilber <jagilber@microsoft.com>
2025-02-05 14:47:57 -0500

    update

--- dotnet-trace-collect.ps1
+++ dotnet-trace-collect.ps1
@@ -1,74 +1,87 @@
 <#
 # needs dotnet sdk and dotnet-trace
 dotnet tool install --global dotnet-trace
+https://learn.microsoft.com/en-us/dotnet/core/tools/dotnet-install-script
 #>
 param(
-  $name = "ManagedIdentityTokenService",
-  $providers = @{
-    "Microsoft.AspNetCore.Server.Kestrel" = 5
-    "Microsoft.AspNetCore.Server.Kestrel.BadRequests" = 5
-    "Microsoft.AspNetCore.Server.Kestrel.Connections" = 5
-    "Microsoft.AspNetCore.Server.Kestrel.Http2" = 5
-    "Microsoft.AspNetCore.Server.Kestrel.Http3" = 5
-    "System.Net" = 5
-    "System.Net.AspNetCore.Http" = 5
-    "System.Net.AspNetCore" = 5
-    "System.Net.Http" = 5
-    "System.Net.NameResolution" = 5
-    "System.Net.Sockets" = 5
-    "System.Net.Security" = 5
-    "System.Net.TestLogging" = 5
-    "Private.InternalDiagnostics.System.Net.Http" = 5
-    "Private.InternalDiagnostics.System.Net.NameResolution" = 5
-    "Private.InternalDiagnostics.System.Net.Sockets" = 5
-    "Private.InternalDiagnostics.System.Net.Security" = 5
-    "Private.InternalDiagnostics.System.Net.Quic" = 5
-    "Private.InternalDiagnostics.System.Net.Http.WinHttpHandler" = 5
-    "Private.InternalDiagnostics.System.Net.HttpListener" = 5
-    "Private.InternalDiagnostics.System.Net.Mail" = 5
-    "Private.InternalDiagnostics.System.Net.NetworkInformation" = 5
-    "Private.InternalDiagnostics.System.Net.Primitives" = 5
-    "Private.InternalDiagnostics.System.Net.Requests" = 5
+  [string]$name = "ManagedIdentityTokenService",
+  [string]$traceFile = "$($name).nettrace",
+  [int]$logLevel = 5,
+  [string]$keywords = '0xffffffffffffffff',
+  [hashtable]$providers = @{
+    "Microsoft-Windows-Crypto-RSAEnh"                            = $logLevel
+    "Microsoft-Windows-Crypto-BCrypt"                            = $logLevel
+    "Microsoft-Windows-Crypto-CAPI2"                             = $logLevel
+    "Microsoft.AspNetCore.Server.Kestrel"                        = $logLevel
+    "Microsoft.AspNetCore.Server.Kestrel.BadRequests"            = $logLevel
+    "Microsoft.AspNetCore.Server.Kestrel.Connections"            = $logLevel
+    "Microsoft.AspNetCore.Server.Kestrel.Http2"                  = $logLevel
+    "Microsoft.AspNetCore.Server.Kestrel.Http3"                  = $logLevel
+    "System.Net"                                                 = $logLevel
+    "System.Net.AspNetCore.Http"                                 = $logLevel
+    "System.Net.AspNetCore"                                      = $logLevel
+    "System.Net.Http"                                            = $logLevel
+    "System.Net.NameResolution"                                  = $logLevel
+    "System.Net.Sockets"                                         = $logLevel
+    "System.Net.Security"                                        = $logLevel
+    "System.Net.TestLogging"                                     = $logLevel
+    "Private.InternalDiagnostics.System.Net.Http"                = $logLevel
+    "Private.InternalDiagnostics.System.Net.NameResolution"      = $logLevel
+    "Private.InternalDiagnostics.System.Net.Sockets"             = $logLevel
+    "Private.InternalDiagnostics.System.Net.Security"            = $logLevel
+    "Private.InternalDiagnostics.System.Net.Quic"                = $logLevel
+    "Private.InternalDiagnostics.System.Net.Http.WinHttpHandler" = $logLevel
+    "Private.InternalDiagnostics.System.Net.HttpListener"        = $logLevel
+    "Private.InternalDiagnostics.System.Net.Mail"                = $logLevel
+    "Private.InternalDiagnostics.System.Net.NetworkInformation"  = $logLevel
+    "Private.InternalDiagnostics.System.Net.Primitives"          = $logLevel
+    "Private.InternalDiagnostics.System.Net.Requests"            = $logLevel
   },
-  [string]$traceFile = "$($name).trace",
   [string]$dotnetSdkScriptUrl = "https://dot.net/v1/dotnet-install.ps1",
-  [string]$version = "6.0",
+  [string]$version = "6.0.428",
   [switch]$whatIf
 )
 
-if(!(get-command dotnet -ErrorAction SilentlyContinue)) {
+if (!(get-command dotnet -ErrorAction SilentlyContinue)) {
   write-host "dotnet core is not installed. this script is only for .net core projects"
   return
 }
 
-if(!(dotnet --list-sdks)) {
-  $downloadFile = "$pwd\dotnet-sdk-$version.exe"
+if (!(dotnet --list-sdks)) {
+  $downloadFile = "$pwd\dotnet-install.ps1"
   write-host "dotnet sdk is not installed. Please install it from https://dotnet.microsoft.com/download"
   $install = read-host "Do you want to install it now? (y/n)"
-  if($install -ne "y") {
+  if ($install -ne "y") {
     return
   }
   invoke-webRequest -uri $dotnetSdkScriptUrl -outFile $downloadFile
-  . $downloadFile --version $version
+  . $downloadFile -version $version
 }
 
-if(!(Get-Command dotnet-trace -ErrorAction SilentlyContinue)) {
+if (!(Get-Command dotnet-trace -ErrorAction SilentlyContinue)) {
   write-warning "dotnet-trace is not installed. Please install it using 'dotnet tool install --global dotnet-trace' *and* restart your shell"
-  return
+  $install = read-host "Do you want to install it now? (y/n)"
+  if ($install -ne "y") {
+    return
+  }
+  dotnet tool install --global dotnet-trace
+  $env:path += ";$env:USERPROFILE\.dotnet\tools"
 }
 
 $providersList = $providers.GetEnumerator() | ForEach-Object {
   $provider = $_.Key
   $level = $_.Value
-  "$($provider):0xffffffffffffffff:$($level)"
+  "$($provider):$($keywords):$($level)"
 }
 
 $providerListString = $providersList -join ","
+write-host "dotnet-trace ps"
+dotnet-trace ps
 
 Write-Host "dotnet-trace collect -n $name --output $traceFile --providers $providerListString"
-if($whatIf) {
+if ($whatIf) {
   return
 }
-else{
+else {
   dotnet-trace collect -n $name --output $traceFile --providers $providerListString
 }
